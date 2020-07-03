757e395a450b88609053a17d34238a32ea93b4da
David Ernst <git@dsernst.com>
2020-07-03 15:55:43 -0700

    Replace Pre-step-A image with editable text

--- src/protocol.tsx
+++ src/protocol.tsx
@@ -2,7 +2,10 @@ import { DownloadOutlined } from '@ant-design/icons'
 
 import styles from './protocol.module.css'
 
-const header = [
+type Line = Record<string, string> | { image: [string, number] }
+type Step = Line[]
+
+const header: Step = [
   { title: 'Secure Internet Voting (SIV) Protocol Overview' },
   { subtitle: 'Fast, Private, Verifiable' },
   {
@@ -11,9 +14,19 @@ All over the internet, no installs necessary.`,
   },
 ]
 
+const preA: Step = [
+  { section_name: 'Pre-Step A: Voter Registration Period' },
+  {
+    description: `Voting authority collects list of all valid voters,
+  using the usual methods (in person, DMV, etc).`,
+  },
+  { example: '1 million eligible San Francisco voters' },
+  { image: ['pre-a-voter-list.png', 450] },
+]
+
 const steps = [
   // 'header',
-  'pre-a-voter-registration',
+  // 'pre-a-voter-registration',
   'pre-b-shufflers-registered',
   'pre-c-ballot-finalized',
   'step-1',
@@ -42,16 +55,37 @@ export default function Protocol(): JSX.Element {
       </a>
 
       <div className={styles.protocol}>
-        {header.map((line, index) => {
-          const type = Object.keys(line)[0]
-          const text = Object.values(line)[0]
+        {[header, preA].map((step, stepIndex) => (
+          <div className={styles.step} key={stepIndex}>
+            {step.map((line, lineIndex) => {
+              const type = Object.keys(line)[0]
 
-          return (
-            <p className={styles[type]} key={index}>
-              {text}
-            </p>
-          )
-        })}
+              if (type === 'image') {
+                const filename = line.image[0]
+                const width = line.image[1]
+
+                return <img src={`./overview/${filename}`} width={width} />
+              }
+
+              const text = Object.values(line)[0] as string
+
+              return (
+                <p className={styles[type]} key={lineIndex}>
+                  {/* label for 'Example:' */}
+                  {type === 'example' && <em>Example: </em>}
+
+                  {/* Split on newlines */}
+                  {text.split('\n').map((item, key) => (
+                    <span key={key}>
+                      {item}
+                      <br />
+                    </span>
+                  ))}
+                </p>
+              )
+            })}
+          </div>
+        ))}
       </div>
       {steps.map((filename) => (
         <div key={filename} style={{ backgroundColor: '#e5eafd', paddingBottom: '2rem' }}>
