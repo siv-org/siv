38ed6edae2a0f3ca95b5a3ae3eb5e99de1b18cf8
David Ernst <git@dsernst.com>
2020-07-02 23:14:53 -0700

    Replace single large image w/ individual steps

--- src/protocol.tsx
+++ src/protocol.tsx
@@ -8,7 +8,30 @@ export default function Protocol(): JSX.Element {
         <DownloadOutlined />
         &nbsp;Download single image
       </a>
-      <img src="./Overview.png" width="100%" />
+      {[
+        'header',
+        'pre-a-voter-registration',
+        'pre-b-shufflers-registered',
+        'pre-c-ballot-finalized',
+        'step-1',
+        'step-2a',
+        'step-2b',
+        'step-2c',
+        'step-2d',
+        'step-2e',
+        'step-2f',
+        'step-2g',
+        'step-2h',
+        'step-3',
+        'step-4',
+        'step-5',
+        'step-6',
+        'step-fin',
+      ].map((filename) => (
+        <div key={filename} style={{ backgroundColor: '#e5eafd', paddingBottom: '2rem' }}>
+          <img src={`./overview/${filename}.png`} width="100%" />
+        </div>
+      ))}
     </>
   )
 }
