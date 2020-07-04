826e85d02074a2320d26bc461d432a006299ad9e
David Ernst <git@dsernst.com>
2020-07-03 22:35:19 -0700

    Remove old step images

--- src/protocol.tsx
+++ src/protocol.tsx
@@ -172,19 +172,7 @@ const editable_steps: Line[][] = [
 ]
 
 const image_steps = [
-  // 'header',
-  // 'pre-a-voter-registration',
-  // 'pre-b-shufflers-registered',
-  // 'pre-c-ballot-finalized',
-  // 'step-1',
-  // 'step-2a',
-  // 'step-2b',
-  // 'step-2c',
-  // 'step-2d',
-  // 'step-2e',
-  // 'step-2f',
-  // 'step-2g',
-  // 'step-2h',
+  //
   'step-3',
   'step-4',
   'step-5',
