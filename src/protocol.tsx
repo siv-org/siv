cac9f4ed081e130cb9ea7bbbe41f134fa74fd71f
David Ernst <git@dsernst.com>
2020-07-12 15:37:56 -0500

    Improve look of Accordion'ed steps

--- src/protocol.tsx
+++ src/protocol.tsx
@@ -16,80 +16,86 @@ export default function Protocol(): JSX.Element {
 
       {/* Header */}
       <div className={styles.protocol}>
-        {header.map((line, lineIndex) => {
-          const type = Object.keys(line)[0]
-          const text = Object.values(line)[0] as string
+        <div style={{ padding: '10px 16px' }}>
+          {header.map((line, lineIndex) => {
+            const type = Object.keys(line)[0]
+            const text = Object.values(line)[0] as string
 
-          return (
-            <p className={styles[type]} key={lineIndex}>
-              {text}
-            </p>
-          )
-        })}
+            return (
+              <p className={styles[type]} key={lineIndex}>
+                {text}
+              </p>
+            )
+          })}
+        </div>
 
         {/* All the other steps */}
-        {steps.map((step, stepIndex) => (
+        {steps.map(({ name, rest }, stepIndex) => (
           <Accordion key={stepIndex}>
-            <Summary expandIcon={<ExpandMore />}>{step[0].step_name}</Summary>
-            {step.map((line, lineIndex) => {
-              // Special handling for breaks
-              if (line === '') {
-                return <br key={lineIndex} />
-              }
+            <Summary expandIcon={<ExpandMore />}>
+              <p className={styles.name}>{name}</p>
+            </Summary>
+            <div className={styles.expanded}>
+              {rest.map((line, lineIndex) => {
+                // Special handling for breaks
+                if (line === '') {
+                  return <br key={lineIndex} />
+                }
 
-              const type = Object.keys(line)[0]
+                const type = Object.keys(line)[0]
 
-              // Special handling for images
-              if (type === 'image') {
-                const filename = (line as ImageLine).image[0]
-                const maxWidth = (line as ImageLine).image[1]
+                // Special handling for images
+                if (type === 'image') {
+                  const filename = (line as ImageLine).image[0]
+                  const maxWidth = (line as ImageLine).image[1]
 
-                return <img key={lineIndex} src={`./overview/${filename}`} style={{ maxWidth, width: '100%' }} />
-              }
+                  return <img key={lineIndex} src={`./overview/${filename}`} style={{ maxWidth, width: '100%' }} />
+                }
 
-              // Specially handling for subsections
-              if (type === 'subsection') {
-                const { header, list } = (line as Subsection).subsection
+                // Specially handling for subsections
+                if (type === 'subsection') {
+                  const { header, list } = (line as Subsection).subsection
+                  return (
+                    <div key={lineIndex} style={{ margin: '3rem' }}>
+                      <p style={{ fontSize: 14, fontWeight: 700 }}>{header}:</p>
+                      <ul style={{ fontSize: 7, paddingInlineStart: 13 }}>
+                        {list.map((item, listIndex) => (
+                          <li key={listIndex} style={{ marginBottom: 15 }}>
+                            <span
+                              dangerouslySetInnerHTML={{ __html: item }}
+                              style={{ fontSize: 14, position: 'relative', top: 2 }}
+                            />
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )
+                }
+
+                // Otherwise it's text
+                const text = Object.values(line)[0] as string
+
+                // Special handling to embed html
+                if (type === 'html') {
+                  return <p dangerouslySetInnerHTML={{ __html: text }} key={lineIndex} />
+                }
+
                 return (
-                  <div key={lineIndex} style={{ margin: '3rem' }}>
-                    <p style={{ fontSize: 14, fontWeight: 700 }}>{header}:</p>
-                    <ul style={{ fontSize: 7, paddingInlineStart: 13 }}>
-                      {list.map((item, listIndex) => (
-                        <li key={listIndex} style={{ marginBottom: 15 }}>
-                          <span
-                            dangerouslySetInnerHTML={{ __html: item }}
-                            style={{ fontSize: 14, position: 'relative', top: 2 }}
-                          />
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
+                  <p className={styles[type]} key={lineIndex}>
+                    {/* label for 'Example:' */}
+                    {type === 'example' && <em>Example: </em>}
+
+                    {/* Split on newlines */}
+                    {text.split('\n').map((item, key) => (
+                      <span key={key}>
+                        {item}
+                        <br />
+                      </span>
+                    ))}
+                  </p>
                 )
-              }
-
-              // Otherwise it's text
-              const text = Object.values(line)[0] as string
-
-              // Special handling to embed html
-              if (type === 'html') {
-                return <p dangerouslySetInnerHTML={{ __html: text }} key={lineIndex} />
-              }
-
-              return (
-                <p className={styles[type]} key={lineIndex}>
-                  {/* label for 'Example:' */}
-                  {type === 'example' && <em>Example: </em>}
-
-                  {/* Split on newlines */}
-                  {text.split('\n').map((item, key) => (
-                    <span key={key}>
-                      {item}
-                      <br />
-                    </span>
-                  ))}
-                </p>
-              )
-            })}
+              })}
+            </div>
           </Accordion>
         ))}
       </div>
