0e8560934c64d9fdcb354be8ce3039b7b0c8862d
David Ernst <git@dsernst.com>
2020-07-14 20:00:29 -0500

    Hook up interactive ballot GUI, driving plaintext data

---
+++ src/context.tsx
@@ -0,0 +1,16 @@
+import { createContext, useMemo, useReducer } from 'react'
+
+const initState = { vote_for_mayor: 'London Breed' }
+type State = Record<string, string>
+
+export const Context = createContext<{ dispatch: (payload: State) => void; state: State }>({
+  dispatch: (payload: State) => void payload,
+  state: initState,
+})
+
+export default function ContextProvider({ children }: { children: JSX.Element }) {
+  const [state, dispatch] = useReducer((prev: State, payload: State) => ({ ...prev, ...payload }), initState)
+  const memoized = useMemo(() => ({ dispatch, state }), [dispatch, state])
+
+  return <Context.Provider value={memoized}>{children}</Context.Provider>
+}
--- src/demo/ballot.tsx
+++ src/demo/ballot.tsx
@@ -1,16 +1,34 @@
 import { FormControlLabel, Paper, Radio, RadioGroup } from '@material-ui/core'
+import { useContext } from 'react'
 
-export default function VoterList(): JSX.Element {
-  return (
-    <Paper elevation={3} style={{ marginBottom: 30, padding: '0.5rem 1.5rem' }}>
-      <p style={{ backgroundColor: '#e6eafb', fontWeight: 'bold', marginBottom: 10, padding: '5px 13px' }}>
-        Who should become the next Mayor of San Francisco?
-      </p>
-      <RadioGroup style={{ paddingLeft: '1.5rem' }} value="">
-        {['Angela Alioto', 'London Breed', 'Mark Leno', 'Jane Kim'].map((name) => (
-          <FormControlLabel control={<Radio color="primary" />} key={name} label={name} value={name} />
-        ))}
-      </RadioGroup>
-    </Paper>
-  )
+import { Context } from '../context'
+
+export default function BallotWrapper(enabled?: boolean) {
+  return function Ballot(): JSX.Element {
+    const { dispatch, state } = useContext(Context)
+
+    return (
+      <Paper elevation={3} style={{ marginBottom: 30, padding: '0.5rem 1.5rem' }}>
+        <p
+          style={{
+            backgroundColor: '#e6eafb',
+            fontWeight: 'bold',
+            marginBottom: 10,
+            padding: '5px 13px',
+          }}
+        >
+          Who should become the next Mayor of San Francisco?
+        </p>
+        <RadioGroup
+          onChange={(event) => dispatch({ vote_for_mayor: event.target.value })}
+          style={{ paddingLeft: '1.5rem' }}
+          value={enabled ? state.vote_for_mayor : ''}
+        >
+          {['Angela Alioto', 'London Breed', 'Mark Leno', 'Jane Kim'].map((name) => (
+            <FormControlLabel control={<Radio color="primary" />} key={name} label={name} value={name} />
+          ))}
+        </RadioGroup>
+      </Paper>
+    )
+  }
 }
