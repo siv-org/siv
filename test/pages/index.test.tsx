6b09a9b136fa49aada023bf1d9d9f6df153d8cea
David Ernst <git@dsernst.com>
2020-07-16 19:40:28 -0500

    Remove unused test files and dependencies

--- test/pages/index.test.tsx
+++
@@ -1,8 +0,0 @@
-describe('Home page', () => {
-  // it('matches snapshot', () => {
-  //   const { asFragment } = render(<Home />, {})
-  //   expect(asFragment()).toMatchSnapshot()
-  // })
-})
-
-export {}
