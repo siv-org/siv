d174f67d304a2681dad503a601c43fb07b387b3c
David Ernst <git@dsernst.com>
2020-07-18 03:06:15 -0500

    Show all submitted ballots

--- src/demo/all-submitted-ballots.tsx
+++ src/demo/all-submitted-ballots.tsx
@@ -1,18 +1,30 @@
 import { Paper } from '@material-ui/core'
 
+import { useContext } from '../context'
+import pickRandomInteger from '../crypto/pick-random-integer'
 import { voters } from './election-parameters'
+import { public_key } from './election-parameters'
 
 export default function AllSubmittedBallots(): JSX.Element {
+  const { state } = useContext()
   return (
     <Paper elevation={3} style={{ overflowWrap: 'break-word', padding: 15 }}>
-      {voters.slice(1).map(({ token }) => (
-        <p key={token}>
-          {'{'} token: <span style={{ color: '#d0021b' }}>&apos;{token}&apos;</span> {'}'}
+      <code>
+        {voters.slice(1).map(({ token }) => (
+          <p key={token}>
+            {`{ token: '${token}', secret: '{ sealed_data: ${pickRandomInteger(
+              public_key.modulo,
+            )}, sealing_factor: ${pickRandomInteger(
+              public_key.modulo,
+            )} }', vote_for_mayor: '{ sealed_data: ${pickRandomInteger(
+              public_key.modulo,
+            )}, sealing_factor: ${pickRandomInteger(public_key.modulo)} }'`}
+          </p>
+        ))}
+        <p>
+          {`{ token: '${voters[0].token}', secret: '${state.encrypted.secret}', vote_for_mayor: '${state.encrypted.vote_for_mayor}' }`}
         </p>
-      ))}
-      <p>
-        {'{'} token: <span style={{ color: '#d0021b' }}>&apos;{voters[0].token}&apos;</span> {'}'}
-      </p>
+      </code>
     </Paper>
   )
 }
