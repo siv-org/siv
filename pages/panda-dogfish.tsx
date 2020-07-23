1c73fc11125ad8d10ba16a7aa251b498dc3c11f5
David Ernst <git@dsernst.com>
2020-07-23 16:59:58 -0500

    Redirect from /panda-dogfish to root url

--- pages/panda-dogfish.tsx
+++ pages/panda-dogfish.tsx
@@ -1,1 +1,11 @@
-export { PandaDogfish as default } from '../src/panda-dogfish'
+import Router from 'next/router'
+import { useEffect } from 'react'
+
+// Redirect to root url
+export default function PandaDogfish() {
+  useEffect(() => {
+    Router.push('/')
+  })
+
+  return <div />
+}
