c13e8b5fb90302b99506c9a4bd1248532f3eb3ed
David Ernst <git@dsernst.com>
2020-07-23 22:03:32 -0500

    Use next/Link for Buttons

--- src/panda-dogfish/Button.tsx
+++ src/panda-dogfish/Button.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 const darkBlue = '#002868'
 
 type ButtonProps = {
@@ -14,28 +16,30 @@ export const Button = ({
 }: ButtonProps & {
   href: string
 }): JSX.Element => (
-  <a href={href} style={style}>
-    {children}
-    <style jsx>{`
-      a {
-        background: none;
-        border: 2px solid ${invertColor ? '#fff' : darkBlue};
-        border-radius: 0.4rem;
-        color: ${invertColor ? '#fff' : darkBlue};
-        display: inline-block;
-        font-weight: bold;
-        margin: 17px;
-        padding: 1.2rem 2.004rem;
-        text-decoration: none;
-        transition: 0.1s background-color linear, 0.1s color linear;
-      }
+  <Link href={href} prefetch={href.startsWith('/') ? undefined : false}>
+    <a style={style}>
+      {children}
+      <style jsx>{`
+        a {
+          background: none;
+          border: 2px solid ${invertColor ? '#fff' : darkBlue};
+          border-radius: 0.4rem;
+          color: ${invertColor ? '#fff' : darkBlue};
+          display: inline-block;
+          font-weight: bold;
+          margin: 17px;
+          padding: 1.2rem 2.004rem;
+          text-decoration: none;
+          transition: 0.1s background-color linear, 0.1s color linear;
+        }
 
-      a:hover {
-        background-color: ${invertColor ? '#fff' : darkBlue};
-        color: ${invertColor ? '#000' : '#fff'};
-      }
-    `}</style>
-  </a>
+        a:hover {
+          background-color: ${invertColor ? '#fff' : darkBlue};
+          color: ${invertColor ? '#000' : '#fff'};
+        }
+      `}</style>
+    </a>
+  </Link>
 )
 
 export const OnClickButton = ({
