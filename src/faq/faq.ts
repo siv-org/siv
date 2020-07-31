e64b368c3faa1eea848180479a85a25113300cc1
David Ernst <git@dsernst.com>
2020-07-31 03:48:53 -0700

    Add FAQ about Vote Selling

--- src/faq/faq.ts
+++ src/faq/faq.ts
@@ -23,7 +23,7 @@ This can all take place well ahead of the election, entirely eliminating concern
 
 SIV also makes it much more practical to add additional authentication requirements, such as allowing voters to provide an email address or phone number ahead of time as an extra authentication factor.
 
-SIV can also make it easy to limit Vote Token distribution to specific IP address geolocations. Election Administrators can draw a geographic fence, and restrict Vote Token downloads to devices within the jurisdiction.
+SIV can also enable limiting Vote Token distribution to specific IP address geolocations. Election Administrators can draw a geographic fence, and restrict Vote Token downloads to devices within the jurisdiction.
 
 At the end of the day, the authentication process is up to Election Administrators.
 
@@ -49,12 +49,27 @@ But some nice things to consider:
 
 2. Each step of the process can be inspected & verified, including by separate hardware. So you can quickly use a friend's phone to confirm your vote was encrypted correctly, or submitted correctly.
 
-3. In case there is a problem, you can submit a request to the Election Admin to invalidate that vote token. We hope this option will only be necessary in rare cases, but it's always available.
+3. If there is a problem, you can submit a request to the Election Admin to invalidate that vote token. We hope this option will only be necessary in rare cases, but it's always available.
 
-Stepping back, we agree that malware is a serious concern. Not just for voting, but for everything in our increasingly digital world. And yet, for comparison, many people shop, manage their bank accounts, and pay taxes online. Some people have even chosen to entrust over $300bn to cryptocurrency algorithms, secured by nothing more than a secret integer on their (or their custodian's) device, and with no reversibility.
+Stepping back, we agree that malware is a serious concern. Not just for voting, but for everything in our increasingly digital world. And yet, for comparison, many people use credit cards, manage their bank accounts, and pay taxes online. Some people have even chosen to entrust over $300bn to cryptocurrency algorithms, secured by nothing more than a secret integer on their (or their custodian's) device, and with no reversibility.
 
-If some people prefer internet money, it's not hard to imagine many would likewise prefer internet voting.
+If some people clearly prefer doing things online, it's not hard to imagine many would likewise prefer internet voting.
 
 We're not looking to force SIV on anyone, just to offer it as an additional option. 🙂`,
+  },
+
+  {
+    q: 'Does SIV enable Vote Selling or Voter Coercion?',
+    resp: `SIV does not have built-in technical guards against voters intentionally transferring their vote.
+
+But it's still a crime. It would be hard to hide attempts to purchase votes at any significant scale.
+
+Both sides of the transaction would be committing a felony, punishable with up to $50k in fines and 2 years jail time, per vote. How much would a market pay for individual votes, anyway? Would it really be worth it for such massive downside risk if caught?
+
+Unfortunately, Vote by Mail is also easily vulnerable to Voter Coercion. And even In-Person Voting too, now that voters bring smartphones into the voting booth with them. The camera can discreetly record proof of how one voted.
+
+Although preventing coercion is important to maintain free & fair elections, the lack of widespread evidence of it in the US, despite current feasibility, suggests we shouldn't worry about it for now, so long as it remains only theoretical.
+
+Rather, let's recognize that SIV can greatly _strengthen_ election security, by offering very real benefits including multi-factor authentication, provable privacy, and end-to-end verifiability.`,
   },
 ]
