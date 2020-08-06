7ca4a77b2c03c3c834434df040563e6d5dc2b1d7
David Ernst <git@dsernst.com>
2020-08-06 16:39:08 -0700

    Remove unused mdx dep

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module '*.mdx' {
-  const MDXComponent: import('react').ComponentType
-  export default MDXComponent
-}
--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMDX = require('@next/mdx')()
-module.exports = withMDX({
+module.exports = {
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // ignoreBuildErrors: true,
   },
-})
+}
