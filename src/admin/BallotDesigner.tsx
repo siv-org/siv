d2838a8392457e24afc4a8060cc7d38ccf85dbb7
Henry Wong <henryw4k@gmail.com>
2020-08-10 15:47:24 -0700

    Add ballot designer to admin page

---
+++ src/admin/BallotDesigner.tsx
@@ -0,0 +1,28 @@
+export const BallotDesigner = () => (
+  <>
+    <p>Enter ballot questions: </p>
+    <textarea disabled>
+      {`[
+  {
+    question: 'What is the best flavor of ice-cream?',
+    choices: [
+      'Chocolate',
+      'Cookie Dough',
+      'Mint',
+      'Strawberry',
+      'Vanilla',
+    ],
+    allow_write_in: true,
+  }
+]`}
+    </textarea>
+
+    <style jsx>{`
+      textarea {
+        width: 100%;
+        height: 220px;
+        padding: 8px;
+      }
+    `}</style>
+  </>
+)
