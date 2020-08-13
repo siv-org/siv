14df24b4ccbab963c370e7e97f4010523a639c55
David Ernst <git@dsernst.com>
2020-08-13 04:44:24 -0700

    Use GlobalCSS on ProtocolPage

--- src/protocol/ProtocolPage.tsx
+++ src/protocol/ProtocolPage.tsx
@@ -1,3 +1,4 @@
+import { GlobalCSS } from '../GlobalCSS'
 import { Head } from '../Head'
 import { Content } from './Content'
 import { ScrollContextProvider } from './ScrollContext'
@@ -25,28 +26,10 @@ export const ProtocolPage = (): JSX.Element => (
       }
     `}</style>
 
+    <GlobalCSS />
     <style global jsx>{`
       body {
-        color: #222;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
-          Droid Sans, Helvetica Neue, sans-serif;
-        font-size: 0.875rem;
-        letter-spacing: 0.01071em;
-        line-height: 1.43;
-
-        max-width: 100%;
         overflow: hidden;
-      }
-
-      a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      a:hover,
-      a:focus,
-      a:active {
-        text-decoration: underline;
       }
     `}</style>
   </>
