66f2b7ce55dad50eaed27a650eee4057f3110be9
David Ernst <git@dsernst.com>
2020-08-13 04:39:51 -0700

    Use GlobalCSS on ElectionStatusPage

--- src/status/ElectionStatusPage.tsx
+++ src/status/ElectionStatusPage.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 
+import { GlobalCSS } from '../GlobalCSS'
 import { Head } from '../Head'
 
 export const ElectionStatusPage = (): JSX.Element => {
@@ -22,30 +23,7 @@ export const ElectionStatusPage = (): JSX.Element => {
           padding: 1rem;
         }
       `}</style>
-
-      <style global jsx>{`
-        body {
-          color: #222;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
-            Droid Sans, Helvetica Neue, sans-serif;
-          font-size: 0.875rem;
-          letter-spacing: 0.01071em;
-          line-height: 1.43;
-
-          max-width: 100%;
-        }
-
-        a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        a:hover,
-        a:focus,
-        a:active {
-          text-decoration: underline;
-        }
-      `}</style>
+      <GlobalCSS />
     </>
   )
 }
