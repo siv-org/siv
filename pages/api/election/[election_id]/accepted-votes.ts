6da9f1c5dcbbc35bded71c2d3450719cc91c3235
David Ernst <git@dsernst.com>
2020-08-14 13:51:14 -0700

    Add api/election/[election_id]/accepted-votes endpoint

---
+++ pages/api/election/[election_id]/accepted-votes.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { firebase } from '../../_services'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { election_id } = req.query
+
+  const election = firebase
+    .firestore()
+    .collection('elections')
+    .doc(election_id as string)
+
+  // Is election_id in DB?
+  if (!(await election.get()).exists) {
+    return res.status(400).end('Unknown Election ID.')
+  }
+
+  // Grab public votes fields
+  const votes = (await election.collection('votes').get()).docs.map((doc) => {
+    const data = doc.data()
+    return { auth: data.auth, best_icecream: data.encryptedString }
+  })
+
+  res.status(200).json(votes)
+}
