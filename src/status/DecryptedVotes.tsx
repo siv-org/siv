18303b08e5939adf2302ea563d14c65a6c1ffbbe
Henry Wong <henryw4k@gmail.com>
2020-08-15 16:35:22 -0700

    Tally up vote totals

--- src/status/DecryptedVotes.tsx
+++ src/status/DecryptedVotes.tsx
@@ -1,3 +1,4 @@
+import { countBy, mapValues, orderBy } from 'lodash-es'
 import ms from 'ms'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
@@ -14,12 +15,26 @@ export const DecryptedVotes = (): JSX.Element => {
 
   if (!data) return <></>
 
+  // Tally vote totals
+  const vote_counts = countBy(data.map((v: { best_icecream: string }) => v.best_icecream))
+  const tuples = mapValues(vote_counts, (votes, name) => ({ name, votes }))
+  const ordered = orderBy(tuples, 'votes', 'desc')
+
   return (
     <div>
       <h3>Decrypted Votes</h3>
       <ol>
         {data.map((vote: NodeJS.Dict<string>, index: number) => (
           <li key={index}>{JSON.stringify(vote)}</li>
+        ))}
+      </ol>
+      <br />
+      <b>Final results:</b>
+      <ol>
+        {ordered.map(({ name, votes }) => (
+          <li key={name}>
+            {name}: {votes}
+          </li>
         ))}
       </ol>
     </div>
