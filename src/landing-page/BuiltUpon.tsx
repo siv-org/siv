b0490d136ba63bfea77916246f23af2d2b354ae3
David Ernst <git@dsernst.com>
2020-11-03 14:10:10 -0800

    Add BuiltUpon link to /about

--- src/landing-page/BuiltUpon.tsx
+++ src/landing-page/BuiltUpon.tsx
@@ -1,15 +1,32 @@
+import Link from 'next/link'
+
 import { JumboBlue } from './JumboBlue'
 
 export const BuiltUpon = (): JSX.Element => (
   <JumboBlue>
     <h2>
       Built upon decades of research and hard work by uncountable contributors in academia, industry, and the public
-      sector.
+      sector.{' '}
+      <Link href="/about">
+        <a>Learn more</a>
+      </Link>
     </h2>
 
     <style jsx>{`
       p {
         font-size: calc(0.72vw + 1rem);
+      }
+
+      a {
+        font-size: 17px;
+        font-style: italic;
+        color: white;
+        font-weight: 400;
+        text-decoration: none;
+      }
+
+      a:hover {
+        text-decoration: underline;
       }
 
       @media (min-width: 750px) {
