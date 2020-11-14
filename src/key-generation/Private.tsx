61397004a0ab09944d81ea39c4be14bf7faa02c8
David Ernst <git@dsernst.com>
2020-11-14 09:53:12 -0800

    /keygen: Tweak look of Private boxes

--- src/key-generation/Private.tsx
+++ src/key-generation/Private.tsx
@@ -1,17 +1,31 @@
 export const Private = ({ children }: { children: string | JSX.Element | JSX.Element[] }) => (
   <div>
-    <p>~~~~~~~ BEGIN PRIVATE ~~~~~~~</p>
+    <p>BEGIN PRIVATE</p>
     {children}
-    <p>~~~~~~~~ END PRIVATE ~~~~~~~~</p>
+    <p>END PRIVATE</p>
     <style jsx>{`
       p {
-        color: purple;
+        color: rgba(128, 0, 128, 0.7);
         margin: 0;
+        position: absolute;
+        background: white;
+        font-size: 12px;
+        padding: 0 3px;
+      }
+
+      p:first-child {
+        top: -10px;
+      }
+
+      p:last-child {
+        bottom: -8px;
       }
 
       div {
-        border: 2px dashed purple;
-        padding: 1rem;
+        border: 2px dashed rgba(128, 0, 128, 0.7);
+        padding: 0.5rem 1rem;
+        position: relative;
+        margin-bottom: 30px;
       }
     `}</style>
   </div>
