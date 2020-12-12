9be42e77c4d834d745c5cb79658c2b39ee2b4c78
David Ernst <git@dsernst.com>
2020-12-11 21:45:10 -0800

    /keygen: Cleanup useKeyGenState.ts

--- src/key-generation/useKeyGenState.ts
+++ src/key-generation/useKeyGenState.ts
@@ -1,16 +1,16 @@
 import { merge } from 'lodash-es'
 
-// import pickRandomInteger from '../crypto/pick-random-integer'
-// import { big } from '../crypto/types'
 import { useLocalStorageReducer } from '../vote/useLocalStorage'
 
 // Define our types
-type Map = Record<string, string>
+type Map = Record<string, unknown>
 export type State = {
   election_id: string
   trustee_auth: string
   trustees: { email: string; you?: boolean }[]
 }
+// Typescript helper w/ State and Dispatch
+export type StateAndDispatch = { dispatch: ReturnType<typeof useLocalStorageReducer>[1]; state: State }
 
 // Core state logic
 function reducer(prev: State, payload: Map) {
@@ -23,5 +23,3 @@ function reducer(prev: State, payload: Map) {
 // Export consumable hook that returns [state, dispatch]
 export const useKeyGenState = ({ election_id, trustee_auth }: { election_id: string; trustee_auth: string }) =>
   useLocalStorageReducer(`keygen-${election_id}-${trustee_auth}`, reducer, { election_id, trustee_auth })
-
-export type StateAndDispatch = { dispatch: ReturnType<typeof useLocalStorageReducer>[1]; state: State }
