22f2fb5995ba5863baaf91974ef927c7e1b1d6af
David Ernst <git@dsernst.com>
2020-12-25 00:43:12 -0800

    keygen: Calculate partial decryption

--- src/key-generation/11-PartialDecryptionTest.tsx
+++ src/key-generation/11-PartialDecryptionTest.tsx
@@ -1,12 +1,27 @@
+import { useEffect } from 'react'
+
+import { partial_decrypt } from '../crypto/threshold-keygen'
 import { big } from '../crypto/types'
 import { EncryptionNote } from './EncryptionNote'
-import { StateAndDispatch } from './keygen-state'
+import { StateAndDispatch, getParameters } from './keygen-state'
+import { PrivateBox } from './PrivateBox'
 
-export const PartialDecryptionTest = ({ state }: StateAndDispatch) => {
-  const { parameters, threshold_public_key } = state
+export const PartialDecryptionTest = ({ dispatch, state }: StateAndDispatch) => {
+  const { parameters, partial_decryption: partial, private_keyshare, threshold_public_key } = state
 
-  const plaintext = '1337'
-  const randomizer = '100'
+  const plaintext = '2020'
+  const randomizer = '108'
+
+  useEffect(() => {
+    // Don't start until we have the threshold key
+    if (!threshold_public_key || !parameters || !private_keyshare) return
+
+    const unlock = big(parameters.g).modPow(big(randomizer), big(parameters.p))
+
+    const partial_decryption = partial_decrypt(unlock, big(private_keyshare), getParameters(state)).toString()
+
+    dispatch({ partial_decryption })
+  }, [threshold_public_key])
 
   if (!threshold_public_key || !parameters) {
     return <></>
@@ -14,7 +29,7 @@ export const PartialDecryptionTest = ({ state }: StateAndDispatch) => {
 
   const p = big(parameters.p)
 
-  const encrypted = big(threshold_public_key).modPow(big(randomizer), p)
+  const encrypted = big(threshold_public_key).modPow(big(randomizer), p).multiply(big(plaintext)).mod(p)
   const unlock = big(parameters.g).modPow(big(randomizer), big(parameters.p))
 
   return (
@@ -30,12 +45,22 @@ export const PartialDecryptionTest = ({ state }: StateAndDispatch) => {
       <EncryptionNote />
       <ul>
         <li>
-          encrypted = {plaintext} * ({threshold_public_key} ^ {randomizer}) % {parameters.p} = {encrypted.toString()}
+          encrypted = {plaintext} * ({threshold_public_key} ^ {randomizer}) % {parameters.p} ≡ {encrypted.toString()}
         </li>
         <li>
-          unlock = ({parameters.g} ^ {randomizer}) % {parameters.p} = {unlock.toString()}
+          unlock = ({parameters.g} ^ {randomizer}) % {parameters.p} ≡ {unlock.toString()}
         </li>
       </ul>
+      <p>
+        Now each trustee can contribute their partial decryption, using their secret keyshare:
+        <br />
+        <i>partial_decryption = unlock ^ trustees_keyshare % p</i>
+      </p>
+      <PrivateBox>
+        <p>
+          partial = {unlock.toString()} ^ {private_keyshare} % {parameters.p} ≡ {partial || '[pending...]'}
+        </p>
+      </PrivateBox>
     </>
   )
 }
--- src/key-generation/keygen-state.ts
+++ src/key-generation/keygen-state.ts
@@ -24,6 +24,7 @@ export type State = {
   pairwise_randomizers_for?: Record<string, string>
   pairwise_shares_for?: Record<string, string>
   parameters?: { g: string; p: string; q: string; t: number }
+  partial_decryption?: string
   personal_key_pair?: { decryption_key: string; public_key: { recipient: string } }
   private_coefficients?: string[]
   private_keyshare?: string
