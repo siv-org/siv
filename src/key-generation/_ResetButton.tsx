f990baa831f27ada8c54e2cfc56b9cb67960f094
David Ernst <git@dsernst.com>
2020-12-28 13:03:00 -0800

    Make ResetButton less obvious

--- src/key-generation/_ResetButton.tsx
+++ src/key-generation/_ResetButton.tsx
@@ -19,20 +19,22 @@ export function ResetButton({ state }: { state: State }) {
       <style jsx>
         {`
           div {
-            border: 1px solid #999;
+            backdrop-filter: blur(10px);
+            border: 1px solid rgba(0, 0, 0, 0.3);
             border-radius: 4px;
-            padding: 2px 10px;
+            box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
             display: inline-block;
+            font-size: 12px;
+            opacity: 0.3;
+            padding: 2px 6px;
             position: fixed;
             right: 10px;
             top: 15px;
-            background-color: white;
             z-index: 10;
-            box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
-            opacity: 0.5;
           }
 
           div:hover {
+            background-color: white;
             cursor: pointer;
             opacity: 1;
           }
