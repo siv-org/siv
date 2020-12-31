257958ad01be956260620b5f5b875cc861f76457
David Ernst <git@dsernst.com>
2020-12-30 17:12:46 -0800

    Parallelize POST api/invite-voters

--- pages/api/invite-voters.ts
+++ pages/api/invite-voters.ts
@@ -5,43 +5,51 @@ import { firebase, pushover, sendEmail } from './_services'
 const { ADMIN_PASSWORD } = process.env
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // 1. Check for password
   const { ballot_design, election_id, password, voters } = req.body
-  if (password !== ADMIN_PASSWORD) {
-    return res.status(401).end('Invalid Password.')
-  }
+
+  // 1. Check for password
+  if (password !== ADMIN_PASSWORD) return res.status(401).send('Invalid Password.')
 
   // 2. Generate auth token for each voter
   const auth_tokens = voters.map(() => generateAuthToken())
 
+  // This will hold all our async tasks
+  const promises: Promise<unknown>[] = []
+
   // 3. Store auth tokens in db
   const election = firebase.firestore().collection('elections').doc(election_id)
-  await election.set({ ballot_design })
-  await Promise.all(
-    voters.map((voter: string, index: number) =>
-      election.collection('voters').doc(voter).set({ auth_token: auth_tokens[index], email: voter, index }),
+  promises.push(election.set({ ballot_design }))
+  promises.push(
+    Promise.all(
+      voters.map((voter: string, index: number) =>
+        election.collection('voters').doc(voter).set({ auth_token: auth_tokens[index], email: voter, index }),
+      ),
     ),
   )
 
   // 4. Email each voter their auth token
-  await Promise.all(
-    voters.map((voter: string, index: number) => {
-      const link = `${req.headers.origin}/election/${election_id}/vote?auth=${auth_tokens[index]}`
+  promises.push(
+    Promise.all(
+      voters.map((voter: string, index: number) => {
+        const link = `${req.headers.origin}/election/${election_id}/vote?auth=${auth_tokens[index]}`
 
-      return sendEmail({
-        recipient: voter,
-        subject: 'Vote Invitation',
-        text: `<h2 style="margin: 0">Vote Invitation</h2>
+        return sendEmail({
+          recipient: voter,
+          subject: 'Vote Invitation',
+          text: `<h2 style="margin: 0">Vote Invitation</h2>
 Click here to securely cast your vote:
 <a href="${link}">${link}</a>
 
 <em style="font-size:13px; opacity: 0.6;">This link is unique for you. Don't share it with anyone.</em>`,
-      })
-    }),
+        })
+      }),
+    ),
   )
 
   // 5. Send Admin push notification
-  pushover(`Invited ${voters.length} voters`, voters.join(', '))
+  promises.push(pushover(`Invited ${voters.length} voters`, voters.join(', ')))
+
+  await Promise.all(promises)
 
   return res.status(201).end(election_id)
 }
