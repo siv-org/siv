2728c2275c424228275a31d95d110d16b97a6c7b
David Ernst <git@dsernst.com>
2021-01-02 16:39:52 -0800

    /status: Push instead of poll for AcceptedVotes updates

--- src/status/AcceptedVotes.tsx
+++ src/status/AcceptedVotes.tsx
@@ -1,29 +1,37 @@
-import ms from 'ms'
 import { useRouter } from 'next/router'
-import useSWR from 'swr'
+import Pusher from 'pusher-js'
+import { useEffect, useState } from 'react'
 
 import { Cipher_Text } from '../crypto/types'
-
-const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 type Vote = { auth: string } & { [index: string]: Cipher_Text }
 
 export const AcceptedVotes = (): JSX.Element => {
   const { election_id } = useRouter().query
 
-  // Grab votes from api
-  const { data, error } = useSWR(election_id ? `/api/election/${election_id}/accepted-votes` : null, fetcher, {
-    refreshInterval: ms('10s'),
-  })
+  const [votes, setVotes] = useState<Vote[]>()
 
-  if (error) return <div>Error loading Accepted Votes</div>
-  if (!data) return <div>Loading...</div>
+  const loadVotes = () =>
+    election_id &&
+    fetch(`/api/election/${election_id}/accepted-votes`)
+      .then((res) => res.json())
+      .then(setVotes)
+
+  // Load votes when election_id is first set
+  useEffect(() => {
+    loadVotes()
+  }, [election_id])
+
+  // Subscribe to pusher updates of new votes
+  subscribeToUpdates(loadVotes, election_id)
+
+  if (!votes) return <div>Loading...</div>
 
   return (
     <div>
       <h3>All Accepted Votes</h3>
       <ol>
-        {data.map((vote: Vote, index: number) => (
+        {votes.map((vote: Vote, index: number) => (
           <li key={index}>{stringifyEncryptedVote(vote)}</li>
         ))}
       </ol>
@@ -42,3 +50,31 @@ export const stringifyEncryptedVote = (vote: Vote) =>
       key === 'auth' ? '' : `, ${key}: { encrypted: '${vote[key].encrypted}', unlock: '${vote[key].unlock}' }`,
     )
     .join('')} }`
+
+function subscribeToUpdates(loadVotes: () => void, election_id?: string | string[]) {
+  function subscribe() {
+    // Enable pusher logging - don't include this in production
+    // Pusher.logToConsole = true
+
+    const pusher = new Pusher('9718ba0612df1a49e52b', { cluster: 'us3' })
+
+    const channel = pusher.subscribe(`create-${election_id}`)
+
+    channel.bind(`votes`, () => {
+      console.log('🆕 Pusher new vote submitted')
+      loadVotes()
+    })
+
+    // Return cleanup code
+    return () => {
+      channel.unbind()
+    }
+  }
+
+  // Subscribe when we get election_id
+  useEffect(() => {
+    if (election_id) {
+      return subscribe()
+    }
+  }, [election_id])
+}
