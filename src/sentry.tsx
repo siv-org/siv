7ff2b351924f23dbb610ebd2106a3a7ff9616a1d
David Ernst <git@dsernst.com>
2021-01-25 20:57:05 -0800

    Disable sentry during development

--- src/sentry.tsx
+++ src/sentry.tsx
@@ -7,6 +7,7 @@ export function useSentry() {
   useEffect(() => {
     Sentry.init({
       dsn: 'https://ec794d9df6c0425586d9f069775a8ad7@o510908.ingest.sentry.io/5607249',
+      enabled: window.location.hostname !== 'localhost', // Disable during development
       integrations: [new Integrations.BrowserTracing()],
       tracesSampleRate: 1.0,
     })
