79e272e9842a5004140b38af95e208c077171e1f
David Ernst <git@dsernst.com>
2021-01-27 17:24:13 -0800

    new-admin: Display ExistingVoters in a table

--- src/admin/Voters/ExistingVoters.tsx
+++ src/admin/Voters/ExistingVoters.tsx
@@ -3,12 +3,42 @@ import { use_stored_info } from '../load-existing'
 export const ExistingVoters = () => {
   const { voters } = use_stored_info()
   return (
-    <>
-      {voters?.map(([email, has_voted], index) => (
-        <p key={email}>
-          {index + 1}. {email} {has_voted ? '— Voted' : ''}
-        </p>
-      ))}
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>email</th>
+          <th>voted</th>
+        </tr>
+      </thead>
+      <tbody>
+        {voters?.map(([email, has_voted], index) => (
+          <tr key={email}>
+            <td>{index + 1}</td>
+            <td>{email}</td>
+            <td>{has_voted}</td>
+          </tr>
+        ))}
+      </tbody>
+      <style jsx>{`
+        table {
+          border-collapse: collapse;
+          display: block;
+          overflow: scroll;
+        }
+
+        th,
+        td {
+          border: 1px solid #ccc;
+          padding: 3px 10px;
+          margin: 0;
+        }
+
+        th {
+          background: #f9f9f9;
+          font-size: 11px;
+        }
+      `}</style>
+    </table>
   )
 }
