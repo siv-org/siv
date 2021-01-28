4370d4620dd2e2a19b14f9d3d726b24efb298e19
David Ernst <git@dsernst.com>
2021-01-28 00:20:32 -0800

    new-admin: Link ElectionID to Status page

--- src/admin/ElectionID.tsx
+++ src/admin/ElectionID.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+
 export const ElectionID = () => {
   const election_id = useElectionID()
 
@@ -6,7 +8,10 @@ export const ElectionID = () => {
 
   return (
     <div>
-      Election ID: {election_id}
+      Election ID:{' '}
+      <Link href={`/election/${election_id}`}>
+        <a target="_blank">{election_id}</a>
+      </Link>
       <style jsx>{`
         div {
           float: right;
@@ -14,6 +19,11 @@ export const ElectionID = () => {
 
           position: relative;
           bottom: 37px;
+        }
+
+        a {
+          font-weight: bold;
+          color: black;
         }
       `}</style>
     </div>
