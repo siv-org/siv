44e94986f89137ca57eafe17ef868add3d28f87c
David Ernst <git@dsernst.com>
2021-02-08 13:30:59 -0800

    Update create-sample-votes script for WSACA demo

--- pages/api/create-sample-votes.ts
+++ pages/api/create-sample-votes.ts
@@ -14,12 +14,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Disable this endpoint
   return res.status(401).end()
 
-  const election_id = '1610777062403'
+  const election_id = '1612818814403'
   const p = '84490233071588324613543045838826431628034872330024413446004719838344478256747'
-  const threshold_key = '9634851030654637484180832422863293041136054844586992867984961075284329440070'
+  const threshold_key = '25753591117431663234613254388754657393582184952307187183880669779500171305735'
   const pub_key = bigPubKey({ generator: '4', modulo: p, recipient: threshold_key })
 
-  const num_samples = 500
+  const num_samples = 100
 
   const election = firebase.firestore().collection('elections').doc(election_id)
 
@@ -33,9 +33,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         }`
 
       const plaintext = {
-        parks_1: randomVote(0),
-        parks_2: randomVote(0),
-        president: randomVote(1),
+        favorite_apple: randomVote(0),
       }
 
       const encoded = mapValues(plaintext, encode)
@@ -59,26 +57,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 const ballot_schema = [
   {
-    id: 'parks',
-    multiple_votes_allowed: 2,
-    options: [
-      { name: 'Zion' },
-      { name: 'Arches' },
-      { name: 'Bryce Canyon' },
-      { name: 'Canyonlands' },
-      { name: 'Capitol Reef' },
-    ],
-    title: 'Which are your two favorite national parks in Utah?',
-  },
-  {
-    id: 'president',
-    options: [
-      { name: 'Dwight D. Eisenhower' },
-      { name: 'Richard Nixon' },
-      { name: 'Gerald Ford' },
-      { name: 'Ronald Reagan' },
-      { name: 'George H.W. Bush' },
-    ],
-    title: 'Who was the greatest Republican President of the late 20th Century?',
+    id: 'favorite_apple',
+    options: [{ name: 'Cosmic Crisp' }, { name: 'Honey Crisp' }, { name: 'Golden Delicious' }],
+    title: 'Which is the best Washington State apple?',
+    write_in_allowed: true,
   },
 ]
