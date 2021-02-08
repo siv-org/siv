ccde908d489299d3667b64baf4360b71f9f3477b
David Ernst <git@dsernst.com>
2021-02-07 19:53:03 -0800

    admin: Tooltip for UI to accept/reject signatures

--- src/admin/Voters/Signature.tsx
+++ src/admin/Voters/Signature.tsx
@@ -1,7 +1,30 @@
+import { Tooltip } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+const useStyles = makeStyles(() => ({
+  customWidth: {
+    width: 300,
+  },
+}))
+
 export const Signature = ({ esignature }: { esignature?: string }) => {
+  const classes = useStyles()
   return (
     <td>
-      <img src={esignature} />
+      <Tooltip
+        interactive
+        className={classes.customWidth}
+        title={
+          <div className="tooltip">
+            <img src={esignature} />
+            <div className="row">
+              <a>👎 Reject</a>
+              <a>👍 Approve</a>
+            </div>
+          </div>
+        }
+      >
+        <img className="small" src={esignature} />
+      </Tooltip>
       <style jsx>{`
         td {
           border: 1px solid #ccc;
@@ -9,8 +32,26 @@ export const Signature = ({ esignature }: { esignature?: string }) => {
           margin: 0;
         }
 
-        img {
+        td:hover {
+          cursor: pointer;
+          background-color: #f2f2f2;
+        }
+
+        img.small {
           max-width: 100px;
+        }
+
+        .tooltip img {
+          max-width: 280px;
+        }
+
+        .row {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        .tooltip a {
+          cursor: pointer;
         }
       `}</style>
     </td>
