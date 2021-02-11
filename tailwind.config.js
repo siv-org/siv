e01e96f3de619ea89d0fe687b84d47ca01a2f204
David Ernst <git@dsernst.com>
2021-02-10 16:52:25 -0800

    Lint tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  darkMode: false,
+  plugins: [],
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
 }
