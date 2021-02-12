2f97265f09efffd553c3239b0315aad594ff0a96
David Ernst <git@dsernst.com>
2021-02-11 21:49:17 -0800

    Create tailwind UI for LoginPage

--- src/admin/LoginPage/LoginPage.tsx
+++ src/admin/LoginPage/LoginPage.tsx
@@ -1,3 +1,55 @@
+import 'tailwindcss/tailwind.css'
+
 export const LoginPage = () => {
-  return <p>Hello world, I am LoginScreen.</p>
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+
+        <form action="#" className="mt-8 space-y-6" method="POST">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <label className="sr-only" htmlFor="email-address">
+              Email address
+            </label>
+            <input
+              required
+              autoComplete="email"
+              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
+              id="email-address"
+              name="email"
+              placeholder="Email address"
+              type="email"
+            />
+          </div>
+
+          <p className="text-xs text-gray-400 italic">You will be emailed a magic link.</p>
+
+          <div>
+            <button
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              type="submit"
+            >
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                {/* Heroicon name: solid/lock-closed */}
+                <svg
+                  aria-hidden="true"
+                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </span>
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  )
 }
