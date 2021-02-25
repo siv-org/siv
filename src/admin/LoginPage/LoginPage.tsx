38ae51b13db6c0511e85a9cd5ab142b3303257b8
David Ernst <git@dsernst.com>
2021-02-24 16:00:22 -0800

    Sort tailwind classNames w/ Headwind VS Code extension

--- src/admin/LoginPage/LoginPage.tsx
+++ src/admin/LoginPage/LoginPage.tsx
@@ -12,19 +12,19 @@ export const LoginPage = () => {
   const [email, setEmail] = useState('')
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+    <main className="flex items-center justify-center min-h-screen px-4 py-12 bg-gray-50 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">Sign in to your account</h2>
 
         <div className="mt-8 space-y-6">
-          <div className="rounded-md shadow-sm -space-y-px">
+          <div className="-space-y-px rounded-md shadow-sm">
             <label className="sr-only" htmlFor="email-address">
               Email address
             </label>
             <input
               required
               autoComplete="email"
-              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
+              className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
               id="email-address"
               name="email"
               placeholder="Email address"
@@ -34,22 +34,22 @@ export const LoginPage = () => {
             />
           </div>
 
-          <p className="text-xs text-gray-400 italic">You will be emailed a login link.</p>
+          <p className="text-xs italic text-gray-400">You will be emailed a login link.</p>
 
           <div>
             <button
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={async () => {
                 const response = await supabase.auth.signIn({ email })
 
                 console.log('response', response)
               }}
             >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 {/* Heroicon name: solid/lock-closed */}
                 <svg
                   aria-hidden="true"
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  className="w-5 h-5 text-indigo-500 group-hover:text-indigo-400"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
