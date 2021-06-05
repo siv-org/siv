81327d7576cb0357b2dc5d33b9fe28a980b846d7
David Ernst <git@dsernst.com>
2021-06-05 11:55:26 -0700

    Add 'secure' to /login page header text

--- src/admin/LoginPage/LoginPage.tsx
+++ src/admin/LoginPage/LoginPage.tsx
@@ -11,7 +11,7 @@ export const LoginPage = () => {
   return (
     <main className="flex items-center justify-center min-h-screen px-4 py-12 bg-gray-50 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
-        <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">Sign in to your account</h2>
+        <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">Sign in to your secure account</h2>
 
         <div className="mt-8 space-y-6">
           <div className="-space-y-px rounded-md shadow-sm">
