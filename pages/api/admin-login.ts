cb08d1c8f0369e22b214f1d79ad1deccaf76a6cc
David Ernst <git@dsernst.com>
2021-06-05 14:15:40 -0700

    admin-login: Send email w/ unique admin-login auth code

--- pages/api/admin-login.ts
+++ pages/api/admin-login.ts
@@ -1,7 +1,8 @@
 import { firestore } from 'firebase-admin'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { firebase } from './_services'
+import { firebase, sendEmail } from './_services'
+import { generateAuthToken } from './invite-voters'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email }: { email: string } = req.body
@@ -11,10 +12,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (!email.includes('@') || !email.includes('.')) return res.status(400).send('Malformed')
 
   // Is this email an approved election manager?
-  const adminDoc = await firebase.firestore().collection('admins').doc(email).get()
+  const adminDoc = await firebase.firestore().collection('admins').doc(email)
 
   // Store 'failed-logins' in db
-  if (!adminDoc.exists) {
+  if (!(await adminDoc.get()).exists) {
     await firebase
       .firestore()
       .collection('failed-logins')
@@ -23,7 +24,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(404).send(`'${email}' is not approved election manager`)
   }
 
-  // TODO: Otherwise trigger their login email
+  // Otherwise trigger their login email...
+
+  // First we'll make & store a login auth token for them.
+  const auth_token = generateAuthToken()
+  adminDoc.collection('logins').doc(new Date().toISOString()).set({ auth_token, created_at: new Date() })
+
+  const link = `${req.headers.origin}/admin?auth=${auth_token}`
+  sendEmail({
+    recipient: email,
+    subject: 'SIV Admin Login',
+    text: `A request was made to access your SIV Admin Dashboard.
+<h3>Click here to login: <a href="${link}">${link}</a></h3>
+
+<em style="font-size:10px; opacity: 0.6;">If you did not authorize this request, please reply to let us know.</em>`,
+  })
 
   res.status(200).send('Success')
 }
