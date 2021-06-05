cc57fc6bbefb6f2462219a03b72b85345fd562d0
David Ernst <git@dsernst.com>
2021-06-05 13:04:50 -0700

    Store failed admin logins in DB

--- pages/api/admin-login.ts
+++ pages/api/admin-login.ts
@@ -1,6 +1,7 @@
+import { firestore } from 'firebase-admin'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-// import { firebase, pushover, sendEmail } from './_services'
+import { firebase } from './_services'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email }: { email: string } = req.body
@@ -8,6 +9,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Confirm they sent a valid email address
   if (!email) return res.status(400).send('Missing email')
   if (!email.includes('@') || !email.includes('.')) return res.status(400).send('Malformed')
+
+  // Is this email an approved election manager?
+  const adminDoc = await firebase.firestore().collection('admins').doc(email).get()
+
+  // Store in 'failed-logins' db if not
+  if (!adminDoc.exists) {
+    await firebase
+      .firestore()
+      .collection('failed-logins')
+      .doc(email)
+      .set({ failed_at: firestore.FieldValue.arrayUnion(new Date()) }, { merge: true })
+    return res.status(404).send(`'${email}' is not approved election manager`)
+  }
+
+  // TODO: Otherwise trigger their login email
 
   res.status(200).send('Success')
 }
