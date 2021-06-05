1409c8a01b6d084b917688543195cfb83882de7b
David Ernst <git@dsernst.com>
2021-06-05 12:03:03 -0700

    Revert supabase for newsletter signups

--- src/landing-page/EmailSignup.tsx
+++ src/landing-page/EmailSignup.tsx
@@ -1,14 +1,9 @@
 import { NoSsr, TextField } from '@material-ui/core'
-import { createClient } from '@supabase/supabase-js'
+import { firestore } from 'firebase/app'
 import { useState } from 'react'
 
 import { api } from '../api-helper'
 import { OnClickButton } from './Button'
-
-const supabaseUrl = 'https://ktoemmjtpzoqhunxvabf.supabase.co'
-const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMjg0ODgxMiwiZXhwIjoxOTI4NDI0ODEyfQ.CGcmI1V3Wwm9JdQtalhatkLNODRw9mTRJLf-m3sra_w'
-const supabase = createClient(supabaseUrl, SUPABASE_KEY)
 
 export const EmailSignup = (): JSX.Element => {
   const [saved, setSaved] = useState(false)
@@ -32,22 +27,26 @@ export const EmailSignup = (): JSX.Element => {
         <OnClickButton
           disabled={saved}
           style={{ margin: 0, padding: '8px 17px' }}
-          onClick={async () => {
-            const email = (document.getElementById('newsletter-signup-field') as HTMLInputElement).value
+          onClick={() => {
+            const fields = {
+              created_at: new Date().toString(),
+              email: (document.getElementById('newsletter-signup-field') as HTMLInputElement).value,
+            }
 
-            // Store submission in DB
-            const { data, error } = await supabase.from('newsletter').insert([{ email }])
+            // Store submission in Firestore
+            firestore()
+              .collection('news-signups')
+              .doc(new Date().toISOString() + ' ' + String(Math.random()).slice(2, 7))
+              .set(fields)
+              .then(() => {
+                setSaved(true)
 
-            console.log({ data, error })
-            if (error) throw error
-
-            setSaved(true)
-
-            // Notify via Pushover
-            api('pushover', {
-              message: email,
-              title: `SIV newsletter signup`,
-            })
+                // Notify via Pushover
+                api('pushover', {
+                  message: fields.email,
+                  title: `SIV newsletter signup`,
+                })
+              })
           }}
         >
           {saved ? 'Done!' : 'Sign Up'}
