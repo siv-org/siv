6f2d093ed66fee924e568f5132ac6a627350dd58
David Ernst <git@dsernst.com>
2021-06-08 12:37:37 -0700

    Attach admin's name in JWT

--- pages/api/admin-check-login-code.ts
+++ pages/api/admin-check-login-code.ts
@@ -10,7 +10,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   // Is this email an approved election manager?
   const adminDoc = firebase.firestore().collection('admins').doc(email)
-  if ((await adminDoc.get()).exists) {
+  const admin = await adminDoc.get()
+
+  if (admin.exists) {
     // Is this a valid auth token for them?
     const [session] = (await adminDoc.collection('logins').where('auth_token', '==', auth).get()).docs
     if (session) {
@@ -21,7 +23,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       const minutes_since = diff / 1000 / 60
       if (minutes_since < 30) {
         // Valid session
-        return res.status(200).send({ jwt: jwt.sign({ email }, JWT_SECRET) })
+        return res
+          .status(200)
+          .send({ jwt: jwt.sign({ email, name: admin?.data()?.name || 'MissingName' }, JWT_SECRET) })
       } else {
         // Expired session
         return res.status(412).send({ error: 'Expired session' })
