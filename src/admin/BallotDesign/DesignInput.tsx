5b9b36717d13fe2d83bb6bcc6d3a13626f3317ae
David Ernst <git@dsernst.com>
2021-06-29 12:39:42 -0700

    admin: Transition to Voters tab after saving ballot_design

--- src/admin/BallotDesign/DesignInput.tsx
+++ src/admin/BallotDesign/DesignInput.tsx
@@ -1,3 +1,4 @@
+import router from 'next/router'
 import { useState } from 'react'
 
 import { api } from '../../api-helper'
@@ -55,6 +56,7 @@ export const DesignInput = () => {
 
           if (response.status === 201) {
             revalidate(election_id)
+            router.push(`${window.location.origin}/admin/${election_id}/voters`)
           } else {
             throw await response.json()
           }
