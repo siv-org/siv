a240b83505ce7d8792084ebd197f870b4f4cec1e
David Ernst <git@dsernst.com>
2021-06-29 22:38:44 -0700

    admin: Make MobileMenu drawer swipeable to close

--- src/admin/MobileMenu.tsx
+++ src/admin/MobileMenu.tsx
@@ -1,4 +1,4 @@
-import Drawer from '@material-ui/core/Drawer'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import { startCase } from 'lodash-es'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
@@ -8,18 +8,27 @@ import { SidebarContent, steps } from './Sidebar'
 
 export const MobileMenu = () => {
   const { section } = useRouter().query
-  const [menu_open, set_menu] = useState(true)
+  const [menu_open, set_menu] = useState(false)
 
   const name = startCase(section as string)
+
+  const iOS = process?.browser && /iPad|iPhone|iPod/.test(navigator.userAgent)
 
   return (
     <div className="mobile-menu">
       <OnClickButton style={{ marginLeft: 0, padding: '5px 11px' }} onClick={() => set_menu(true)}>
         {section ? `Step ${steps.indexOf(name as typeof steps[number]) + 1}: ${name}` : 'Menu'}
       </OnClickButton>
-      <Drawer anchor="left" open={menu_open} onClose={() => set_menu(false)}>
+      <SwipeableDrawer
+        anchor="left"
+        disableBackdropTransition={!iOS}
+        disableDiscovery={iOS}
+        open={menu_open}
+        onClose={() => set_menu(false)}
+        onOpen={() => set_menu(true)}
+      >
         <SidebarContent closeMenu={() => set_menu(false)} />
-      </Drawer>
+      </SwipeableDrawer>
       <style jsx>{`
         /* Hide for all but small screens */
         @media (min-width: 500px) {
