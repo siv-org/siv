b224ba23c7cc5d0c80f30ab72000c24021b5ea6f
David Ernst <git@dsernst.com>
2021-07-28 11:44:28 -0700

    Reorder PointAndClick TODOs

--- src/admin/BallotDesign/PointAndClick.tsx
+++ src/admin/BallotDesign/PointAndClick.tsx
@@ -8,21 +8,22 @@ export const PointAndClick = ({ design }: { design: string; setDesign: (s: strin
 
     - [x] See current design
 
-    - [ ] Add new items
-    - [ ] Set item ID
     - [ ] Edit item title
+    - [ ] Edit options name
+    - [ ] Delete existing options
+    - [ ] Toggle 'Write in' allowed
+    - [ ] Create new options
+    - [ ] Add new items
+    - [ ] Delete items
+
+    - [ ] Re-order items
+    - [ ] Set item ID
     - [ ] Edit item description
     - [ ] Edit item final question ("Should this bill be")
-    - [ ] Re-order items
-    - [ ] Delete items
-    
-    - [ ] Create new options
-    - [ ] Delete existing options
     - [ ] Reorder existing options
-    - [ ] Edit options name
-        - [ ] Edit option's subline (e.g. Party affiliation)
-        - [ ] Edit option's short_id (if too long)
-    - [ ] Toggle 'Write in' allowed
+    - [ ] Edit option's subline (e.g. Party affiliation)
+    - [ ] Edit option's short_id (if too long)
+
 */
   const [json, setJson] = useState<Item[]>()
 
