07a59a57a52aa5e4197493f6bd77780eb21ae4a5
David Ernst <git@dsernst.com>
2021-08-04 02:34:50 -0700

    BallotDesign: Add api/finalize-ballot-design endpoint

---
+++ pages/api/election/[election_id]/admin/finalize-ballot-design.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { firebase } from '../../../_services'
+import { checkJwtOwnsElection } from '../../../validate-admin-jwt'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { election_id } = req.query as { election_id: string }
+
+  // Confirm they're a valid admin that created this election
+  const jwt = await checkJwtOwnsElection(req, res, election_id)
+  if (!jwt.valid) return
+
+  // Update ballot_design_finalized in db
+  await firebase.firestore().collection('elections').doc(election_id).update({ ballot_design_finalized: true })
+
+  return res.status(201).json({ message: 'Finalized ballot design' })
+}
