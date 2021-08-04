908059b61fdb278518cae812aa2c32f667d6b49e
David Ernst <git@dsernst.com>
2021-08-04 02:29:11 -0700

    BallotDesigner: Improve look of <AutoSaver /> notification

--- src/admin/BallotDesign/AutoSaver.tsx
+++ src/admin/BallotDesign/AutoSaver.tsx
@@ -5,29 +5,30 @@ import { api } from '../../api-helper'
 import { useStored } from '../useStored'
 
 export const AutoSaver = ({ design }: { design: string }) => {
-  const [saved, setSaved] = useState(false)
+  const [unsaved, setUnsaved] = useState(false)
   const { election_id } = useStored()
 
-  const secondsVisible = 2
+  const debouncedSaveDraft = debounce(
+    async () => {
+      const response = await api(`election/${election_id}/admin/save-ballot-design`, { ballot_design: design })
 
-  const debouncedSaveDraft = debounce(async () => {
-    const response = await api(`election/${election_id}/admin/save-ballot-design`, { ballot_design: design })
-
-    if (response.status !== 201) return alert(JSON.stringify(await response.json()))
-
-    setSaved(true)
-    setTimeout(() => setSaved(false), secondsVisible * 1000)
-  }, 2000)
+      if (response.status !== 201) return alert(JSON.stringify(await response.json()))
+      setUnsaved(false)
+    },
+    500,
+    { trailing: true },
+  )
 
   // Whenever design changes...
   useEffect(() => {
+    setUnsaved(true)
     // Set a debounce to autosave after 1 second
     debouncedSaveDraft()
   }, [design])
 
   return (
     <div>
-      <span className={saved ? 'visible' : ''}>Saved.</span>
+      <span className={unsaved ? 'visible' : ''}>Saving...</span>
       <style jsx>{`
         div {
           position: relative;
@@ -39,25 +40,11 @@ export const AutoSaver = ({ design }: { design: string }) => {
           left: -4px;
           font-size: 12px;
           opacity: 0;
+          transition: opacity 0.5s;
         }
 
         .visible {
-          animation: fadein ${secondsVisible}s;
-        }
-
-        @keyframes fadein {
-          0% {
-            opacity: 0;
-          }
-          5% {
-            opacity: 0.8;
-          }
-          90% {
-            opacity: 0.8;
-          }
-          100% {
-            opacity: 0;
-          }
+          opacity: 0.75;
         }
       `}</style>
     </div>
