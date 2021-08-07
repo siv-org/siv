738f9f41fbd95d504d098bd97edc7afe83ec96ed
David Ernst <git@dsernst.com>
2021-08-07 12:00:49 -0700

    login: Use same MUI input for Login-email on small screens

--- src/admin/LoginPage/LoginInput.tsx
+++ src/admin/LoginPage/LoginInput.tsx
@@ -1,9 +1,12 @@
+import { TextField } from '@material-ui/core'
 import { OnClickButton } from 'src/landing-page/Button'
 
 export const LoginInput = ({ mobile }: { mobile?: boolean }) => {
+  const Input = mobile ? MUIInput : PlainInput
+
   return (
     <>
-      <input placeholder="Login Email" />
+      <Input />
       <OnClickButton
         invertColor={!mobile}
         style={{ margin: 0, padding: `${mobile ? 7 : 6}px 15px`, whiteSpace: 'nowrap' }}
@@ -11,18 +14,28 @@ export const LoginInput = ({ mobile }: { mobile?: boolean }) => {
       >
         Send Code
       </OnClickButton>
-      <style jsx>{`
-        input {
-          padding: 9px 15px;
-          font-size: 16px;
-          flex-grow: 1;
-          border-radius: 4px;
-          border: ${mobile ? '1px solid #ccc' : 0};
-          outline-width: 0;
-          width: 50px;
-          margin-right: 10px;
-        }
-      `}</style>
     </>
   )
 }
+
+const PlainInput = () => (
+  <>
+    <input placeholder="Login Email" />
+    <style jsx>{`
+      input {
+        padding: 9px 15px;
+        font-size: 16px;
+        flex-grow: 1;
+        border-radius: 4px;
+        border: 0;
+        outline-width: 0;
+        width: 50px;
+        margin-right: 10px;
+      }
+    `}</style>
+  </>
+)
+
+const MUIInput = () => (
+  <TextField label="Login Email" size="small" style={{ backgroundColor: '#fff', marginRight: 10 }} variant="outlined" />
+)
