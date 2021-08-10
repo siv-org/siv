7367555b43b4207a1c5d4c69160340b7b9c6bdc8
David Ernst <git@dsernst.com>
2021-08-10 00:48:32 -0700

    Tweak default-ballot-design so stringify() doesn't immediately add newlines

--- src/admin/BallotDesign/default-ballot-design.ts
+++ src/admin/BallotDesign/default-ballot-design.ts
@@ -3,9 +3,15 @@ export const default_ballot_design = `[
     "id": "president",
     "title": "Who should become President?",
     "options": [
-      { "name": "George H. W. Bush" },
-      { "name": "Bill Clinton" },
-      { "name": "Ross Perot" }
+      {
+        "name": "George H. W. Bush"
+      },
+      {
+        "name": "Bill Clinton"
+      },
+      {
+        "name": "Ross Perot"
+      }
     ],
     "write_in_allowed": true
   }
