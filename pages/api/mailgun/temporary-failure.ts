00b2c594ea46f1b99315ab9c8b11637eccabc25f
David Ernst <git@dsernst.com>
2021-08-17 18:51:04 -0700

    Webhook to store mailgun temporary failures

---
+++ pages/api/mailgun/temporary-failure.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { pushover } from '../_services'
+import { supabase } from '../_supabase'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // console.log(req.body)
+
+  const { data, error } = await supabase.from('mailgun-temporary-failures').insert([{ json: req.body }])
+
+  if (error) {
+    console.error(error)
+    await pushover('mailgun-temporary-failures webhook error', JSON.stringify(error))
+    return res.status(400).send({ error })
+  }
+
+  // console.log({ data })
+
+  res.status(200).send({ data })
+}
