a9c895b7ff75df6a8a8e9a0df354a28efb262b19
David Ernst <git@dsernst.com>
2021-08-20 17:08:06 -0700

    docs: Create <Todo> component

---
+++ pages/docs/Todo.tsx
@@ -0,0 +1,33 @@
+export const Todo = ({ children }: { children: JSX.Element }) => {
+  return (
+    <div>
+      <label>Todo</label>
+      {children}
+      <style jsx>{`
+        div {
+          border: 2px solid pink;
+          border-radius: 4px;
+
+          padding: 5px;
+          right: 5px;
+          position: relative;
+
+          color: #999;
+        }
+
+        label {
+          position: absolute;
+          top: -10px;
+          left: 5px;
+
+          padding: 0 5px;
+
+          color: pink;
+          font-weight: bold;
+
+          background-color: white;
+        }
+      `}</style>
+    </div>
+  )
+}
