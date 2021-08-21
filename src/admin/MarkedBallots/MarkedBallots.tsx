8d1ef941735964dc59c56d3f63d03080703841ee
David Ernst <git@dsernst.com>
2021-08-21 16:06:54 -0700

    admin/marked-ballots: List unlocked votes

--- src/admin/MarkedBallots/MarkedBallots.tsx
+++ src/admin/MarkedBallots/MarkedBallots.tsx
@@ -1,11 +1,21 @@
+import { useDecryptedVotes } from 'src/status/use-decrypted-votes'
+
 export const MarkedBallots = () => {
+  const votes = useDecryptedVotes()
+
   return (
     <>
       <h2>Marked Ballots</h2>
-      <p>There are no Unlocked votes yet.</p>
-      <p>
-        You can unlock votes from the <b>Voters</b> tab.
-      </p>
+      {!votes?.length ? (
+        <>
+          <p>There are no Unlocked votes yet.</p>
+          <p>
+            You can unlock votes from the <b>Voters</b> tab.
+          </p>
+        </>
+      ) : (
+        votes.map((vote) => <p key={vote.verification}>{JSON.stringify(vote)}</p>)
+      )}
     </>
   )
 }
