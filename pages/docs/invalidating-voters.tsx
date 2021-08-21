8f3d0cf0e3b10f301d4616d83a987fc27543bd5b
David Ernst <git@dsernst.com>
2021-08-20 21:59:06 -0700

    Add <title> to docs/

--- pages/docs/invalidating-voters.tsx
+++ pages/docs/invalidating-voters.tsx
@@ -1,10 +1,13 @@
 import { GlobalCSS } from 'src/GlobalCSS'
+import { Head } from 'src/Head'
 
 import InvalidatingVoters from './invalidating-voters.mdx'
 
 const DocsPage = () => {
   return (
     <main>
+      <Head title="Docs: Invalidating Voters" />
+
       <InvalidatingVoters />
       <GlobalCSS />
       <style jsx>{`
