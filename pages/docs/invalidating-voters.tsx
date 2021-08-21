139a2b284be7af280b854d800fb25e2e02791457
David Ernst <git@dsernst.com>
2021-08-20 22:06:07 -0700

    Use 'Todo:' in <title> when in docs ?todo mode

--- pages/docs/invalidating-voters.tsx
+++ pages/docs/invalidating-voters.tsx
@@ -1,12 +1,15 @@
+import { useRouter } from 'next/router'
 import { GlobalCSS } from 'src/GlobalCSS'
 import { Head } from 'src/Head'
 
 import InvalidatingVoters from './invalidating-voters.mdx'
 
 const DocsPage = () => {
+  const { query } = useRouter()
+
   return (
     <main>
-      <Head title="Docs: Invalidating Voters" />
+      <Head title={`${query?.todo !== undefined ? 'Todo' : 'Docs'}: Invalidating Voters`} />
 
       <InvalidatingVoters />
       <GlobalCSS />
