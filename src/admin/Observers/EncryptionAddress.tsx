979f001275e6bbc9afff65749ed4894fee8d4cf1
David Ernst <git@dsernst.com>
2021-08-24 11:42:15 -0700

    admin: Show button to Add Voters after Public Key is set

--- src/admin/Observers/EncryptionAddress.tsx
+++ src/admin/Observers/EncryptionAddress.tsx
@@ -1,3 +1,5 @@
+import { Button } from 'src/landing-page/Button'
+
 import { useStored } from '../useStored'
 
 export const EncryptionAddress = () => {
@@ -6,20 +8,25 @@ export const EncryptionAddress = () => {
   if (!threshold_public_key || !trustees) return null
 
   return (
-    <div>
-      {trustees.length > 1 ? (
-        <>
-          <h4>✅ The Verifying Observers completed the Pre-Election setup.</h4>
-          They created a Multi-Party Unlocking Key for the encryption address:
-        </>
-      ) : (
-        <>
-          <h4>No extra Verifying Observers were added.</h4>✅ SIV has created a unique Unlocking Key for the encryption
-          address:
-        </>
-      )}
-      <span>{threshold_public_key}</span>
-      <p>The election can now begin.</p>
+    <>
+      <div>
+        {trustees.length > 1 ? (
+          <>
+            <h4>✅ The Verifying Observers completed the Pre-Election setup.</h4>
+            They created a Multi-Party Unlocking Key for the encryption address:
+          </>
+        ) : (
+          <>
+            <h4>No extra Verifying Observers were added.</h4>✅ SIV has created a unique Unlocking Key for the
+            encryption address:
+          </>
+        )}
+        <span>{threshold_public_key}</span>
+        <p>The election can now begin.</p>
+      </div>
+      <Button href="./voters" style={{ marginLeft: 0, marginTop: 30 }}>
+        Step 3: Add Voters
+      </Button>
       <style jsx>{`
         div {
           border: 1px solid #ccc;
@@ -46,6 +53,6 @@ export const EncryptionAddress = () => {
           margin-bottom: 0;
         }
       `}</style>
-    </div>
+    </>
   )
 }
