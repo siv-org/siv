ffa1acdba514f795f2d0e95055bfc2fd6cb54340
David Ernst <git@dsernst.com>
2021-09-20 21:16:12 -0400

    WIP: Improve transition AfterShuffle

---
+++ src/status/Mixnet/AfterShuffle.tsx
@@ -0,0 +1,33 @@
+import { StaticPileOfVotes } from './StaticPileofVotes'
+
+export const AfterShuffle = ({ name }: { name: string }) => {
+  return (
+    <section>
+      <img src="/vote/shuffle.png" />
+      <StaticPileOfVotes />
+      {name && <label>{name}</label>}
+      <style jsx>{`
+        section {
+          display: flex;
+          align-items: center;
+
+          position: relative;
+        }
+
+        img {
+          width: 40px;
+          margin: 0 15px;
+        }
+
+        label {
+          position: absolute;
+          top: 110px;
+          line-height: 17px;
+          width: 93px;
+          right: 0;
+          text-align: center;
+        }
+      `}</style>
+    </section>
+  )
+}
--- src/status/Mixnet/Mixnet.tsx
+++ src/status/Mixnet/Mixnet.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 
+import { AfterShuffle } from './AfterShuffle'
 import { RandomPathsCSS } from './RandomPathsCSS'
 import { ReplayButton } from './ReplayButton'
 import { ShufflingVotes } from './ShufflingVotes'
@@ -9,11 +10,11 @@ import { StepLabel } from './StepLabel'
 
 export const Mixnet = () => {
   const observers = ['SIV Server', 'David Ernst', 'Ariana Ivan']
-  const [step, setStep] = useState(2)
+  const [step, setStep] = useState(0)
 
   useEffect(() => {
     setInterval(() => {
-      setStep((v) => (v < 2 ? v + 1 : v))
+      setStep((v) => (v < observers.length * 3 ? v + 1 : v))
     }, 1000)
   }, [])
 
@@ -30,9 +31,9 @@ export const Mixnet = () => {
           <div key={index}>
             {step > index * 3 && (
               <>
-                {step % 3 === 1 && <SlidingVotes name={o} {...{ index }} />}
-                {step % 3 === 2 && <ShufflingVotes name={o} />}
-                {step % 3 === 0 && <StaticPileOfVotes name={o} />}
+                {step === index * 3 + 1 && <SlidingVotes name={o} {...{ index }} />}
+                {step === index * 3 + 2 && <ShufflingVotes name={o} />}
+                {step >= index * 3 + 3 && <AfterShuffle name={o} />}
               </>
             )}
           </div>
