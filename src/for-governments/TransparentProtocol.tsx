8c75028aa5ba9ba8030b5b4f209496b18c49129e
David Ernst <git@dsernst.com>
2021-10-04 14:01:54 -0700

    Link to faq#secure & faq#privacy from for-govts relevent links

--- src/for-governments/TransparentProtocol.tsx
+++ src/for-governments/TransparentProtocol.tsx
@@ -1,10 +1,19 @@
+import Link from 'next/link'
+
 import { Button } from '../landing-page/Button'
 
 export const TransparentProtocol = (): JSX.Element => (
   <div>
     <h2>
-      SIV&apos;s transparent protocol helps you give citizens the option to vote with far stronger <i>security</i> &amp;{' '}
-      <i>privacy</i> than paper methods.
+      SIV&apos;s transparent protocol helps you give citizens the option to vote with far stronger{' '}
+      <Link href="/faq#secure">
+        <a>security</a>
+      </Link>{' '}
+      &amp;{' '}
+      <Link href="/faq#privacy">
+        <a>privacy</a>
+      </Link>{' '}
+      than paper methods.
     </h2>
     <Button href="/protocol">See an interactive explanation of how SIV works</Button>
 
@@ -14,6 +23,16 @@ export const TransparentProtocol = (): JSX.Element => (
         text-align: center;
         max-width: 1240px;
         margin: 0 auto;
+      }
+
+      a {
+        color: black;
+        font-style: italic;
+        text-decoration: none;
+      }
+
+      a:hover {
+        text-decoration: underline;
       }
     `}</style>
   </div>
