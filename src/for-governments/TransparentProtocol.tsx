5b9d92c26c4095a10206241397413968b8b74f18
David Ernst <git@dsernst.com>
2021-10-04 13:46:13 -0700

    for-govt: Improve language introducing SIV Protocol

--- src/for-governments/TransparentProtocol.tsx
+++ src/for-governments/TransparentProtocol.tsx
@@ -2,8 +2,11 @@ import { Button } from '../landing-page/Button'
 
 export const TransparentProtocol = (): JSX.Element => (
   <div>
-    <h2>SIV&apos;s transparent protocol lets you guarantee all votes are 100% private and secure.</h2>
-    <Button href="/protocol">See an interactive explanation of how this works</Button>
+    <h2>
+      SIV&apos;s transparent protocol helps you give citizens the option to vote with far stronger <i>security</i> &amp;{' '}
+      <i>privacy</i> than paper methods.
+    </h2>
+    <Button href="/protocol">See an interactive explanation of how SIV works</Button>
 
     <style jsx>{`
       div {
