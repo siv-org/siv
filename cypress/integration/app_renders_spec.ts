c15ea933953917a8644d853e3620fb6dd3be934a
David Ernst <git@dsernst.com>
2021-10-29 15:19:27 -0700

    Cypress check /protocol renders

--- cypress/integration/app_renders_spec.ts
+++ cypress/integration/app_renders_spec.ts
@@ -1,5 +1,30 @@
+/*
+Tests:
+
+- [ ] Can load pages without rendering error:
+  - [x] /
+  - [x] /protocol
+  - [ ] /about
+  - [ ] /faq
+  - [ ] /login
+  - [ ] /admin
+  - [ ] /${election_id}/ sub-pages:
+  - [ ] /ballot
+  - [ ] /observers
+  - [ ] /voters
+  - [ ] /vote -> EnterAuth
+  - [ ] /vote w/ Auth
+  - [ ] /${election_id}/ Status page
+  - [ ] /observer/keygen
+  - [ ] /observer/shuffle
+*/
+
 describe('The webapp should render', () => {
-  it('homepage', () => {
+  it('/ · homepage', () => {
     cy.visit('/').contains('Fast. Private. Verifiable')
+  })
+
+  it('/protocol', () => {
+    cy.visit('/protocol').contains('Secure Internet Voting (SIV) Protocol Overview')
   })
 })
