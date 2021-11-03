a3c1ef9ca18e409bd5e59490663f74d12c1f498a
David Ernst <git@dsernst.com>
2021-11-02 23:46:43 -0700

    Remove Firebase frontend dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../src/reset.css'
 
 import { AppProps } from 'next/app'
 
-import { FirebaseProvider } from '../src/firebase'
 import { GAnalytics } from '../src/gAnalytics'
 import { useSentry } from '../src/sentry'
 
@@ -10,9 +9,9 @@ export default function App({ Component, pageProps }: AppProps) {
   useSentry()
 
   return (
-    <FirebaseProvider>
+    <>
       <Component {...pageProps} />
       <GAnalytics />
-    </FirebaseProvider>
+    </>
   )
 }
