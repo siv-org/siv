19a42e3844ac52f38cf0cd03f4a6991cfa013f30
David Ernst <git@dsernst.com>
2021-12-27 15:32:12 -0800

    home3: Style Verifiability section

--- src/homepage3/Verifiability.tsx
+++ src/homepage3/Verifiability.tsx
@@ -1,13 +1,37 @@
+import { darkBlue } from './colors'
+
 export const Verifiability = () => (
   <section>
     <h2>Powerful Election Verifiability</h2>
     <p>
-      Unlike paper elections, SIV gives voters the ability to personally confirm their own submission is counted
-      correctly, and recount all results themselves.
+      Unlike paper elections, SIV gives voters the ability to personally confirm their
+      <br /> own submission is counted correctly, and recount all results themselves.
     </p>
-    <p>
-      <img src="/home3/verifiability-1.png" width="400px" />
-      <img src="/home3/verifiability-2.png" width="400px" />
-    </p>
+    <div>
+      <img src="/home3/verifiability-1.png" width="371px" />
+      <img src="/home3/verifiability-2.png" width="371px" />
+    </div>
+    <style jsx>{`
+      section {
+        margin: 3rem 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+
+      h2 {
+        text-align: center;
+        color: ${darkBlue};
+        font-size: 30px;
+        font-weight: 800;
+        margin-bottom: 0;
+      }
+
+      p {
+        font-size: 19px;
+        font-weight: 600;
+        margin-bottom: 2.5rem;
+      }
+    `}</style>
   </section>
 )
