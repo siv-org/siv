8bec9fb69f026b0b2dd36873882cf0a9e9e80562
David Ernst <git@dsernst.com>
2021-12-27 12:06:25 -0800

    homepage3: misc code cleanup

--- src/homepage3/HomePage.tsx
+++ src/homepage3/HomePage.tsx
@@ -2,12 +2,10 @@ import { AboveFold } from './AboveFold'
 import { Features } from './Features'
 import { Privacy } from './Privacy'
 
-export const HomePage = () => {
-  return (
-    <p>
-      <AboveFold />
-      <Features />
-      <Privacy />
-    </p>
-  )
-}
+export const HomePage = () => (
+  <p>
+    <AboveFold />
+    <Features />
+    <Privacy />
+  </p>
+)
