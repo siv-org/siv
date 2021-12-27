4d215c2ca977be15e2cd09fe965d4c6f288cbbec
David Ernst <git@dsernst.com>
2021-12-27 12:04:42 -0800

    homepage3: Add content for Privacy section

--- src/homepage3/HomePage.tsx
+++ src/homepage3/HomePage.tsx
@@ -1,11 +1,13 @@
 import { AboveFold } from './AboveFold'
 import { Features } from './Features'
+import { Privacy } from './Privacy'
 
 export const HomePage = () => {
   return (
     <p>
       <AboveFold />
       <Features />
+      <Privacy />
     </p>
   )
 }
---
+++ src/homepage3/Privacy.tsx
@@ -0,0 +1,12 @@
+export const Privacy = () => (
+  <section>
+    <h2>Free and fair elections require strong privacy</h2>
+    <h3>
+      SIV offers Multi-Party Encryption and strong Cryptographic Shuffles so that no one can see how anyone votes.
+    </h3>
+    <h5>All while maintaining full auditability and verifability.</h5>
+    <p>
+      <a href="/faq#privacy">Learn More</a>
+    </p>
+  </section>
+)
