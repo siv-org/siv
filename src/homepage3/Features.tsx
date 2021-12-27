f9a4c4e3f1818713f3f2f6baf47cdd2e49ceabb4
David Ernst <git@dsernst.com>
2021-12-27 12:55:50 -0800

    home3: Add Features animated icons

--- src/homepage3/Features.tsx
+++ src/homepage3/Features.tsx
@@ -2,14 +2,17 @@ export const Features = () => (
   <section>
     <div className="row">
       <div>
+        <img src="/home3/easy-to-use.gif" width="150px" />
         <h2>Easy To Use</h2>
         <p>Voters can vote from their preferred device in seconds, without needing to install anything.</p>
       </div>
       <div>
+        <img src="/home3/quick-results.gif" width="150px" />
         <h2>Quick Results</h2>
         <p>Ballots can be submitted, confirmed, and tallied instantly.</p>
       </div>
       <div>
+        <img src="/home3/verifiable.gif" width="150px" />
         <h2>Verifiable</h2>
         <p>Voters can personally verify that their vote was counted correctly & recount all votes themselves.</p>
       </div>
