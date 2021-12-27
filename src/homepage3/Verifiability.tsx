14e982d722e1212c8c2aadc8f0a34eeb18605d5f
David Ernst <git@dsernst.com>
2021-12-27 12:59:08 -0800

    home3: Add Verifiability images

--- src/homepage3/Verifiability.tsx
+++ src/homepage3/Verifiability.tsx
@@ -5,5 +5,9 @@ export const Verifiability = () => (
       Unlike paper elections, SIV gives voters the ability to personally confirm their own submission is counted
       correctly, and recount all results themselves.
     </p>
+    <p>
+      <img src="/home3/verifiability-1.png" width="400px" />
+      <img src="/home3/verifiability-2.png" width="400px" />
+    </p>
   </section>
 )
