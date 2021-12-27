c44395c8b14d41da6f4b6afcaebe3eea5741897d
David Ernst <git@dsernst.com>
2021-12-27 12:52:37 -0800

    home3: Add AboveFold phone UI animation

--- src/homepage3/AboveFold.tsx
+++ src/homepage3/AboveFold.tsx
@@ -1,13 +1,14 @@
 export const AboveFold = () => (
   <section>
-    <div>
+    <header>
       <a>SIV</a> <a href="/faq">FAQ</a> <a href="/protocol">Protocol</a> <a href="/admin">Sign In</a>
-    </div>
+    </header>
     <h1>Secure Internet Voting</h1>
     <h2>Fast. Private. Verifiable.</h2>
     <p>Millions of people want to be able to vote from their phones.</p>
     <p>
       <a href="/admin">Get Started</a> <a href="mailto:hi@secureinternetvoting.org">Contact Team</a>
     </p>
+    <img src="/home3/voter-interface.gif" width="350px" />
   </section>
 )
