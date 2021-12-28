4a9b25dc960e422c31bdf4019670d4d63f52eca0
David Ernst <git@dsernst.com>
2021-12-27 17:49:46 -0800

    home3: Make AboveFold responsive as screen grows

--- src/homepage3/AboveFold.tsx
+++ src/homepage3/AboveFold.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { darkBlue } from './colors'
 import { HeaderBar } from './HeaderBar'
 
@@ -5,9 +7,14 @@ export const AboveFold = () => (
   <section>
     <HeaderBar />
     <h1>
-      Secure Internet Voting <img className="yellow" src="/home3/yellow-underline.gif" />
+      Secure Internet Voting
+      <div className="underline-container">
+        <Image height={810} layout="responsive" src="/home3/yellow-underline.gif" width={1440} />
+      </div>
     </h1>
-    <img className="interface" src="/home3/voter-interface.gif" />
+    <div className="phone-container">
+      <Image height={1247} layout="responsive" src="/home3/voter-interface.gif" width={935} />
+    </div>
     <h2>Fast. Private. Verifiable.</h2>
     <p>Millions of people want to be able to vote from their phones.</p>
     <div>
@@ -23,46 +30,50 @@ export const AboveFold = () => (
 
       h1 {
         color: ${darkBlue};
-        font-size: 33px;
-        margin-top: 5rem;
+        font-size: 4.5vw;
+        margin-top: 10vw;
         font-weight: 800;
-        letter-spacing: 0.6px;
+        letter-spacing: 0.075vw;
         position: relative;
       }
 
-      img.yellow {
-        width: 125px;
+      .underline-container {
+        width: 17vw;
         position: absolute;
-        left: -5px;
-        top: 16px;
+        left: -0.65vw;
+        top: 2vw;
         z-index: -10;
       }
 
       h2 {
-        margin-top: 2rem;
-        margin-bottom: 4rem;
+        font-size: 2.5vw;
+        margin-top: 4vw;
+        margin-bottom: 8vw;
       }
 
-      img.interface {
+      .phone-container {
         float: right;
-        width: 200px;
+        width: 35%;
+        max-width: 450px;
+        height: 80%;
         position: relative;
-        top: -5rem;
-        right: -1.5rem;
+        top: -10vw;
+        right: -3vw;
       }
 
       p {
+        font-size: 1.75vw;
         font-weight: 700;
-        margin-bottom: 1.5rem;
+        margin-bottom: 3vw;
       }
 
       a.button {
-        border: 2px solid ${darkBlue};
+        border: 0.25vw solid ${darkBlue};
         background-color: ${darkBlue};
         color: white;
-        padding: 4px 50px;
-        border-radius: 6px;
-        margin-right: 30px;
+        padding: 0.5vw 6.25vw;
+        border-radius: 0.75vw;
+        margin-right: 3.75vw;
       }
 
       a.button:hover {
@@ -72,6 +83,7 @@ export const AboveFold = () => (
       }
 
       a {
+        font-size: 1.5vw;
         color: black;
         font-weight: 600;
       }
--- src/homepage3/HeaderBar.tsx
+++ src/homepage3/HeaderBar.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { darkBlue } from './colors'
 
 export const HeaderBar = () => (
@@ -7,28 +9,32 @@ export const HeaderBar = () => (
       <a href="/faq">FAQ</a>
       <a href="/protocol">Protocol</a>
       <a href="/about">Research</a>
-      <a href="/admin">Sign In</a>
+      <a href="/admin">
+        Sign In
+        <div className="blue-underline">
+          <Image layout="fill" src="/home3/blue-underline.png" />
+        </div>
+      </a>
     </span>
-    <img src="/home3/blue-underline.png" />
     <style jsx>{`
       header {
         display: flex;
         justify-content: space-between;
-        padding-top: 1rem;
+        padding-top: 2vw;
       }
 
       h3 {
         margin: 0;
         color: ${darkBlue};
-        font-size: 24px;
+        font-size: 3vw;
       }
 
       a {
         color: ${darkBlue};
         text-decoration: none;
-        padding: 0 13px;
-        margin: 0 3px;
-        font-size: 18px;
+        padding: 0 1.5vw;
+        margin: 0 0.4vw;
+        font-size: 2vw;
         font-weight: 500;
       }
 
@@ -37,14 +43,17 @@ export const HeaderBar = () => (
       }
 
       a:last-child {
-        margin-left: 35px;
+        margin-left: 4vw;
+        position: relative;
       }
 
-      img {
-        width: 100px;
+      .blue-underline {
+        width: 12vw;
+        height: 6vw;
         position: absolute;
-        right: 40px;
-        top: 40px;
+        right: -1vw;
+        left: -1vw;
+        top: 0;
         z-index: -10;
       }
     `}</style>
