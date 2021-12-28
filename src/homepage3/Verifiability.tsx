d0758c26202f47f89291b6535b4ecfc57fe809ab
David Ernst <git@dsernst.com>
2021-12-27 19:02:44 -0800

    home3: Make Verifiability section responsive to large screens

--- src/homepage3/Verifiability.tsx
+++ src/homepage3/Verifiability.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { darkBlue } from './colors'
 
 export const Verifiability = () => (
@@ -7,13 +9,17 @@ export const Verifiability = () => (
       Unlike paper elections, SIV gives voters the ability to personally confirm their
       <br /> own submission is counted correctly, and recount all results themselves.
     </p>
-    <div>
-      <img src="/home3/verifiability-1.png" width="371px" />
-      <img src="/home3/verifiability-2.png" width="371px" />
+    <div className="container">
+      <div>
+        <Image height={1080} layout="responsive" src="/home3/verifiability-1.png" width={1920} />
+      </div>
+      <div>
+        <Image height={1080} layout="responsive" src="/home3/verifiability-2.png" width={1920} />
+      </div>
     </div>
     <style jsx>{`
       section {
-        margin: 3rem 0;
+        margin: 8vw 0 15vw;
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -22,15 +28,26 @@ export const Verifiability = () => (
       h2 {
         text-align: center;
         color: ${darkBlue};
-        font-size: 30px;
+        font-size: 3.75vw;
         font-weight: 800;
         margin-bottom: 0;
       }
 
       p {
-        font-size: 19px;
+        font-size: 2.375vw;
         font-weight: 600;
-        margin-bottom: 2.5rem;
+        margin-bottom: 5vw;
+      }
+
+      .container {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+      }
+
+      .container > div {
+        position: relative;
+        width: 49%;
       }
     `}</style>
   </section>
