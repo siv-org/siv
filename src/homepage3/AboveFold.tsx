70fb568da5171caf3d19b39953f428e1366c2628
David Ernst <git@dsernst.com>
2021-12-28 00:34:15 -0800

    home3: AboveFold mobile responsive

--- src/homepage3/AboveFold.tsx
+++ src/homepage3/AboveFold.tsx
@@ -12,10 +12,10 @@ export const AboveFold = () => (
         <Image height={810} layout="responsive" src="/home3/yellow-underline.gif" width={1440} />
       </div>
     </h1>
+    <h2>Fast. Private. Verifiable.</h2>
     <div className="phone-container">
       <Image height={912} layout="responsive" src="/home3/above-interface.gif" width={464} />
     </div>
-    <h2>Fast. Private. Verifiable.</h2>
     <p>Millions of people want to be able to vote from their phones.</p>
     <div>
       <a className="button" href="/admin">
@@ -23,6 +23,7 @@ export const AboveFold = () => (
       </a>
       <a href="mailto:hi@secureinternetvoting.org">Contact Team</a>
     </div>
+
     <style jsx>{`
       h1 {
         color: ${darkBlue};
@@ -44,7 +45,6 @@ export const AboveFold = () => (
       h2 {
         font-size: 2.5vw;
         margin-top: 4vw;
-        margin-bottom: 8vw;
       }
 
       .phone-container {
@@ -53,11 +53,12 @@ export const AboveFold = () => (
         max-width: 300px;
         height: 80%;
         position: relative;
-        top: -14vw;
+        top: -16vw;
         right: 2vw;
       }
 
       p {
+        margin-top: 8vw;
         font-size: 1.75vw;
         font-weight: 700;
         margin-bottom: 3vw;
@@ -83,6 +84,47 @@ export const AboveFold = () => (
         font-size: 1.5vw;
         color: black;
         font-weight: 600;
+      }
+
+      /* 1-column for small screens */
+      @media (max-width: 700px) {
+        section {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        h1 {
+          font-size: 6.2vw;
+        }
+
+        .underline-container {
+          width: 23.4vw;
+        }
+
+        h2 {
+          font-size: 4.5vw;
+        }
+
+        .phone-container {
+          float: none;
+          top: 0;
+          left: 0;
+        }
+
+        p {
+          font-size: 3vw;
+          text-align: center;
+          margin-bottom: 6vw;
+        }
+
+        a {
+          font-size: 3.5vw;
+        }
+
+        a.button {
+          padding: 1.5vw 6.25vw;
+        }
       }
     `}</style>
   </section>
