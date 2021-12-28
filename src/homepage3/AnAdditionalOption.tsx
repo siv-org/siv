0684064686e223941a291ed99520f07779866da4
David Ernst <git@dsernst.com>
2021-12-27 19:18:38 -0800

    home3: Make AnAdditionalOption section responsive for large screens

--- src/homepage3/AnAdditionalOption.tsx
+++ src/homepage3/AnAdditionalOption.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { Link } from 'react-scroll'
 
 import { darkBlue } from './colors'
@@ -8,15 +9,21 @@ export const AnAdditionalOption = () => (
     <p>SIV works alongside paper methods, so voters can use their preference.</p>
     <ul>
       <li>
-        <img src="/home3/methods-1.png" />
+        <div>
+          <Image height={1080} layout="responsive" src="/home3/methods-1.png" width={1920} />
+        </div>
         In Person
       </li>
       <li>
-        <img src="/home3/methods-2.png" />
+        <div>
+          <Image height={1080} layout="responsive" src="/home3/methods-2.png" width={1920} />
+        </div>
         By Mail
       </li>
       <li>
-        <img src="/home3/methods-3.png" />
+        <div>
+          <Image height={223} layout="responsive" src="/home3/methods-3.png" width={256} />
+        </div>
         Online
       </li>
     </ul>
@@ -27,65 +34,72 @@ export const AnAdditionalOption = () => (
     </p>
     <style jsx>{`
       section {
-        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
       }
 
       h2 {
         color: ${darkBlue};
-        font-size: 32px;
+        font-size: 4vw;
         font-weight: 800;
       }
 
       p {
-        font-size: 19px;
+        font-size: 2.375vw;
       }
 
       ul {
         display: flex;
         align-items: center;
         justify-content: space-between;
+        width: 100%;
+        max-width: 1240px;
+        margin: 0 auto;
+        padding-inline-start: 0;
       }
 
       li {
         list-style: none;
         position: relative;
-        margin-top: 120px;
-        font-size: 16px;
+        font-size: 2vw;
+        margin: 15vw 8vw 0;
       }
 
-      img {
-        width: 80px;
+      div {
+        width: 10vw;
         position: absolute;
-        top: -4rem;
+        top: -8vw;
       }
 
-      li:nth-child(1) img {
-        left: -5px;
+      li:nth-child(1) div {
+        left: -0.625vw;
       }
 
-      li:nth-child(2) img {
-        left: -13px;
+      li:nth-child(2) div {
+        left: -1.625vw;
       }
 
-      li:nth-child(3) img {
-        left: -13px;
-        top: -4.5rem;
+      li:nth-child(3) div {
+        left: -1.8vw;
+        top: -9vw;
       }
     `}</style>
     <style global jsx>{`
       #help-bring-btn {
         cursor: pointer;
 
-        border: 2px solid ${darkBlue};
+        border: 0.25vw solid ${darkBlue};
         background-color: ${darkBlue};
         color: white;
-        padding: 4px 40px;
+        padding: 0.5vw 5vw;
+        border-radius: 0.75vw;
         border-radius: 6px;
         font-weight: 600;
-        font-size: 14px;
+        font-size: 1.875vw;
 
         display: inline-block;
-        margin: 3rem 0;
+        margin: 6vw 0;
       }
 
       #help-bring-btn:hover {
