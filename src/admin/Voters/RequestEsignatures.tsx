1a2fcd3617a49055287081b2bc876ff36542492e
David Ernst <git@dsernst.com>
2022-02-12 14:43:50 -0800

    admin/voters: Add eSignature icon

--- src/admin/Voters/RequestEsignatures.tsx
+++ src/admin/Voters/RequestEsignatures.tsx
@@ -1,4 +1,6 @@
 import { Switch } from '@material-ui/core'
+import Image from 'next/image'
+import esignatureIcon from 'public/esignature.png'
 import { useState } from 'react'
 
 import { api } from '../../api-helper'
@@ -8,30 +10,40 @@ import { revalidate, useStored } from '../useStored'
 export const RequestEsignatures = () => {
   const [updating, setUpdating] = useState(false)
   const { election_id, esignature_requested } = useStored()
+
+  async function toggleESignature() {
+    setUpdating(true)
+    const response = await api(`election/${election_id}/admin/set-esignature-requested`, {
+      esignature_requested: !esignature_requested,
+    })
+
+    if (response.status === 201) {
+      revalidate(election_id)
+      setUpdating(false)
+    } else {
+      throw await response.json()
+    }
+  }
+
   return (
     <div>
-      <label>Request eSignatures?</label>
-      <Switch
-        checked={!!esignature_requested}
-        color="primary"
-        onClick={async () => {
-          setUpdating(true)
-          const response = await api(`election/${election_id}/admin/set-esignature-requested`, {
-            esignature_requested: !esignature_requested,
-          })
-
-          if (response.status === 201) {
-            revalidate(election_id)
-            setUpdating(false)
-          } else {
-            throw await response.json()
-          }
-        }}
-      />
+      <label onClick={toggleESignature}>
+        <div style={{ display: 'inline-block', marginRight: 5, position: 'relative', top: 2 }}>
+          <Image height={(218 / 700) * 70} layout="fixed" placeholder="blur" src={esignatureIcon} width={70} />
+        </div>
+        Request eSignatures?
+      </label>
+      <div style={{ bottom: 3, display: 'inline-block', position: 'relative' }}>
+        <Switch checked={!!esignature_requested} color="primary" onClick={toggleESignature} />
+      </div>
       {updating && <Spinner />}
       <style jsx>{`
         div {
           margin-bottom: 30px;
+        }
+
+        label {
+          cursor: pointer;
         }
       `}</style>
     </div>
