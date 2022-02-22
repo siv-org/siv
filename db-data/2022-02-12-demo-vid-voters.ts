daf98d04ec30968ec464e642dd1c3017fac9f74f
David Ernst <git@dsernst.com>
2022-02-22 13:11:31 -0800

    Demo vid will cut away to have voters 2-10 vote, instead of trickling in throughout

--- db-data/2022-02-12-demo-vid-voters.ts
+++ db-data/2022-02-12-demo-vid-voters.ts
@@ -7,7 +7,7 @@ Execute it w/:
 
 import './_env'
 
-import { mapValues, round } from 'lodash'
+import { mapValues, range } from 'lodash'
 import fetch from 'node-fetch' // Replaced in node 17.5 w/ experimental native fetch
 
 import { firebase } from '../pages/api/_services'
@@ -15,6 +15,10 @@ import { RP, random_bigint, stringToPoint } from '../src/crypto/curve'
 import encrypt from '../src/crypto/encrypt'
 import { CipherStrings } from '../src/crypto/stringify-shuffle'
 import { generateTrackingNum } from '../src/vote/tracking-num'
+
+const election_id = '1645244736891'
+const first_voter = 2
+const last_voter = 10
 
 const api = (route: string, body?: Record<string, unknown>) =>
   fetch(`http://localhost:3000/api/${route}`, {
@@ -25,41 +29,6 @@ const api = (route: string, body?: Record<string, unknown>) =>
     },
     method: 'POST',
   })
-
-// At the [key] second mark, our target % having voted is the value on the right.
-const turnoutGoalAtSecond = {
-  120: 0.85,
-  25: 0,
-  30: 0.01,
-  40: 0.1,
-  48: 0.3,
-  55: 0.5,
-  70: 0.75,
-  80: 0.9,
-}
-const smoothTurnoutGoal = (currentSecond: number): number => {
-  // First check if there's an exact match.
-  const exact = turnoutGoalAtSecond[currentSecond]
-  if (exact !== undefined) return exact
-
-  const sortedKeys = Object.keys(turnoutGoalAtSecond)
-    .map((k) => parseInt(k))
-    .sort((a, b) => a - b)
-
-  // If not, find what it's between
-  const above = sortedKeys.find((key) => key > currentSecond)
-  const below = [...sortedKeys].reverse().find((key) => key < currentSecond)
-
-  // If we're at either end, return their values
-  if (!below) return turnoutGoalAtSecond[above]
-  if (!above) return turnoutGoalAtSecond[below]
-
-  // Otherwise, interpolate between them
-  const belowValue = turnoutGoalAtSecond[below]
-  const aboveValue = turnoutGoalAtSecond[above]
-  const fraction = (currentSecond - below) / (above - below)
-  return round(belowValue + (aboveValue - belowValue) * fraction, 2)
-}
 
 const ballot_design = [
   {
@@ -108,7 +77,7 @@ const ballot_design = [
     write_in_allowed: false,
   },
 ]
-const pub_key = RP.fromHex('0a6becbe92272f6083053abf79662622af0b520971c5261f0045ba12e7606b55')
+let pub_key: RP
 type Vote = { mayor: CipherStrings; us_house_rep: CipherStrings }
 const random_vote = () => {
   const verification = generateTrackingNum()
@@ -138,7 +107,6 @@ const random_vote = () => {
     return memo
   }, {} as Vote)
 }
-const election_id = '1645244736891'
 const submit_random_encrypted_vote = async (auth_token: string) => {
   const response = await api('submit-vote', { auth: auth_token, election_id, encrypted_vote: random_vote() })
   if (response.status !== 200) {
@@ -147,67 +115,43 @@ const submit_random_encrypted_vote = async (auth_token: string) => {
   }
 }
 
-// TODO: Once election begins the unlocking stage, no more votes should come in.
-const hasBegunUnlocking = () => false
-
 type Voter = { auth_token: string; email: string }
 const getVoters = async (): Promise<Voter[]> =>
-  (await firebase.firestore().collection('elections').doc(election_id).collection('voters').get()).docs.map(
-    (doc) => ({ ...doc.data() } as Voter),
+  Promise.all(
+    range(first_voter, last_voter + 1).map(
+      async (x) =>
+        ({
+          ...(
+            await firebase
+              .firestore()
+              .collection('elections')
+              .doc(election_id)
+              .collection('voters')
+              .doc(`demo-voter-${x}@siv.org`)
+              .get()
+          ).data(),
+        } as Voter),
+    ),
   )
 // const getVoters = () => range(200).map((n) => ({ auth_token: '', email: `demo-voter-${n}@siv.org` }))
 
-const hasNotVoted: Voter[] = []
-let numVoters: number
-let hasVoted = 0
-
-const mainLoop = (currentSecond: number) => {
-  //   Every second, we'll loop:
-
-  // If election began unlocking, we'll stop the loop.
-  if (hasBegunUnlocking()) {
-    console.log('Election began unlocking 👍')
-    process.exit(0)
-  }
-
-  //  What % should have voted by now?
-  const currentTurnoutGoal = smoothTurnoutGoal(currentSecond)
-
-  // What % has voted?
-  const hasVotedPct = round(hasVoted / numVoters, 2)
-  // What's the diff?
-  const diff = round(currentTurnoutGoal - hasVotedPct, 2)
-
-  console.log(
-    `Time: ${currentSecond}. Diff: ${diff}. Target: ${currentTurnoutGoal} vs ${hasVotedPct} Actual: ${hasVoted} / ${numVoters}`,
-  )
-
-  // For each voter who hasn't voted yet
-  hasNotVoted.forEach(({ auth_token }, index) => {
-    // if (diff > RNG)
-    // cast a vote for them in this round
-    if (diff > Math.random()) {
-      //   console.log(`${email}: Simulating vote`)
-      hasNotVoted.splice(index, 1)
-      hasVoted++
-      // console.log(random_vote())
-      submit_random_encrypted_vote(auth_token)
-    }
-  })
-}
+/** Load this election's pub key from the db */
+const getPubkey = async (): Promise<RP> =>
+  RP.fromHex((await firebase.firestore().collection('elections').doc(election_id).get()).data().threshold_public_key)
 
 ;(async () => {
-  let seconds = 0
-
+  pub_key = await getPubkey()
+  console.log(`Found pubkey ${pub_key}`)
   const voters = await getVoters()
-  numVoters = voters.length
-  console.log(`Found ${numVoters} voters`)
+  console.log(`Found ${voters.length} voters`)
 
   // Only simulate for the demo voters
-  const demoVoters = voters.filter(({ email }) => email.includes('demo'))
-  hasNotVoted.push(...demoVoters)
+  await Promise.all(
+    voters.map(async ({ auth_token, email }) => {
+      await submit_random_encrypted_vote(auth_token)
+      console.log({ auth_token, email, submitted: true })
+    }),
+  )
 
-  setInterval(() => {
-    mainLoop(seconds++)
-  }, 1000)
+  console.log('\nDone 👍')
 })()
