a14ae5866fabaff21c3c5389e72caecbc7ac5bad
David Ernst <git@dsernst.com>
2022-02-25 14:23:33 -0800

    Analytics: Track each visibility session individually

--- src/demo/useAnalytics.ts
+++ src/demo/useAnalytics.ts
@@ -3,19 +3,24 @@ import { api } from 'src/api-helper'
 
 const isBrowser = () => typeof window !== 'undefined'
 
+const startSession = (hash: string) => {
+  api('load', { hash }).then(async (res) => {
+    if (res.status === 200) {
+      const id = await res.text()
+
+      document.onvisibilitychange = async () => {
+        if (id && document.visibilityState === 'hidden') navigator.sendBeacon(`/api/unload?i=${id}`)
+        if (document.visibilityState === 'visible') startSession(hash)
+      }
+    }
+  })
+}
+
 export const useAnalytics = () => {
   useEffect(() => {
     if (!isBrowser) return
     const hash = window.location.hash
 
-    api('load', { hash }).then(async (res) => {
-      if (res.status === 200) {
-        const id = await res.text()
-
-        window.onbeforeunload = async () => {
-          if (id) navigator.sendBeacon(`/api/unload?i=${id}`)
-        }
-      }
-    })
+    startSession(hash)
   }, [])
 }
