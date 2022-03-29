b127aef0bb011ea59109e52c948c297b7f4d1c00
David Ernst <git@dsernst.com>
2022-03-28 21:52:21 -0700

    protocol step2: Remove 'Regerate' Verification# btn

--- src/protocol/VerificationSecret.tsx
+++ src/protocol/VerificationSecret.tsx
@@ -1,14 +1,12 @@
-import { Button, NoSsr, TextField } from '@material-ui/core'
+import { NoSsr, TextField } from '@material-ui/core'
 import { useEffect } from 'react'
 
 import { generateTrackingNum } from '../vote/tracking-num'
 import { Paper } from './Paper'
-import { useWindowDimensions } from './useWindowDimensions'
 import { useVoteContext } from './VoteContext'
 
 export function VerificationSecret(): JSX.Element {
   const { dispatch, state } = useVoteContext()
-  const { width } = useWindowDimensions()
 
   const generate = () => {
     dispatch({ verification: generateTrackingNum() })
@@ -39,14 +37,11 @@ export function VerificationSecret(): JSX.Element {
               value={state.plaintext.verification}
               variant="outlined"
             />
-            <Button color="primary" onClick={generate}>
-              {width < 450 ? 'Regen' : 'Regenerate'}
-            </Button>
           </NoSsr>
         </div>
         <p>
-          <b>This Verification # will be shown once votes are unlocked.</b> It allows you to easily verify your vote was
-          counted correctly, while protecting your privacy.
+          <b>This Verification # will be publicly shown once votes are unlocked.</b> It allows you to easily verify your
+          vote was counted correctly, while protecting your privacy.
         </p>
         <p style={{ fontSize: 12 }}>
           This unique value was generated on your own device. Don&apos;t share it with anyone.
