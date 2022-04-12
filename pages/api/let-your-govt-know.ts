a1c42280c6cb1b3156c2f88eb8d1003d25e1614b
David Ernst <git@dsernst.com>
2022-04-12 15:18:37 -0700

    /api/let-your-govt-know: Validate submission is an object
    Co-authored-by: Ariana Ivan <73454259+arianaivan@users.noreply.github.com>

--- pages/api/let-your-govt-know.ts
+++ pages/api/let-your-govt-know.ts
@@ -5,6 +5,8 @@ import { firebase, pushover } from './_services'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const fields = req.body
 
+  if (typeof fields !== 'object') return res.status(400).json({ error: 'Invalid request' })
+
   // Store submission in Firestore
   await firebase
     .firestore()
