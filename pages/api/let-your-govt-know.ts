187cea65c920bdda94d32511a9fc9c91c8a8c5dc
David Ernst <git@dsernst.com>
2022-04-12 17:17:13 -0700

    /api/let-your-govt-know: Very basic zip code validation
    Co-authored-by: Ariana Ivan <73454259+arianaivan@users.noreply.github.com>

--- pages/api/let-your-govt-know.ts
+++ pages/api/let-your-govt-know.ts
@@ -8,6 +8,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (typeof fields !== 'object') return res.status(400).json({ error: 'Invalid submission' })
   if (!Object.keys(fields).length) return res.status(400).json({ error: 'Invalid submission' })
   if (!fields.zip) return res.status(400).json({ error: 'Zip code is required' })
+  const startsWithThreeDigitsRegex = /^\d{3}/
+  if (!startsWithThreeDigitsRegex.test(fields.zip)) {
+    await pushover(`SIV Endorsement Invalid ZIP Code`, JSON.stringify(fields))
+    return res.status(400).json({ error: 'Invalid zip code' })
+  }
 
   // Store submission in Firestore
   await firebase
@@ -20,7 +25,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     })
 
   // Notify admin via Pushover
-  pushover(`SIV Endorsement: ${fields.name} (${fields.zip})`, `${fields.email}\n\n${fields.message}`)
+  await pushover(`SIV Endorsement: ${fields.name} (${fields.zip})`, `${fields.email}\n\n${fields.message}`)
 
   // Send back success
   return res.status(201).json({ success: true })
