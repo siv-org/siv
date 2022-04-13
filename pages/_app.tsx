e86357547fc6db35fd302abd690204cc87364502
David Ernst <git@dsernst.com>
2022-04-12 19:43:13 -0700

    Revert "Temp fix to @ts-ignore _app.tsx <Component />"
    This reverts commit df0feab2afe151eb1fde38e50938884f51c3f276.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,9 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      {/* Temp fix for ts bug introduced around 4/12/22 see https://stackoverflow.com/questions/71831601/ts2786-component-cannot-be-used-as-a-jsx-component or https://stackoverflow.com/questions/71843247/react-nextjs-type-error-component-cannot-be-used-as-a-jsx-component  */}
-      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-      {/* @ts-ignore */}
       <Component {...pageProps} />
       <GAnalytics />
     </>
