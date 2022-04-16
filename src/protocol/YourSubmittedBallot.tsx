4d6330cbf84a63844d1b8f39e828de45944435fb
David Ernst <git@dsernst.com>
2022-04-15 19:27:18 -0700

    protocol: Fix horiz scrolling

--- src/protocol/YourSubmittedBallot.tsx
+++ src/protocol/YourSubmittedBallot.tsx
@@ -4,7 +4,7 @@ export function YourSubmittedBallot(): JSX.Element {
   const { auth, mayor_vote } = useVoteContext().state.encrypted
 
   return (
-    <code style={{ display: 'block', lineHeight: '16px', padding: '0 6%' }}>
+    <code style={{ display: 'block', lineBreak: 'anywhere', lineHeight: '16px', padding: '0 6%' }}>
       {`{`} auth: <span style={{ color: '#e67e37', fontWeight: 700 }}>&apos;{auth}&apos;</span>, mayor_vote:{' '}
       {mayor_vote} {`}`}
     </code>
