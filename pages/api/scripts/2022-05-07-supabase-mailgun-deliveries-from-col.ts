05418a607008d1552a4356fd7b82d84cf0ce4240
David Ernst <git@dsernst.com>
2022-05-07 16:30:21 -0700

    mailgun-deliveries: Include name in 'from' field, not just email
    Co-authored-by: Ariana Ivan <73454259+arianaivan@users.noreply.github.com>

--- pages/api/scripts/2022-05-07-supabase-mailgun-deliveries-from-col.ts
+++ pages/api/scripts/2022-05-07-supabase-mailgun-deliveries-from-col.ts
@@ -1,7 +1,7 @@
 // This script will populate the supabase.mailgun-deliveries table's `from` column.
 
 import { supabase } from 'api/_supabase'
-import { validate as validateEmail } from 'email-validator'
+// import { validate as validateEmail } from 'email-validator'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -12,38 +12,41 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { count, data: deliveries } = await supabase
     .from('mailgun-deliveries')
     .select('*', { count: 'exact' })
-    .is('from', null)
-    .limit(1000)
+    .order('id', { ascending: true })
+  // .range(6000, 7000)
+  // .limit(1000)
 
   // Find `from` field
-  const updates = await Promise.all(
-    (deliveries || []).map(async ({ id, json }) => {
+  await Promise.all(
+    (deliveries || []).map(async ({ from, id, json }) => {
       const from_string = json['event-data'].message.headers.from
 
-      let email = ''
-      // If from_string is not a valid email, extract the email from within < and >
-      if (validateEmail(from_string)) {
-        email = from_string
-      } else {
-        const after_lt = from_string.split('<')[1]
-        if (!after_lt) throw new Error(`${id}: Invalid after_lt (${after_lt}), from_string: ${from_string}`)
+      if (from_string === from) return `${id}: done already`
 
-        email = after_lt.split('>')[0]
-      }
+      //   let email = ''
+      //   // If from_string is not a valid email, extract the email from within < and >
+      //   if (validateEmail(from_string)) {
+      //     email = from_string
+      //   } else {
+      //     const after_lt = from_string.split('<')[1]
+      //     if (!after_lt) throw new Error(`${id}: Invalid after_lt (${after_lt}), from_string: ${from_string}`)
 
-      // Confirm extracted email is valid
-      if (!validateEmail(email)) {
-        throw new Error(`${id}: Invalid email (${email}), from_string: ${from_string}`)
-      }
+      //     email = after_lt.split('>')[0]
+      //   }
+
+      //   // Confirm extracted email is valid
+      //   if (!validateEmail(email)) {
+      //     throw new Error(`${id}: Invalid email (${email}), from_string: ${from_string}`)
+      //   }
 
       // Update db
-      const { error } = await supabase.from('mailgun-deliveries').update({ from: email }).eq('id', id)
+      const { error } = await supabase.from('mailgun-deliveries').update({ from: from_string }).eq('id', id)
 
       if (error) throw error
 
-      return `${id}: ${email}`
+      return `${id}: ${from_string}`
     }),
   )
 
-  res.status(201).send({ count, updates })
+  res.status(201).send({ count, length: deliveries?.length })
 }
