4cdc8db84e43ee749059bc45c1770d7507d1cd6b
David Ernst <git@dsernst.com>
2022-05-07 18:51:32 -0700

    outreach: Display emails sent by an authenticated user
    Co-authored-by: Ariana Ivan <73454259+arianaivan@users.noreply.github.com>

--- src/outreach/EmailsSent.tsx
+++ src/outreach/EmailsSent.tsx
@@ -1,5 +1,5 @@
 import { User } from '@supabase/supabase-js'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { supabase } from './supabase'
 
@@ -7,18 +7,70 @@ export const EmailsSent = () => {
   if (!supabase.auth.user()) return null
 
   const { email } = supabase.auth.user() as User
+  const [emails, setEmails] = useState([])
 
   async function getSentEmails(email?: string) {
-    const { data, error } = await supabase.from('mailgun-deliveries').select('*').eq('from', email).limit(50)
+    const { data, error } = await supabase
+      .from('mailgun-deliveries')
+      .select('*')
+      .eq('from', email)
+      .order('id', { ascending: false })
+      .limit(50)
 
     if (error) return alert(JSON.stringify(error))
 
-    return data
+    setEmails(data)
   }
 
   useEffect(() => {
     getSentEmails(email)
   }, [email])
 
-  return <h3>Emails sent</h3>
+  return (
+    <>
+      <h3>Emails sent</h3>
+      <ul>
+        {emails.map((email) => {
+          const { created_at, from, subject, to } = email
+          return (
+            <li key={created_at}>
+              <div style={{ width: 200 }}>{created_at}</div>
+              <div style={{ width: 120 }}>{from}</div>
+              <div style={{ width: 200 }}>{to}</div>
+              <div style={{ width: 200 }}>{subject}</div>
+              <div>
+                <a onClick={() => alert(JSON.stringify(email))}>full json</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+
+      <style jsx>{`
+        ul {
+          padding-inline: 0;
+        }
+
+        li {
+          list-style-type: none;
+        }
+
+        li div {
+          overflow: scroll;
+          display: inline-block;
+          white-space: nowrap;
+
+          margin-left: 20px;
+        }
+
+        li div::-webkit-scrollbar {
+          display: none;
+        }
+
+        li div:first-child {
+          margin-left: 0;
+        }
+      `}</style>
+    </>
+  )
 }
