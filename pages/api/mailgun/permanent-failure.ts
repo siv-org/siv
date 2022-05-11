8a2f99da942e6a59cbc7834642764eb811ed7ed2
David Ernst <git@dsernst.com>
2022-05-10 17:18:08 -0700

    mailgun permanent-failures: Store from, to, subject as indiv cols

--- pages/api/mailgun/permanent-failure.ts
+++ pages/api/mailgun/permanent-failure.ts
@@ -6,7 +6,18 @@ import { supabase } from '../_supabase'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // console.log(req.body)
 
-  const { error } = await supabase.from('mailgun-permanent-failures').insert([{ json: req.body }])
+  const json = req.body
+  const eventData = json['event-data']
+  if (!eventData) {
+    console.error('Missing event data. req.body:', req.body)
+    console.error('req.headers:', req.headers)
+    return res.status(400).json({ error: 'Missing event data.' })
+  }
+  const { message } = eventData
+  const { headers } = message
+  const { from, subject, to } = headers
+
+  const { error } = await supabase.from('mailgun-permanent-failures').insert([{ from, json: req.body, subject, to }])
 
   if (error) {
     console.error(error)
