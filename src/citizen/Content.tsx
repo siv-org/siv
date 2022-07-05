818a4d160cac3820415e8f1a4ffeb4b37bbe0a1b
arianaivan <73454259+arianaivan@users.noreply.github.com>
2022-07-05 16:22:28 -0700

    /citizen: Convert markdown to html
    Co-authored-by: David Ernst <git@dsernst.com>

--- src/citizen/Content.tsx
+++ src/citizen/Content.tsx
@@ -1,175 +1,250 @@
-void `# The SIV Token
-
-We want to create a way for anyone and everyone to be able to invest in SIV’s growth and success.
-
-Using powerful new cryptographic tools, we can.
-
-———
-
-## Simple & Innovative Token Economics:
-
-All future revenue that SIV earns from governments will be paid in USD dividends to SIV Token Holders.
-
-——-
-
-## Right now….
-
-The city of San Francisco (as one example) pays ***$25 per year per registered voter*** (whether they actually vote or not!) to Dominion Election Systems, for paper voting equipment.
-
-The US government — aka, we the taxpayers — spends $4 billion every year on election administration. $8b in 2020 because of COVID.
-
-———-
-
-## SIV has created far superior technology:
-
-- Easier
-- Faster
-- End-to-end Verifiable
-- Cryptographically Private
-- Fully Auditable
-- Pandemic-Resistant
-
-This allows every day citizens like you and me to vote from the internet-connected supercomputers in our pockets.
-
-And all in a way that creates *more trust* than the current election systems we use right now….
-
-SIV allows us to verify for ourselves that our vote is cast and counted correctly, without tampering, and allows each and every one of us to fully recount the vote for ourselves if we want.
-
-No more vote-counts trapped behind closed doors, behind any curtains. 
-
-Each and every one of us can see for ourselves what the correct results are supposed to be — all ***while preserving our individual vote privacy***.
-
-Learn more how it works: [The Secure Internet Voting (SIV) Protocol](https://siv.org/protocol)
-
-——-
-
-## But! There is one major barrier…
-
-In order to be able to use Secure Internet Voting, ***we the citizens*** have to first be offered SIV as an option.
-
-This requires buy-in from:
-
-- our election officials — many of whom are hard-working, honest public servants!
-- our state legislators — who are very busy, and have trouble knowing what citizens want them to prioritize!
-
-——
-
-## This is where you come in!
-
-We at [SIV.org](https://siv.org) have spent the last 2 years reaching out and pitching to hundreds of election officials, throughout the country. 
-
-Although it’s been an uncountable number of hours, this is only a drop in the bucket, as throughout the US there are over 3000 counties running elections, each with many different people involved.
-
-We’ve heard from them over and over again: “I know electronic voting is the way things are of course eventually heading, but are we ready yet?” For VERY GOOD REASONS, these officials are technologically conservative & risk-averse.
-
-It’s naturally frightening to go first or try new things. Especially with something so vital as ***elections***.
-
-——-
-
-## They need to SEE & HEAR from us citizens that this is something that we want.
-
-Whenever you get the opportunity, please tell them you want to be able to vote from your phone in an easy, safe, and secure way.
-
-Tell them you’re excited about [SIV.org](https://siv.org), and would like to be [able to try it out]()!
-
-Best is if you talk to them in person, or call them on the phone. Be polite. They’re human too, of course, and appreciate a positive impression.
-
-——
-
-## We’ve also made it easy to send them a note yourself
-
-You can write your message here. We’ll find your specific election officials & representatives, forward your message, and let you know of any replies.
-
-[ Your name ]  
-[ Your ZIP Code ]  
-[ Your email (optional) ]  
-[ Your message ]  
-
-——-
-
-## AND! You can personally invest in SIV’s Growth & Future Success.
-
-Directly, right now, without needing anyone’s permission.
-
-### How it works:
-
-1) You send USD, and receive SIV Tokens.
-
-2) [SIV.org](https://siv.org) can immediately make use of your investment, to further our mission of letting all of us vote quickly, easily, and safely from our phones. All of our finances are transparently published.
-
-3) Everyone, including our election officials, can see that citizens really believe in, want SIV to exist, and be more widely available.  
-This furthers our mission, and allows us to overcome the network effects needed to create ***Legislative Change*** — legalizing Secure Internet Voting for citizens in your area.
-
-4) 100% of all future SIV revenue from governments is legally mandated to be distributed proportionally to SIV Token Holders. [SIV.org](https://siv.org) itself is a Nonprofit Foundation, and all of our code is open-source.
-
-- SIV Tokens are a liquid asset, built using cryptocurrency, and can be sold at any time. 
-- To make things easier, we are also happy to help custody any American’s SIV Tokens as long as needed, collecting dividends until ready to withdraw, with zero fees.
-
-———
-
-## Democracy is under siege worldwide.
-
-We’re doing what we can to make our representative governments more trustworthy, accessible, and modern.
-
-***We invite you to join us.***
-
-——
-
-## Ready to Invest in the Future of Free Society?
-
-[ Your Name ]  
-[ Your Email ]  
-[ Your Preferred Investment Amount ]
-
-*Payment options: Credit/Debit Card, Paypal, Bank Transfer, Cryptocurrency*
-
-This SIV Token ***is registered*** under the Securities Act of 1933. Here is [our S-1 filing]().
-
-——
-
-## FAQ
-
-**1. When can I start voting from my phone?**
-
-As soon as your local government will allow you. 
-
-The technology is all ready, and we’ve run a number of highly successful private pilots. The core barrier left is state and local legislative action. 
-
-Thanks to the federal UOCAVA Act, Secretaries of State and local election officials do have some legal leeway to run small-scale pilots, especially for Overseas Voters (like military voters) and Disabled Voters. But these election officials can always use more public encouragement.
-
-**2. Where can I learn more about how SIV works? How do I know this is secure?**
-
-- [Technological Protocol Description]()
-- [The Peer-Reviewed Computer Science Research SIV is based upon]()
-- [How SIV Addresses the Top Internet Voting Concerns]()
-- [The 34 Most Frequently Asked Questions About Using SIV]()
-
-**3. Are governments equipped to use cryptocurrency?**
-
-Whether they are or not, governments never need to touch cryptocurrency to use Secure Internet Voting. The SIV Foundation interfaces with governments using the traditional financial infrastructure, ensuring the process is as easy and straightforward as possible.
-
-**4. Is this partisan?**
-
-Absolutely not. Partisanship is artificially dividing our country, and getting in the way of us collaborating effectively. 99% of Americans agree on 99% of issues, but we find ourselves caught in Lesser-of-Two-Evils traps, strangling dialogue and empowering extremists.  
-
-[SIV.org](https://siv.org) strongly recommends [Approval Voting](https://electionscience.org/library/approval-voting/) to eliminate the Spoiler Effect, and voting method reform more generally.
-
-——
-
-### Submit your question
-
-[ Your Name (optional) ]  
-[ Your Email (optional — if you want a reply) ]  
-[ Your Question ]`
-
 export const Content = () => (
   <div>
     <h1>The SIV Token</h1>
-    <h3>We want to create a way for anyone and everyone to be able to invest in SIV’s growth and success.</h3>
-    <h3>Using powerful new cryptographic tools, we can.</h3>
+    <p>We want to create a way for anyone and everyone to be able to invest in SIV’s growth and success.</p>
+    <p>Using powerful new cryptographic tools, we can.</p>
+
     <hr />
 
     <h2>Simple & Innovative Token Economics:</h2>
-    <h3>All future revenue that SIV earns from governments will be paid in USD dividends to SIV Token Holders.</h3>
+    <p>All future revenue that SIV earns from governments will be paid in USD dividends to SIV Token Holders.</p>
+
+    <hr />
+
+    <h2>Right Now...</h2>
+    <p>
+      The city of San Francisco (as one example) pays{' '}
+      <b>
+        <i>$25 per year per registered voter</i>
+      </b>{' '}
+      (whether they actually vote or not!) to Dominion Election Systems, for paper voting equipment.
+    </p>
+    <p>
+      The US government — aka, we the taxpayers — spends $4 billion every year on election administration. $8b in 2020
+      because of COVID.
+    </p>
+
+    <hr />
+
+    <h2>SIV has created far superior technology:</h2>
+    <ul>
+      <li>Easier</li>
+      <li>Faster </li>
+      <li>End-to-end Verifiable </li>
+      <li>Cryptographically Private </li>
+      <li>Fully Auditable </li>
+      <li>Pandemic-Resistant</li>
+    </ul>
+    <p>
+      This allows every day citizens like you and me to vote from the internet-connected supercomputers in our pockets.
+    </p>
+    <p>
+      And all in a way that creates <i>more trust</i> than the current election systems we use right now…
+    </p>
+    <p>
+      SIV allows us to verify for ourselves that our vote is cast and counted correctly, without tampering, and allows
+      each and every one of us to fully recount the vote for ourselves if we want.
+    </p>
+    <p>No more vote-counts trapped behind closed doors, behind any curtains. </p>
+    <p>
+      Each and every one of us can see for ourselves what the correct results are supposed to be — all{' '}
+      <b>
+        <i>while preserving our individual vote privacy</i>
+      </b>
+      .
+    </p>
+    <p>
+      Learn more how it works: <a href="https://siv.org/protocol">The Secure Internet Voting (SIV) Protocol</a>
+    </p>
+    <hr />
+    <h2> But! There is one major barrier… </h2>
+    <p>
+      In order to be able to use Secure Internet Voting,{' '}
+      <b>
+        <i>we the citizens</i>
+      </b>{' '}
+      have to first be offered SIV as an option.
+    </p>
+    <p>This requires buy-in from:</p>
+    <ul>
+      <li> our election officials — many of whom are hard-working, honest public servants!</li>
+      <li>
+        our state legislators — who are very busy, and have trouble knowing what citizens want them to prioritize!
+      </li>
+    </ul>
+
+    <hr />
+
+    <h2>This is where you come in!</h2>
+    <p>
+      We at <a href="https://siv.org">SIV.org</a> have spent the last 2 years reaching out and pitching to hundreds of
+      election officials, throughout the country.
+    </p>
+    <p>
+      Although it’s been an uncountable number of hours, this is only a drop in the bucket, as throughout the US there
+      are over 3000 counties running elections, each with many different people involved.
+    </p>
+    <p>
+      We’ve heard from them over and over again: “I know electronic voting is the way things are of course eventually
+      heading, but are we ready yet?” For VERY GOOD REASONS, these officials are technologically conservative &
+      risk-averse.
+    </p>
+    <p>
+      It’s naturally frightening to go first or try new things. Especially with something so vital as{' '}
+      <b>
+        <i>elections</i>
+      </b>
+      .
+    </p>
+
+    <hr />
+
+    <h2>They need to SEE & HEAR from us citizens that this is something that we want.</h2>
+    <p>
+      Whenever you get the opportunity, please tell them you want to be able to vote from your phone in an easy, safe,
+      and secure way.
+    </p>
+    <p>
+      Tell them you’re excited about <a href="https://siv.org">SIV.org</a>, and would like to be{' '}
+      <a href="">able to try it out</a>!
+    </p>
+    <p>
+      Best is if you talk to them in person, or call them on the phone. Be polite. They’re human too, of course, and
+      appreciate a positive impression.
+    </p>
+
+    <hr />
+
+    <h2> We’ve also made it easy to send them a note yourself </h2>
+    <p>
+      You can write your message here. We’ll find your specific election officials & representatives, forward your
+      message, and let you know of any replies.
+    </p>
+    <p>[ Your name ] [ Your ZIP Code ] [ Your email (optional) ] [ Your message ]</p>
+
+    <hr />
+
+    <h2>AND! You can personally invest in SIV’s Growth & Future Success.</h2>
+    <p>Directly, right now, without needing anyone’s permission.</p>
+    <h3>How it works:</h3>
+    <p>1) You send USD, and receive SIV Tokens.</p>
+    <p>
+      2) <a href="https://siv.org/">SIV.org</a> can immediately make use of your investment, to further our mission of
+      letting all of us vote quickly, easily, and safely from our phones. All of our finances are transparently
+      published.
+    </p>
+    <p>
+      3) Everyone, including our election officials, can see that citizens really believe in, want SIV to exist, and be
+      more widely available. This furthers our mission, and allows us to overcome the network effects needed to create{' '}
+      <b>
+        <i>Legislative Change</i>
+      </b>{' '}
+      — legalizing Secure Internet Voting for citizens in your area.
+    </p>
+    <p>
+      4) 100% of all future SIV revenue from governments is legally mandated to be distributed proportionally to SIV
+      Token Holders. <a href="https://siv.org/">SIV.org</a> itself is a Nonprofit Foundation, and all of our code is
+      open-source.
+    </p>
+    <ul>
+      <li>SIV Tokens are a liquid asset, built using cryptocurrency, and can be sold at any time. </li>
+      <li>
+        To make things easier, we are also happy to help custody any American’s SIV Tokens as long as needed, collecting
+        dividends until ready to withdraw, with zero fees.
+      </li>
+    </ul>
+
+    <hr />
+
+    <h2>Democracy is under siege worldwide.</h2>
+    <p>We’re doing what we can to make our representative governments more trustworthy, accessible, and modern.</p>
+
+    <p>
+      <b>
+        <i>We invite you to join us.</i>
+      </b>
+    </p>
+
+    <hr />
+
+    <h2>Ready to Invest in the Future of Free Society?</h2>
+    <p>[ Your Name ] [ Your Email ] [ Your Preferred Investment Amount ]</p>
+    <p>
+      <i>Payment options: Credit/Debit Card, Paypal, Bank Transfer, Cryptocurrency</i>
+    </p>
+    <p>
+      This SIV Token{' '}
+      <b>
+        <i>is registered</i>
+      </b>{' '}
+      under the Securities Act of 1933. Here is <a href="">our S-1 filing</a>.
+    </p>
+
+    <hr />
+
+    <h2>FAQ</h2>
+    <p>
+      <b>1. When can I start voting from my phone?</b>
+    </p>
+
+    <p>
+      As soon as your local government will allow you. The technology is all ready, and we’ve run a number of highly
+      successful private pilots. The core barrier left is state and local legislative action. Thanks to the federal
+      UOCAVA Act, Secretaries of State and local election officials do have some legal leeway to run small-scale pilots,
+      especially for Overseas Voters (like military voters) and Disabled Voters. But these election officials can always
+      use more public encouragement.
+    </p>
+
+    <p>
+      <b>2. Where can I learn more about how SIV works? How do I know this is secure?</b>
+    </p>
+
+    <ul>
+      <li>
+        <a href="https://siv.org/protocol">Technological Protocol Description</a>
+      </li>
+      <li>
+        <a href="https://siv.org/about">The Peer-Reviewed Computer Science Research SIV is based upon</a>
+      </li>
+      <li>
+        <a href="https://blog.siv.org/2022/04/top-internet-voting-concerns">
+          How SIV Addresses the Top Internet Voting Concerns
+        </a>
+      </li>
+      <li>
+        <a href="https://siv.org/faq">The 34 Most Frequently Asked Questions About Using SIV</a>
+      </li>
+    </ul>
+
+    <p>
+      <b>3. Are governments equipped to use cryptocurrency?</b>
+    </p>
+
+    <p>
+      Whether they are or not, governments never need to touch cryptocurrency to use Secure Internet Voting. The SIV
+      Foundation interfaces with governments using the traditional financial infrastructure, ensuring the process is as
+      easy and straightforward as possible.
+    </p>
+
+    <p>
+      <b>4. Is this partisan?</b>
+    </p>
+
+    <p>
+      Absolutely not. Partisanship is artificially dividing our country, and getting in the way of us collaborating
+      effectively. 99% of Americans agree on 99% of issues, but we find ourselves caught in Lesser-of-Two-Evils traps,
+      strangling dialogue and empowering extremists.
+    </p>
+
+    <p>
+      <a href="https://siv.org/">SIV.org</a> strongly recommends{' '}
+      <a href="https://electionscience.org/library/approval-voting/">Approval Voting</a> to eliminate the Spoiler
+      Effect, and voting method reform more generally.
+    </p>
+
+    <hr />
+
+    <h2>Submit your question</h2>
+    <p>[ Your Name (optional) ] [ Your Email (optional — if you want a reply) ] [ Your Question ]</p>
   </div>
 )
