0befc5e6e6208a5948ca4a83a70a2c56b81cec49
David Ernst <git@dsernst.com>
2022-10-18 21:27:26 -0700

    registration-page: Frontend form to apply to vote

--- src/vote/EnterAuthToken.tsx
+++ src/vote/EnterAuthToken.tsx
@@ -17,74 +17,82 @@ export const EnterAuthToken = () => {
 
   return (
     <div className="enter-auth-token">
-      <h1>To cast a vote...</h1>
-      {voter_applications_allowed ? (
-        <div style={{ textAlign: 'center' }}>
-          <OnClickButton
-            style={{ margin: 0, marginTop: 20, padding: '10px 15px' }}
-            onClick={() => setOpenedVoterAuthInput(!openedVoterAuthInput)}
-          >
-            Enter your Voter Authorization Token
-          </OnClickButton>
-        </div>
-      ) : (
-        <p>Enter your Voter Authorization Token:</p>
-      )}
-      {openedVoterAuthInput && (
-        <>
-          <div className="row" style={{ marginTop: 25 }}>
-            <TextField
-              autoFocus
-              InputLabelProps={{ style: { fontSize: 22 } }}
-              InputProps={{ style: { fontSize: 22 } }}
-              error={!!error}
-              helperText={error}
-              label="Auth token"
-              style={{ flex: 1, fontSize: 20 }}
-              variant="outlined"
-              onChange={(event) => {
-                setError('')
-
-                try {
-                  testAuthToken(event.target.value)
-                } catch (e) {
-                  if (typeof e === 'string') return setError(e)
-                  setError('Caught error w/o message')
-                }
-
-                setText(event.target.value)
-              }}
-              onKeyPress={(event) => event.key === 'Enter' && submitBtn.current?.click()}
-            />
+      <div className="max-w-[350px] mx-auto">
+        <h1>To cast a vote...</h1>
+        {voter_applications_allowed ? (
+          <div style={{ textAlign: 'center' }}>
             <OnClickButton
-              disabled={text.length !== 10 || !!error}
-              ref={submitBtn}
-              style={{ margin: 0, marginLeft: 10, padding: '19px 15px' }}
-              onClick={() => {
-                // Update auth in URL
-                const url = new URL(window.location.toString())
-                url.searchParams.set('auth', text.toLowerCase())
-                router.push(url)
-              }}
+              style={{ margin: 0, marginTop: 20, padding: '10px 15px' }}
+              onClick={() => setOpenedVoterAuthInput(!openedVoterAuthInput)}
             >
-              Submit
+              Enter your Voter Authorization Token
             </OnClickButton>
           </div>
-          <p className="grey">
-            <i>Example:</i> 22671df063
-            <br />
-            <br />
-            Auth tokens are 10 characters long, made up of the numbers <i>0–9</i> and the letters <i>a–f</i>.
-            <br />
-            <br />
-            Unique for each election.
-          </p>
-        </>
-      )}
+        ) : (
+          <p>Enter your Voter Authorization Token:</p>
+        )}
+
+        {openedVoterAuthInput && (
+          <>
+            <div className="row" style={{ marginTop: 25 }}>
+              <TextField
+                autoFocus
+                InputLabelProps={{ style: { fontSize: 22 } }}
+                InputProps={{ style: { fontSize: 22 } }}
+                error={!!error}
+                helperText={error}
+                label="Auth token"
+                style={{ flex: 1, fontSize: 20 }}
+                variant="outlined"
+                onChange={(event) => {
+                  setError('')
+
+                  try {
+                    testAuthToken(event.target.value)
+                  } catch (e) {
+                    if (typeof e === 'string') return setError(e)
+                    setError('Caught error w/o message')
+                  }
+
+                  setText(event.target.value)
+                }}
+                onKeyPress={(event) => event.key === 'Enter' && submitBtn.current?.click()}
+              />
+              <OnClickButton
+                disabled={text.length !== 10 || !!error}
+                ref={submitBtn}
+                style={{ margin: 0, marginLeft: 10, padding: '19px 15px' }}
+                onClick={() => {
+                  // Update auth in URL
+                  const url = new URL(window.location.toString())
+                  url.searchParams.set('auth', text.toLowerCase())
+                  router.push(url)
+                }}
+              >
+                Submit
+              </OnClickButton>
+            </div>
+            <p className="grey">
+              <i>Example:</i> 22671df063
+              <br />
+              <br />
+              Auth tokens are 10 characters long, made up of the numbers <i>0–9</i> and the letters <i>a–f</i>.
+              <br />
+              <br />
+              Unique for each election.
+            </p>
+          </>
+        )}
+      </div>
 
       {voter_applications_allowed && (
         <div className="application">
-          <h2>— &nbsp; or &nbsp; —</h2>
+          <h2 className="my-12">— &nbsp; or &nbsp; —</h2>
+
+          <section className="py-2 mb-6 text-lg rounded bg-blue-900/10">
+            The Election Administrator is accepting applications to join this vote.
+          </section>
+
           <OnClickButton
             style={{ margin: 0, padding: '10px 15px' }}
             onClick={() => setOpenedRegistration(!openedRegistration)}
@@ -97,7 +105,6 @@ export const EnterAuthToken = () => {
 
       <style jsx>{`
         .enter-auth-token {
-          max-width: 350px;
           margin: 0 auto;
         }
 
@@ -116,7 +123,6 @@ export const EnterAuthToken = () => {
         }
 
         h2 {
-          margin: 30px 0;
           font-weight: 500;
           letter-spacing: -1px;
           font-style: italic;
--- src/vote/VoterRegistrationForm.tsx
+++ src/vote/VoterRegistrationForm.tsx
@@ -1,12 +1,78 @@
+import { TextField } from '@material-ui/core'
+import { useRef, useState } from 'react'
+import { OnClickButton } from 'src/_shared/Button'
+
 export const VoterRegistrationForm = () => {
+  const [error, setError] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const submitBtn = useRef<HTMLAnchorElement>(null)
+
   return (
-    <section className="text-lg rounded bg-blue-900/20">
-      The Election Administrator is accepting applications to join this vote.
-      <style jsx>{`
-        section {
-          margin-top: 15px;
-        }
-      `}</style>
+    <section className="text-lg">
+      <p>Please provide your identifying information, then you can cast your vote.</p>
+      <div className="flex flex-col mb-4 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-3 row">
+        <TextField
+          autoFocus
+          InputLabelProps={{ style: { fontSize: 22 } }}
+          InputProps={{ style: { fontSize: 22 } }}
+          error={!!error}
+          helperText={error}
+          label="First Name"
+          style={{ flex: 1, fontSize: 20 }}
+          variant="outlined"
+          onChange={(event) => {
+            setError('')
+
+            setFirstName(event.target.value)
+          }}
+        />
+        <TextField
+          InputLabelProps={{ style: { fontSize: 22 } }}
+          InputProps={{ style: { fontSize: 22 } }}
+          error={!!error}
+          helperText={error}
+          label="Last Name"
+          style={{ flex: 1, fontSize: 20 }}
+          variant="outlined"
+          onChange={(event) => {
+            setError('')
+
+            setLastName(event.target.value)
+          }}
+        />
+      </div>
+      <div className="flex mx-auto mb-4 sm:max-w-md">
+        <TextField
+          InputLabelProps={{ style: { fontSize: 22 } }}
+          InputProps={{ style: { fontSize: 22 } }}
+          error={!!error}
+          helperText={error}
+          label="Email (to be verified)"
+          style={{ flex: 1, fontSize: 20 }}
+          variant="outlined"
+          onChange={(event) => {
+            setError('')
+
+            setEmail(event.target.value)
+          }}
+          onKeyPress={(event) => event.key === 'Enter' && submitBtn.current?.click()}
+        />
+      </div>
+      <OnClickButton
+        disabled={!firstName || !lastName || !email || !!error}
+        ref={submitBtn}
+        style={{ margin: 0, padding: '19px 15px' }}
+        onClick={() => {
+          // Update auth in URL
+          // const url = new URL(window.location.toString())
+          // url.searchParams.set('auth', text.toLowerCase())
+          // router.push(url)
+        }}
+      >
+        Continue to Vote
+      </OnClickButton>
     </section>
   )
 }
