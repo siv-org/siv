3deffcb920fb28b8ffea3575da2a590d8c058303
David Ernst <git@dsernst.com>
2023-04-22 16:07:50 +0300

    Script to view admin's elections numVoters x numCols

---
+++ db-data/2023-04-22-get-ballot-design-columns.ts
@@ -0,0 +1,31 @@
+// Execute this file with:
+// npx ts-node db-data/2023-04-22-get-ballot-design-columns.ts
+
+import './_env'
+
+import { firebase } from '../pages/api/_services'
+
+// CHANGE ME 👇
+const admin_email = ''
+
+// Find all elections created by them
+;(async () => {
+  const elections = (
+    await firebase
+      .firestore()
+      .collection('elections')
+      .where('creator', '==', admin_email)
+      .orderBy('created_at', 'desc')
+      .get()
+  ).docs
+
+  console.log(`Found ${elections.length} elections\n`)
+
+  elections.forEach((election, n) => {
+    const data = election.data()
+    const { ballot_design, election_title, num_voters } = data
+    const numCols = JSON.parse(ballot_design || []).length
+
+    console.log(`${n}. ${election_title}: ${num_voters} voters x ${numCols} col`)
+  })
+})()
