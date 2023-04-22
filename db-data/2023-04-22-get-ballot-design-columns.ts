a4804d07d79a5cff03fae048a04da999a5c6265d
David Ernst <git@dsernst.com>
2023-04-22 21:39:22 +0300

    Improve script to view multiple elections' status

--- db-data/2023-04-22-get-ballot-design-columns.ts
+++ db-data/2023-04-22-get-ballot-design-columns.ts
@@ -7,6 +7,7 @@ import { firebase } from '../pages/api/_services'
 
 // CHANGE ME 👇
 const admin_email = ''
+const min_votes = 0
 
 // Find all elections created by them
 ;(async () => {
@@ -19,13 +20,22 @@ const admin_email = ''
       .get()
   ).docs
 
+  console.log('Current time:', new Date().toLocaleString())
   console.log(`Found ${elections.length} elections\n`)
 
-  elections.forEach((election, n) => {
+  if (min_votes > 0) console.log('Filtering for at least', min_votes, 'votes cast:')
+
+  elections.slice(0, 17).forEach((election, n) => {
     const data = election.data()
-    const { ballot_design, election_title, num_voters, num_votes } = data
+    const { ballot_design, decrypted = [], election_title, num_voters, num_votes } = data
+    if (num_votes < min_votes) return
+
     const numCols = JSON.parse(ballot_design || []).length
 
-    console.log(`${n + 1}. ${election_title}: ${num_voters} voters x ${numCols} col — ${num_votes} votes`)
+    console.log(
+      `${n + 1}. ${election_title}: ${num_voters} voters x ${numCols} col — ${num_votes} votes, ${
+        decrypted.length
+      } unlocked`,
+    )
   })
 })()
