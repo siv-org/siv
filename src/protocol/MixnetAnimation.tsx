ddb86905538ebdf72451d45232d82f02b5c00647
Ariana Ivan <git@arianaivan.com>
2023-05-25 14:10:22 -0700

    Protocol Page: Replace Verifying Observers with Privacy Protectors

--- src/protocol/MixnetAnimation.tsx
+++ src/protocol/MixnetAnimation.tsx
@@ -1,4 +1,6 @@
 import { Animation } from 'src/status/Mixnet/Animation'
 import { range } from 'src/utils'
 
-export const MixnetAnimation = () => <Animation protocolPage observers={range(1, 6).map((i) => `Observer #${i}`)} />
+export const MixnetAnimation = () => (
+  <Animation protocolPage observers={range(1, 6).map((i) => `Privacy Protector #${i}`)} />
+)
