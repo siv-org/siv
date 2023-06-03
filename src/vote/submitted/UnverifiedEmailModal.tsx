acc35ec398101a353633e1c65f8f81101da374e1
Ariana Ivan <git@arianaivan.com>
2023-06-02 18:43:17 -0700

    Submitted page: if i haven't verified email, will display message to verify email

--- src/vote/submitted/UnverifiedEmailModal.tsx
+++ src/vote/submitted/UnverifiedEmailModal.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import { api } from 'src/api-helper'
 
 export const UnverifiedEmailModal = () => {
-  const [isModalOpen, setModalOpen] = useState(true)
+  const [isModalOpen, setModalOpen] = useState(false)
   const [email, setEmail] = useState('')
   const { auth, election_id } = useRouter().query as { auth?: string; election_id?: string }
 
@@ -11,42 +11,46 @@ export const UnverifiedEmailModal = () => {
     async function getVerificationStatus() {
       const response = await api(`election/${election_id}/get-application-status`, { auth })
 
+      if (response.status >= 400) return
       const { email } = await response.json()
       setEmail(email)
-      if (email == 'Verified') setModalOpen(false)
+      if (email.includes('@')) setModalOpen(true)
     }
     getVerificationStatus()
   }, [])
   return (
-    <div className={`${isModalOpen ? 'bg-gray-900/60' : ''} absolute inset-0 z-20`}>
-      {/* Modal */}
-      {isModalOpen && (
-        <div className="flex flex-col items-center justify-center h-full">
-          <div className="p-3 bg-white rounded-lg">
-            <div className="flex justify-end">
-              {/* Close Button */}
-              <a
-                className="text-gray-500 bg-transparent shadow-none hover:text-gray-700"
-                onClick={() => setModalOpen(false)}
-              >
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+    <div>
+      {email && email.includes('@') && <p>A verification email was sent to {email} </p>}
+      <div className={`${isModalOpen ? 'bg-gray-900/60' : ''} absolute inset-0 z-20`}>
+        {/* Modal */}
+        {isModalOpen && (
+          <div className="flex flex-col items-center justify-center h-full">
+            <div className="p-3 bg-white rounded-lg">
+              <div className="flex justify-end">
+                {/* Close Button */}
+                <a
+                  className="text-gray-500 bg-transparent shadow-none hover:text-gray-700"
+                  onClick={() => setModalOpen(false)}
                 >
-                  <path d="M6 18L18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} />
-                </svg>
-              </a>
-            </div>
-            <div className="p-4 text-center">
-              {/* Message Box */}
-              <p className="text-lg">A Verification Email has been sent to {email}.</p>
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M6 18L18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} />
+                  </svg>
+                </a>
+              </div>
+              <div className="p-4 text-center">
+                {/* Message Box */}
+                <p className="text-lg">A Verification Email has been sent to {email}.</p>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   )
 }
