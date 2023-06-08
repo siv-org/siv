993ea9033d2493be5f866d81c8b624deeeec1104
Ariana Ivan <git@arianaivan.com>
2023-06-08 16:45:29 -0700

    Invalidate Voters: simplify code

--- pages/api/election/[election_id]/admin/invalidate-voters.ts
+++ pages/api/election/[election_id]/admin/invalidate-voters.ts
@@ -1,6 +1,6 @@
+import { firebase, sendEmail } from 'api/_services'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { firebase } from '../../../_services'
 import { checkJwtOwnsElection } from '../../../validate-admin-jwt'
 import { Voter } from './load-admin'
 
@@ -17,35 +17,45 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       const db = firebase.firestore()
       const voterRef = db.collection('elections').doc(election_id).collection('voters').doc(voter.email)
 
+      // Do all in parallel
       return Promise.all([
+        // Mark the auth token as invalidated
         voterRef.update({ invalidated_at: new Date() }),
-        invalidateVotes(election_id, voter.auth_token),
-        sendEmail(voter.email),
+
+        // If votes were cast with this auth, move them to an 'invalidated-votes' collection
+        (async function invalidateVotes() {
+          const votes = await db
+            .collection('elections')
+            .doc(election_id)
+            .collection('votes')
+            .where('auth', '==', voter.auth_token)
+            .get()
+
+          await Promise.all(
+            votes.docs.map(async (vote) => {
+              const invalidatedVote = { ...vote.data(), invalidated_at: new Date() }
+              await db
+                .collection('elections')
+                .doc(election_id)
+                .collection('invalidated_votes')
+                .doc(vote.id)
+                .set(invalidatedVote)
+              await vote.ref.delete()
+            }),
+          )
+        })(),
+
+        // Notify the voter over email
+        sendEmail({
+          recipient: voter.email,
+          subject: 'Your vote has been invalidated',
+          text: `The election administrator invalidated your submitted vote.
+        
+        If you believe this was an error, you can press reply to write to the Election Administrator.`,
+        }),
       ])
     }),
   )
 
   return res.status(201).json({ message: 'Done' })
-}
-
-async function invalidateVotes(election_id: string, auth: string) {
-  const db = firebase.firestore()
-  const votes = await db.collection('elections').doc(election_id).collection('votes').where('auth', '==', auth).get()
-
-  await Promise.all(
-    votes.docs.map(async (vote) => {
-      const invalidatedVote = { ...vote.data(), invalidated_at: new Date() }
-      await db
-        .collection('elections')
-        .doc(election_id)
-        .collection('invalidated_votes')
-        .doc(vote.id)
-        .set(invalidatedVote)
-      await vote.ref.delete()
-    }),
-  )
-}
-
-function sendEmail(voter: string) {
-  console.log(`Sending email to voter: ${voter}`) // Replace this with actual code to send email
 }
