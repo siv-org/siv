106639b6e59f38b6faef66f2276c451e5e7e6567
Ariana Ivan <git@arianaivan.com>
2023-06-10 14:49:39 -0700

    Invalidate Voters: basic styling for error message on /submitted

--- src/vote/submitted/InvalidatedVoteMessage.tsx
+++ src/vote/submitted/InvalidatedVoteMessage.tsx
@@ -9,11 +9,16 @@ export const InvalidatedVoteMessage = () => {
   }
 
   return (
-    <div>
-      <h3>Your Submitted Vote was invalidated.</h3>
+    <div className="p-3 mb-5 bg-red-100 rounded">
+      <h3 className="m-0">Your Submitted Vote was invalidated.</h3>
       <p>If you believe this was in error, you can write a message to the Election Administrator:</p>
       <hr />
-      <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Write your message here..." />
+      <textarea
+        className="w-full h-16 p-1"
+        placeholder="Write your message here..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
       <br />
       <button onClick={handleSubmit}>Submit</button>
     </div>
