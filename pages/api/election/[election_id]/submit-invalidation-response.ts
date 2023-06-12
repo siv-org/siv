253ff025700a4f48e0a07eee87151bb5cec72a4e
Ariana Ivan <git@arianaivan.com>
2023-06-12 15:54:14 -0700

    submit-invalidation-response: store response in the db

--- pages/api/election/[election_id]/submit-invalidation-response.ts
+++ pages/api/election/[election_id]/submit-invalidation-response.ts
@@ -1,12 +1,26 @@
+import { firebase } from 'api/_services'
+import { firestore } from 'firebase-admin'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { auth, message } = req.body
   const { election_id } = req.query
 
-  console.log(auth, message, election_id)
-  // handle the message here
-  // e.g., store in a database or send an email
+  // Store in database on the invalidated_vote doc
+  const votes = await firebase
+    .firestore()
+    .collection('elections')
+    .doc(election_id as string)
+    .collection('invalidated_votes')
+    .where('auth', '==', auth)
+    .get()
+  await Promise.all(
+    votes.docs.map((vote) =>
+      vote.ref.update({ responses: firestore.FieldValue.arrayUnion({ message, timestamp: new Date() }) }),
+    ),
+  )
+
+  // TODO: Send admin email
 
   res.status(200).json({ message: 'Message received' })
 }
