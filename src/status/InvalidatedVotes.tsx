8d84e8da8412eafc5c3221f6435bd5e83cab9845
Ariana Ivan <git@arianaivan.com>
2023-06-14 21:30:42 -0700

    Election Status Page: make invalidated votes table collapsable

--- src/status/InvalidatedVotes.tsx
+++ src/status/InvalidatedVotes.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import useSWR from 'swr'
 
 import { EncryptedVote, subscribeToUpdates } from './AcceptedVotes'
@@ -7,6 +8,7 @@ const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 export const InvalidatedVotes = (): JSX.Element => {
   const { election_id } = useRouter().query
+  const [isTableVisible, setTableVisibility] = useState(false)
 
   const { data: votes, mutate } = useSWR<EncryptedVote[]>(
     !election_id ? null : `/api/election/${election_id}/invalidated-votes`,
@@ -21,23 +23,31 @@ export const InvalidatedVotes = (): JSX.Element => {
   return (
     <section className="p-4 mb-8 bg-white rounded shadow-md">
       <h3 className="mb-1">Invalidated Votes</h3>
-      <p className="text-sm italic opacity-70">These votes were invalidated by the election administrator.</p>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th className="py-2 text-left">Auth Token</th>
-          </tr>
-        </thead>
-        <tbody>
-          {votes.map((vote, index) => (
-            <tr key={index}>
-              <td>{index + 1}.</td>
-              <td className="py-2">{vote.auth}</td>
+      <p className="text-sm italic opacity-70">
+        These votes were invalidated by the election administrator.{' '}
+        <button onClick={() => setTableVisibility(!isTableVisible)}>
+          {isTableVisible ? 'Hide' : 'Show'} Invalidated Votes
+        </button>
+      </p>
+
+      {isTableVisible && (
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th className="py-2 text-left">Auth Token</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {votes.map((vote, index) => (
+              <tr key={index}>
+                <td>{index + 1}.</td>
+                <td className="">{vote.auth}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </section>
   )
 }
