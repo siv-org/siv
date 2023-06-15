8369b362adf2dab36dd39625f35956aa2e57fc61
Ariana Ivan <git@arianaivan.com>
2023-06-14 21:44:41 -0700

    Election Status Page: styling invalidated votes table

--- src/status/InvalidatedVotes.tsx
+++ src/status/InvalidatedVotes.tsx
@@ -24,29 +24,26 @@ export const InvalidatedVotes = (): JSX.Element => {
     <section className="p-4 mb-8 bg-white rounded shadow-md">
       <h3 className="mb-1">Invalidated Votes</h3>
       <p className="text-sm italic opacity-70">
-        These votes were invalidated by the election administrator.{' '}
-        <button onClick={() => setTableVisibility(!isTableVisible)}>
-          {isTableVisible ? 'Hide' : 'Show'} Invalidated Votes
-        </button>
+        {votes.length} vote{votes.length > 1 ? 's were' : ' was'} invalidated by the election administrator.{' '}
+        <a className="text-xs cursor-pointer" onClick={() => setTableVisibility(!isTableVisible)}>
+          {isTableVisible ? 'Hide' : 'Show'}
+        </a>
       </p>
 
       {isTableVisible && (
-        <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th className="py-2 text-left">Auth Token</th>
-            </tr>
-          </thead>
-          <tbody>
-            {votes.map((vote, index) => (
-              <tr key={index}>
-                <td>{index + 1}.</td>
-                <td className="">{vote.auth}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <>
+          <div className="text-xs text-gray-400">Auth Tokens:</div>
+          <table>
+            <tbody>
+              {votes.map((vote, index) => (
+                <tr key={index}>
+                  <td>{index + 1}.</td>
+                  <td className="">{vote.auth}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       )}
     </section>
   )
