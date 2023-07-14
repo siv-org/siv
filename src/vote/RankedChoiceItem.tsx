cb56ebc253b6a7f46fd61bff9e5604ce66d8b6c0
David Ernst <git@dsernst.com>
2023-07-14 01:09:06 -0700

    ranked vote: Use <table> for more consistent alignment

--- src/vote/RankedChoiceItem.tsx
+++ src/vote/RankedChoiceItem.tsx
@@ -27,56 +27,64 @@ export const RankedChoiceItem = ({
     <>
       <TitleDescriptionQuestion {...{ description, question, title }} />
 
-      {/* Top row Choice labels */}
-      <div className="flex ml-24">
-        {new Array(rankings_allowed).fill(0).map((_, index) => (
-          <div className="text-center w-11" key={index}>
-            {getOrdinal(index + 1)}
-          </div>
-        ))}
-      </div>
+      <table className="ml-3">
+        {/* Top row Choice labels */}
+        <thead>
+          <tr>
+            <th />
 
-      <div className="pl-4">
+            {new Array(rankings_allowed).fill(0).map((_, index) => (
+              <th className="text-center" key={index}>
+                {getOrdinal(index + 1)}
+              </th>
+            ))}
+          </tr>
+        </thead>
+
         {/* List one row for each candidate */}
-        {options.map(({ name, sub, value }) => {
-          const val = value || name.slice(0, max_string_length)
+        <tbody>
+          {options.map(({ name, sub, value }) => {
+            const val = value || name.slice(0, max_string_length)
 
-          return (
-            <div className="flex" key={name}>
-              <Label {...{ name, sub }} />
+            return (
+              <tr key={name}>
+                <td className="relative pr-1 bottom-0.5">
+                  <Label {...{ name, sub }} />
+                </td>
 
-              {/* And one column for each ranking option */}
-              {new Array(rankings_allowed).fill(0).map((_, index) => (
-                <div className="ml-2" key={index}>
-                  <input
-                    readOnly
-                    checked={state.plaintext[`${id}_${index + 1}`] === val}
-                    className="w-7 h-7 bg-white border-gray-300 border-solid rounded-full appearance-none cursor-pointer hover:bg-blue-100 checked:!bg-[#002868] checked:border-white/30"
-                    type="radio"
-                    onClick={() => {
-                      const update: Record<string, string> = {}
+                {/* And one column for each ranking option */}
+                {new Array(rankings_allowed).fill(0).map((_, index) => (
+                  <td className="ml-2" key={index}>
+                    <input
+                      readOnly
+                      checked={state.plaintext[`${id}_${index + 1}`] === val}
+                      className="w-7 h-7 bg-white border-gray-300 border-solid rounded-full appearance-none cursor-pointer hover:bg-blue-100 checked:!bg-[#002868] border-2 checked:border-white/30"
+                      type="radio"
+                      onClick={() => {
+                        const update: Record<string, string> = {}
 
-                      // Fill in all unchecked rankings to prevent encryption holes
-                      for (let i = 1; i <= rankings_allowed; i++) {
-                        update[`${id}_${i}`] = state.plaintext[`${id}_${i}`] || 'BLANK'
-                      }
+                        // Fill in all unchecked rankings to prevent encryption holes
+                        for (let i = 1; i <= rankings_allowed; i++) {
+                          update[`${id}_${i}`] = state.plaintext[`${id}_${i}`] || 'BLANK'
+                        }
 
-                      const key = `${id}_${index + 1}`
-                      update[key] = val
-                      // console.log(key, val)
+                        const key = `${id}_${index + 1}`
+                        update[key] = val
+                        // console.log(key, val)
 
-                      // Are they deselecting their existing selection?
-                      if (state.plaintext[key] === val) update[key] = 'BLANK'
+                        // Are they deselecting their existing selection?
+                        if (state.plaintext[key] === val) update[key] = 'BLANK'
 
-                      dispatch(update)
-                    }}
-                  />
-                </div>
-              ))}
-            </div>
-          )
-        })}
-      </div>
+                        dispatch(update)
+                      }}
+                    />
+                  </td>
+                ))}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
 
       <br />
     </>
