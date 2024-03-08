ff841aac5353c41ed827ecbe1570b4d3b63a3909
David Ernst <git@dsernst.com>
2024-03-08 13:32:13 -0600

    IRV: fill out more edge case tests

--- src/status/tallying/rcv-irv.test.ts
+++ src/status/tallying/rcv-irv.test.ts
@@ -117,9 +117,43 @@ describe('IRV tallying', () => {
   })
   test.todo("doesn't crash when there is a tie in the bottom choice")
   test.todo("doesn't crash when there is a tie in the top choice early on")
-  test.todo('handles when a ballot runs out of non-eliminated candidates')
+  test('handles when a ballot runs out of non-eliminated candidates', () => {
+    const results2 = tallyVotes(sampleVotes.ballot_items_by_id, [
+      ...sampleVotes.votes,
+      { president_1: 'BLANK', tracking: '3333-3333-3333' },
+    ])
+    // Since we only added a blank vote, the results should be the same
+    expect(results2.irv.president.rounds[1].tallies).toEqual(results.irv.president.rounds[1].tallies)
+  })
   test.todo('handles when there is a tie in the final round')
   test.todo('handles a ballot w/ multiple IRV items, and other types as well')
-  test.todo('good when someone wins in the first round')
-  test.todo('supports write-ins')
+  test('good when someone wins in the first round', () => {
+    const results2 = tallyVotes(sampleVotes.ballot_items_by_id, [
+      ...sampleVotes.votes,
+      { president_1: 'Bill Clinton', tracking: '3333-3333-3333' },
+      { president_1: 'Bill Clinton', tracking: '4444-4444-4444' },
+    ])
+    const { rounds, winner } = results2.irv.president
+    expect(winner).toEqual('Bill Clinton')
+    expect(rounds.length).toEqual(1)
+    expect(rounds[0].tallies).toEqual({ 'Bill Clinton': 4, 'George H. W. Bu': 2, 'Ross Perot': 1 })
+  })
+  test('supports write-ins', () => {
+    const results2 = tallyVotes(sampleVotes.ballot_items_by_id, [
+      ...sampleVotes.votes,
+      { president_1: 'Abraham Lincoln', tracking: '1111-1111-1111' },
+      { president_1: 'Abraham Lincoln', tracking: '2222-2222-2222' },
+      { president_1: 'Abraham Lincoln', tracking: '3333-3333-3333' },
+      {
+        president_1: 'George H. W. Bu',
+        president_2: 'Ross Perot',
+        president_3: 'Abraham Lincoln',
+        tracking: '4444-4444-4444',
+      },
+    ])
+    const { rounds, winner } = results2.irv.president
+    expect(winner).toEqual('Bill Clinton')
+    expect(rounds.length).toEqual(3)
+    expect(rounds[2].tallies).toEqual({ 'Abraham Lincoln': 4, 'Bill Clinton': 5 })
+  })
 })
