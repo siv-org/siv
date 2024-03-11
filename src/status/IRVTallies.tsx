b6e2eaf385d132a32f2b1864b2216074806a071c
David Ernst <git@dsernst.com>
2024-03-10 23:19:15 -0500

    IRV tally: warn about elimination-ties

--- src/status/IRVTallies.tsx
+++ src/status/IRVTallies.tsx
@@ -33,9 +33,48 @@ export const IRVTallies = ({
             <RoundResults
               {...{ ballot_design, id, ordered: round.ordered, tallies: round.tallies, totalVotes: round.totalVotes }}
             />
+
+            <CheckForTies {...{ index, ordered: round.ordered, tallies: round.tallies }} />
           </div>
         ))}
       </div>
+    </div>
+  )
+}
+
+/** Component to check and warn about ambiguity if there's a tie among who to eliminate */
+const CheckForTies = ({
+  index,
+  ordered,
+  tallies,
+}: {
+  index: number
+  ordered: string[]
+  tallies: Record<string, number>
+}) => {
+  const last_place = ordered.at(-1)
+  if (!last_place) return <></>
+  const last_votes = tallies[last_place]
+
+  const second_last_place = ordered.at(-2)
+  if (!second_last_place) return <></>
+  const second_last_votes = tallies[second_last_place]
+
+  if (last_votes !== second_last_votes) return <></>
+
+  return (
+    <div
+      className="px-1 ml-5 border border-yellow-400 border-solid rounded cursor-pointer text-black/70 hover:bg-yellow-50"
+      onClick={() =>
+        alert(
+          `Round ${
+            index + 1
+          } has multiple tying bottom options.\n\nCurrently, the alphabetically-last gets eliminated, but your election may require different rules.`,
+        )
+      }
+    >
+      Tie among bottom choices.{' '}
+      <span className="px-[4.25px] border border-solid rounded-full opacity-50 text-xs">?</span>
     </div>
   )
 }
