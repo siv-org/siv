7e0264956b9487b9d6e9ab54aa473df1b56e5fcf
David Ernst <git@dsernst.com>
2024-04-09 21:25:32 -0700

    Convert <AdminPage /> css to tailwind

--- src/admin/AdminPage.tsx
+++ src/admin/AdminPage.tsx
@@ -27,46 +27,27 @@ export const AdminPage = (): JSX.Element => {
       <Head title="Create new election" />
 
       <HeaderBar />
-      <main>
+      <main className="flex w-full">
         <Sidebar />
-        <div id="main-content">
+        <div
+          className="w-full p-4 overflow-auto absolute top-[66px] right-0 bottom-0 sm:left-[215px] sm:px-8 sm:w-auto"
+          id="main-content"
+        >
           <MobileMenu />
-          {!election_id && <AllYourElections />}
-          {!election_id && <CreateNewElection />}
+
+          {!election_id && (
+            <>
+              <AllYourElections />
+              <CreateNewElection />
+            </>
+          )}
+
           {section === 'observers' && <Observers />}
           {section === 'ballot-design' && <BallotDesign />}
           {section === 'voters' && <AddVoters />}
           {section === 'marked-ballots' && <MarkedBallots />}
         </div>
       </main>
-
-      <style jsx>{`
-        main {
-          width: 100%;
-          display: flex;
-        }
-
-        #main-content {
-          padding: 1rem 2rem;
-          overflow: auto;
-          position: absolute;
-          left: 215px;
-          top: 66px;
-          right: 0;
-          bottom: 0;
-        }
-
-        /* When sidebar disappears */
-        @media (max-width: 640px) {
-          #main-content {
-            left: 0;
-
-            width: 100%;
-
-            padding: 1rem;
-          }
-        }
-      `}</style>
       <GlobalCSS />
       <style global jsx>{`
         body {
