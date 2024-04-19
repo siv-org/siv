14ad5dc57c9bf037ae58f02e228e965b2a3264bf
David Ernst <git@dsernst.com>
2024-04-19 15:23:50 -0700

    convention: Generate & store unique voter auths for each linked ballot

--- pages/api/conventions/[convention_id]/create-ballot-auths-for-qrs.ts
+++ pages/api/conventions/[convention_id]/create-ballot-auths-for-qrs.ts
@@ -1,4 +1,5 @@
 import { firebase } from 'api/_services'
+import { generateAuthToken } from 'src/crypto/generate-auth-tokens'
 
 /** Given a convention ID and ballot ID, make sure all the current generated QR Codes for that Convention have matching ballot auth tokens */
 export const createBallotAuthsForQrs = async (convention_id: string, ballot_id: string) => {
@@ -6,8 +7,22 @@ export const createBallotAuthsForQrs = async (convention_id: string, ballot_id: 
   const conventionDoc = firebase.firestore().collection('conventions').doc(convention_id)
   const currentQrs = (await conventionDoc.collection('qr_ids').get()).docs.map((d) => d.data())
 
-  console.log(ballot_id, currentQrs)
-
   // Create unique ballot auth tokens for each QR code
-  // Store each ballot auth token in QR doc
+  return Promise.all(
+    currentQrs.map(({ ballot_auths, qr_id }) => {
+      const update = { ...ballot_auths }
+
+      // Does it already have one for this ballot?
+      if (update[ballot_id]) return
+
+      // Generate the auth
+      const newBallotAuthToken = generateAuthToken()
+      update[ballot_id] = newBallotAuthToken
+
+      // TODO: Store the ballot auth in the ballot itself
+
+      // Store the new ballot auth on the QR doc
+      return conventionDoc.collection('qr_ids').doc(qr_id).update({ ballot_auths: update })
+    }),
+  )
 }
