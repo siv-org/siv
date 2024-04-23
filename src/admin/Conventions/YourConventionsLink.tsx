5a5d174231389430cd607e75682c9c285e6cd321
David Ernst <git@dsernst.com>
2024-04-23 16:09:13 -0700

    admin: Add <BetaBadge /> for Conventions

--- src/admin/Conventions/YourConventionsLink.tsx
+++ src/admin/Conventions/YourConventionsLink.tsx
@@ -4,7 +4,9 @@ export const YourConventionsLink = () => {
   return (
     <Link href="/admin/conventions">
       <div className="pb-2 pl-2 pr-4 -ml-2 transition border-2 border-transparent border-solid rounded-lg cursor-pointer sm:pl-4 sm:-ml-4 hover:border-[#002868] mt-14">
-        <h2>Your Conventions</h2>
+        <h2>
+          Your Conventions <BetaBadge />
+        </h2>
         <p>
           Reusable voter credentials, for multiple ballots throughout a day.{' '}
           <a className="ml-3 opacity-70 hover:no-underline">Go {'→'}</a>
@@ -13,3 +15,9 @@ export const YourConventionsLink = () => {
     </Link>
   )
 }
+
+const BetaBadge = () => (
+  <span className="text-[11px] border-solid border border-[#002868]/70 relative bottom-[3px] text-[#002868]/90 font-bold p-1 rounded ml-1">
+    Beta
+  </span>
+)
