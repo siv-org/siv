85ce9f2df30db2d310f2ac4e231eff018ee7554b
David Ernst <git@dsernst.com>
2024-04-26 15:39:24 -0700

    convention admin: Clearer Set redirect button

--- src/admin/Conventions/SetRedirection.tsx
+++ src/admin/Conventions/SetRedirection.tsx
@@ -47,7 +47,7 @@ export const SetRedirection = () => {
 
           {/* Table header */}
           <tr className="text-[12px] opacity-60 [&>*]:p-1 [&>*]:py-2">
-            <th></th>
+            <th />
             <th className="text-left">Ballot name</th>
             <th>Created</th>
             <th>Voters</th>
@@ -61,13 +61,17 @@ export const SetRedirection = () => {
         {filteredData.map((e, i) => {
           const is_active_redirect = e.id === active_redirect
           return (
+            // Each row
             <tr
               className={`text-center py-1 px-1.5 rounded hover:bg-gray-200 group [&>td]:px-1 ${
                 is_active_redirect ? 'bg-blue-800/20 hover:!bg-blue-800/30' : ''
               }`}
               key={e.id}
             >
+              {/* # */}
               <td className="opacity-50 text-[11px]">{i + 1}</td>
+
+              {/* Ballot name */}
               <td className="max-w-[300px] text-left">
                 <a
                   className="text-black/90 hover:text-blue-700"
@@ -78,15 +82,26 @@ export const SetRedirection = () => {
                   {e.election_title}
                 </a>
               </td>
+
+              {/* Created */}
               <td className="opacity-50 text-[11px]">
                 <TimeAgo datetime={new Date(e.created_at._seconds * 1000)} />
               </td>
+
+              {/* Voters */}
               <td className={!e.num_voters ? 'opacity-30' : ''}>{e.num_voters}</td>
+
+              {/* Votes */}
               <td className={!e.num_votes ? 'opacity-30' : ''}>{e.num_votes}</td>
+
+              {/* Finalized? */}
               <td className="!px-2">
                 <input disabled checked={e.ballot_design_finalized && !!e.threshold_public_key} type="checkbox" />
+
+                {/* Hints if active but not finalized */}
                 {is_active_redirect && (
                   <>
+                    {/* Ballot not finalized */}
                     {!e.ballot_design_finalized ? (
                       <div>
                         <span className="text-[8px] relative bottom-0.5 opacity-70 hover:no-underline">❌ </span>
@@ -102,6 +117,8 @@ export const SetRedirection = () => {
                     ) : (
                       ''
                     )}
+
+                    {/* Observers not set */}
                     {!e.threshold_public_key ? (
                       <div className="text-left">
                         <span className="text-[8px] relative bottom-0.5 opacity-70">❌ </span>
@@ -121,9 +138,9 @@ export const SetRedirection = () => {
                 )}
               </td>
 
-              {/* 'Set' hover hint */}
+              {/* Redirect: Set / Active */}
               <td
-                className={`text-xs cursor-pointer group-hover:opacity-30 ${
+                className={`text-xs cursor-pointer group-hover:opacity-60 ${
                   is_active_redirect ? '!opacity-60' : 'opacity-0'
                 } hover:!opacity-80 hover:bg-blue-800/30`}
                 onClick={async () =>
@@ -136,7 +153,11 @@ export const SetRedirection = () => {
                   revalidate(convention_id || '')
                 }
               >
-                {is_active_redirect ? 'Active' : 'Set'}
+                {is_active_redirect ? (
+                  'Active'
+                ) : (
+                  <span className="px-3 bg-white border border-solid rounded-lg border-black/50">Set</span>
+                )}
               </td>
             </tr>
           )
