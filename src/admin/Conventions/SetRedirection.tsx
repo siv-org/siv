6e5b8336c52714eb9d85831b4d21fd6da6b1bd58
David Ernst <git@dsernst.com>
2024-04-25 17:59:04 -0700

    conventions admin: Begin improving SetRedirection UI

--- src/admin/Conventions/SetRedirection.tsx
+++ src/admin/Conventions/SetRedirection.tsx
@@ -40,34 +40,62 @@ export const SetRedirection = () => {
       {!filteredData.length && <p className="italic opacity-70">No elections found</p>}
 
       {/* Filtered list */}
-      <ol className="h-64 px-1.5 py-2 mb-4 overflow-y-scroll list-none rounded-lg shadow-inner bg-neutral-50">
+      <table className="mt-1 h-64 px-1.5 py-2 mb-4 overflow-y-scroll rounded-lg shadow-inner bg-neutral-50">
+        <thead>
+          <tr className="text-[12px] opacity-70 [&>*]:px-1">
+            <th></th>
+            <th className="text-left">Ballot name</th>
+            <th>Created</th>
+            <th>Voters</th>
+            <th>Linked QRs</th>
+            <th>Votes Cast</th>
+            <th>Manage</th>
+            <th>Finalized?</th>
+            <th>Redirect</th>
+          </tr>
+        </thead>
         {filteredData.map(({ created_at, election_title, id }, i) => (
-          <li
-            className={`flex justify-between items-center py-1 px-1.5 rounded cursor-pointer hover:bg-gray-200 group ${
+          <tr
+            className={`text-center py-1 px-1.5 rounded hover:bg-gray-200 group ${
               active_redirect === id ? 'bg-blue-800/20 hover:!bg-blue-800/30' : ''
             }`}
             key={id}
-            onClick={async () =>
-              confirm(`${active_redirect !== id ? `Redirect QRs to '${election_title}'?` : 'Remove redirection?'}`) &&
-              (await api(`/conventions/${convention_id}/set-redirect`, {
-                election_id: active_redirect !== id ? id : '',
-              })) &&
-              revalidate(convention_id || '')
-            }
           >
-            <span>
-              <span className="mr-0.5 opacity-50">{i + 1}. </span>
-              <span className="relative">{election_title} </span>
-              <span className="opacity-50">
-                <TimeAgo datetime={new Date(created_at._seconds * 1000)} />
-              </span>
-            </span>
+            <td className="opacity-50 text-[11px]">{i + 1}</td>
+            <td className="max-w-[300px] text-left">{election_title} </td>
+            <td className="opacity-50">
+              <TimeAgo datetime={new Date(created_at._seconds * 1000)} />
+            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td className="cursor-pointer">
+              <a href={`/admin/${id}/voters`} rel="noreferrer" target="_blank">
+                🔗
+              </a>
+            </td>
+            <td>
+              <input checked={false} type="checkbox" />
+            </td>
 
             {/* 'Set' hover hint */}
-            <span className="text-xs opacity-0 group-hover:opacity-30">Set</span>
-          </li>
+            <td
+              className={`text-xs cursor-pointer group-hover:opacity-30 ${
+                active_redirect === id ? 'opacity-60' : 'opacity-0'
+              } hover:!opacity-80 hover:bg-blue-800/30`}
+              onClick={async () =>
+                confirm(`${active_redirect !== id ? `Redirect QRs to '${election_title}'?` : 'Remove redirection?'}`) &&
+                (await api(`/conventions/${convention_id}/set-redirect`, {
+                  election_id: active_redirect !== id ? id : '',
+                })) &&
+                revalidate(convention_id || '')
+              }
+            >
+              {active_redirect === id ? 'Active' : 'Set'}
+            </td>
+          </tr>
         ))}
-      </ol>
+      </table>
     </div>
   )
 }
