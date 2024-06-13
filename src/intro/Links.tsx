c1233252e1e7df42c25da10771cbd94eac1489b6
David Ernst <git@dsernst.com>
2024-06-13 05:09:39 -0700

    intro: Begin adding available links

--- src/intro/Links.tsx
+++ src/intro/Links.tsx
@@ -2,26 +2,33 @@ import { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 type Text = string
-type Href = string | false
+type Href = string
 type CustomClasses = string
 const linkArray: [Text, Href?, CustomClasses?][] = [
-  ['Demo — How SIV Works', false, 'border-green-800 bg-green-200'],
-  ['Bring to my area', false, 'border-orange-800 bg-orange-200'],
-  ['Create Own SIV Election', false, 'border-blue-800 bg-blue-200'],
-  ['Questions? FAQ', false, 'border-gray-400 bg-gray-200'],
+  ['Demo — How SIV Works', undefined, 'border-green-800 bg-green-200'],
+  ['Bring to my area', '/#let-your-govt-know', 'border-orange-800 bg-orange-200'],
+  ['Create Own SIV Election', '/login', 'border-blue-800 bg-blue-200'],
+  ['Questions? FAQ', '/faq', 'border-gray-400 bg-gray-200'],
   [
     'Earn up to $10,000 for finding Security Flaws',
-    false,
+    undefined,
     'border-purple-800 text-purple-700 bg-purple-100 bg-opacity-0',
   ],
-  ['Compare vs Paper', false, 'border-amber-800 bg-amber-800 bg-opacity-10 hover:bg-opacity-20 active:bg-opacity-30'],
-  ['Contribute', false, 'border-pink-800 bg-pink-200'],
-  ['Blog', false, 'border-gray-600 bg-gray-50 hover:bg-gray-100 active:bg-gray-200/80'],
-  ['Technical Documentation'],
-  ['CS Research Powering SIV'],
-  ['Spoiler-Free Voting Methods'],
-  ['Preventing Future Authoritarianism'],
-  ['Use in War Zones'],
+  [
+    'Compare vs Paper',
+    'https://docs.siv.org/compare',
+    'border-amber-800 bg-amber-800 bg-opacity-10 hover:bg-opacity-20 active:bg-opacity-30',
+  ],
+  ['Contribute', undefined, 'border-pink-800 bg-pink-200'],
+  ['Blog', 'https://blog.siv.org', 'border-gray-600 bg-gray-50 hover:bg-gray-100 active:bg-gray-200/80'],
+  ['Technical Documentation', 'https://docs.siv.org', 'border-red-800 bg-red-200'],
+  ['CS Research Powering SIV', '/about'],
+  ['Spoiler-Free Voting Methods', 'https://docs.siv.org/benefits#6-spoiler-resistant-voting-methods'],
+  [
+    'Preventing Future Authoritarianism',
+    'https://docs.siv.org/research-in-progress/ukraine#a-powerful-tool-against-future-authoritarianism',
+  ],
+  ['Use in War Zones', '/ukraine'],
   ['Malware-Resistant Verification'],
 ]
 
@@ -44,6 +51,8 @@ const SingleLink = ({ customClasses, href, text }: { customClasses: CustomClasse
           'block p-2 mb-2 border rounded-lg font-medium bg-opacity-50 hover:bg-opacity-70 active:bg-opacity-100 cursor-pointer',
           customClasses,
         )}
+        href={href}
+        rel="noreferrer"
         target="_blank"
         onClick={() => {
           if (href) return
