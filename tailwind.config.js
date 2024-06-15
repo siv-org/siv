ea318069ddaaf9cf75d844d06300143679d1ce26
Ariana Ivan <git@arianaivan.com>
2024-06-15 14:30:54 -0700

    Fix tailwind default variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx'],
+  corePlugins: {
+    preflight: false,
+  },
   plugins: [],
   theme: { extend: {} },
 }
