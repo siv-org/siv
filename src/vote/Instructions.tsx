701349538e5d5f8ab65864e8bbd59e7171cfb37d
David Ernst <git@dsernst.com>
2024-07-04 18:23:37 +0300

    /vote: Convert <Instructions /> to tailwind

--- src/vote/Instructions.tsx
+++ src/vote/Instructions.tsx
@@ -4,16 +4,8 @@ export const Instructions = () => (
       <b>Instructions:</b> Mark your selections then press <em>Submit</em> at the bottom.
     </p>
     <p>
-      <img src="/vote/lock.png" width="12px" />
+      <img className="relative mr-[7px] top-[3px] opacity-90" src="/vote/lock.png" width="12px" />
       Your choices are encrypted on your own device for strong privacy.
     </p>
-    <style jsx>{`
-      img {
-        margin-right: 7px;
-        position: relative;
-        top: 3px;
-        opacity: 0.9;
-      }
-    `}</style>
   </>
 )
