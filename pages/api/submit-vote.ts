b1019803c4349fd84f7b6065794d42814fad7a98
David Ernst <git@dsernst.com>
2024-07-05 00:35:52 +0300

    submit-vote: JSON.parse(encrypted_vote) if string

--- pages/api/submit-vote.ts
+++ pages/api/submit-vote.ts
@@ -10,7 +10,9 @@ import { pusher } from './pusher'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const payload = req.method === 'POST' ? req.body : req.query
-  const { auth, election_id, encrypted_vote } = payload
+  const { auth, election_id } = payload
+  let { encrypted_vote } = payload
+  if (typeof encrypted_vote === 'string') encrypted_vote = JSON.parse(encrypted_vote)
 
   // return res.status(200).json({ auth, election_id, encrypted_vote })
 
