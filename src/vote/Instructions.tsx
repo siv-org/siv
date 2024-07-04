0a56d1dbdaf21512510cb7f40d30fdca0defbc4b
David Ernst <git@dsernst.com>
2024-07-04 18:38:22 +0300

    /vote instructions: Clarify privacy

--- src/vote/Instructions.tsx
+++ src/vote/Instructions.tsx
@@ -5,7 +5,18 @@ export const Instructions = () => (
     </p>
     <p>
       <img className="relative mr-[7px] top-[3px] opacity-90" src="/vote/lock.png" width="12px" />
-      Your choices are encrypted on your own device for strong privacy.
+      Your choices are encrypted on your own device for strong privacy.{' '}
+      <span className="mt-0 text-black/50 sm:pl-5 sm:block">
+        No one will see how you vote. Everyone will see vote totals.{' '}
+        <a
+          className="underline text-inherit hover:text-black/80"
+          href="https://docs.siv.org/privacy"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Learn more
+        </a>
+      </span>
     </p>
   </>
 )
