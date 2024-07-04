2b119577fa71e6452a1b0f6ccb43d3feb3c660c8
David Ernst <git@dsernst.com>
2024-07-04 11:14:02 +0300

    Cleanup verify-registration-email-code

--- pages/api/election/[election_id]/verify-registration-email-code.ts
+++ pages/api/election/[election_id]/verify-registration-email-code.ts
@@ -3,37 +3,34 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { code, election_id, email } = req.body
+  // Validate the request has require parameters
+  if (!code || !email || !election_id) return res.status(400).json({ error: 'Missing parameters.' })
 
-  try {
-    // Fetch the voter document based on the email
-    const voterSnapshot = await firebase
-      .firestore()
-      .collection('elections')
-      .doc(election_id)
-      .collection('voters')
-      .doc(email)
-      .get()
+  // Lookup the voter doc
+  const voterDoc = await firebase
+    .firestore()
+    .collection('elections')
+    .doc(election_id)
+    .collection('voters')
+    .doc(email)
+    .get()
 
-    // Check if the voter document exists and has the same verification code
-    if (voterSnapshot.exists && code && voterSnapshot.data()?.verification_code === code) {
-      // Update the status to 'verified'
-      await voterSnapshot.ref.update({ is_email_verified: true })
+  // Check if the verification code is good
+  if (!voterDoc.exists || voterDoc.data()?.verification_code !== code) {
+    pushover(
+      'Verify reg-link, bad code',
+      `Email:${email}\n\nInput code: ${code}\nDB code: ${
+        voterDoc.data()?.verification_code
+      }\n\nElection ID: ${election_id}`,
+    )
 
-      // Return a success response
-      return res.status(200).json({ message: 'Email verified successfully.' })
-    } else {
-      pushover(
-        'Reg link verification bad code',
-        `Email:${email}\n\nInput code: ${code}\nDB code: ${
-          voterSnapshot.data()?.verification_code
-        }\n\nElection ID: ${election_id}`,
-      )
-      // Verification failed, return an error response
-      return res.status(400).json({ error: 'Invalid email or verification code.' })
-    }
-  } catch (error) {
-    console.error('Error verifying email:', error)
-    // Return an error response
-    return res.status(500).json({ error: 'Internal server error.' })
+    // Verification failed, return an error response
+    return res.status(400).json({ error: 'Invalid verification code' })
   }
+
+  // Update the status to 'verified'
+  await voterDoc.ref.update({ is_email_verified: true })
+
+  // Return a success response
+  return res.status(200).json({ message: 'Email verified successfully.' })
 }
