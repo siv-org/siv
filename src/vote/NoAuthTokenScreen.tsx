6fca6ae4371280f20f67d7b797c510206e21bbf6
David Ernst <git@dsernst.com>
2024-07-06 18:08:25 +0300

    Remove Auth-then-Vote logic

--- src/vote/NoAuthTokenScreen.tsx
+++ src/vote/NoAuthTokenScreen.tsx
@@ -1,90 +1,41 @@
 import { ElectionInfo } from 'api/election/[election_id]/info'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { Spinner } from 'src/admin/Spinner'
 
-import { OnClickButton } from '../_shared/Button'
+import { Button } from '../_shared/Button'
 import { EnterAuthToken } from './EnterAuthToken'
-import { VoterRegistrationForm } from './VoterRegistrationForm'
 
 export const NoAuthTokenScreen = () => {
   const { election_title, loaded, voter_applications_allowed } = useElectionInfo()
-  const [openedVoterAuthInput, setOpenedVoterAuthInput] = useState(false)
-  const [openedRegistration, setOpenedRegistration] = useState(false)
-
-  // If Registrations allowed, default to opening those, otherwise default to opening Auth Token input.
-  useEffect(() => {
-    setOpenedVoterAuthInput(!voter_applications_allowed)
-    setOpenedRegistration(!!voter_applications_allowed)
-  }, [voter_applications_allowed])
-
-  if (!loaded)
-    return (
-      <h2>
-        <Spinner />
-        Loading...
-      </h2>
-    )
-
-  function EnterAuth() {
-    return (
-      <div className="max-w-[350px] mx-auto">
-        {voter_applications_allowed ? (
-          <div>
-            <OnClickButton
-              style={{ margin: 0, padding: '10px 15px' }}
-              onClick={() => setOpenedVoterAuthInput(!openedVoterAuthInput)}
-            >
-              Enter your Voter Authorization Token
-            </OnClickButton>
-          </div>
-        ) : (
-          <p>Enter your Voter Authorization Token:</p>
-        )}
-
-        {openedVoterAuthInput && <EnterAuthToken />}
-      </div>
-    )
-  }
-
-  function Or() {
-    return <h2 className="my-12 italic font-medium opacity-70">— &nbsp; or &nbsp; —</h2>
-  }
-
-  function EnterRegistrationInfo() {
-    return (
-      <div>
-        <OnClickButton
-          style={{ margin: 0, padding: '10px 15px' }}
-          onClick={() => setOpenedRegistration(!openedRegistration)}
-        >
-          Enter Your Info To Join Election
-        </OnClickButton>
-        {!voter_applications_allowed && (
-          <p className="italic font-semibold text-rose-700">
-            This election is not accepting new voter registrations at this time.
-          </p>
-        )}
-        {openedRegistration && voter_applications_allowed && <VoterRegistrationForm />}
-      </div>
-    )
-  }
 
   return (
     <div className="text-center">
-      <h1>{election_title}</h1>
+      {/* Election title */}
+      <h1>
+        {!loaded ? <i className="text-lg text-gray-300 animate-pulse">Loading election title...</i> : election_title}
+      </h1>
 
-      {!voter_applications_allowed ? (
+      {/* Enter Auth Token */}
+      <div className="max-w-[350px] mx-auto">
+        <div>
+          <h3>Enter your Voter Authorization Token</h3>
+        </div>
+
+        <EnterAuthToken />
+      </div>
+
+      {/* Or... link to Vote-then-Auth */}
+      {voter_applications_allowed && (
         <>
-          <EnterAuth />
-          <Or />
-          <EnterRegistrationInfo />
-        </>
-      ) : (
-        <>
-          <EnterRegistrationInfo />
-          <Or />
-          <EnterAuth />
+          {/* Or */}
+          <h2 className="my-12 italic font-medium opacity-70">— &nbsp; or &nbsp; —</h2>
+
+          {/* Button link to /vote?auth=link */}
+          <div>
+            <Button href={`${window.location.href}?auth=link`} style={{ margin: 0, padding: '10px 15px' }}>
+              Vote then Authenticate
+            </Button>
+          </div>
         </>
       )}
     </div>
