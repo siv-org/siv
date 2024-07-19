2b97ae132b5c2e44450161168aca0a5f97be700e
David Ernst <git@dsernst.com>
2024-07-19 19:04:09 +0300

    duplicate-election script: Use batched writes for speed

--- db-data/2024-07-18-duplicate-election.ts
+++ db-data/2024-07-18-duplicate-election.ts
@@ -27,17 +27,23 @@ async function copySubcollection(
   const sourceSubcollection = sourceDoc.collection(subcollectionName)
   const targetSubcollection = targetDoc.collection(subcollectionName)
 
-  const snapshot = await sourceSubcollection.get()
-  const docs = snapshot.docs
-  const amount = docs.length
+  const { docs } = await sourceSubcollection.get()
   let progress = 0
-  const intervalToReport = 20
 
-  for (const doc of docs) {
-    await targetSubcollection.doc(doc.id).set(doc.data())
-    progress++
-    if (progress % intervalToReport === 0)
-      console.log(`${subcollectionName}: ${progress}/${amount} - ${((progress / amount) * 100).toFixed(0)}%`)
+  // Use firestore batched writes
+  const batchLimit = 500 // max operations per batch
+  for (let i = 0; i < docs.length; i += batchLimit) {
+    const batch: FirebaseFirestore.WriteBatch = sourceDoc.firestore.batch()
+
+    const batchDocs = docs.slice(i, i + batchLimit)
+    for (const doc of batchDocs) {
+      batch.set(targetSubcollection.doc(doc.id), doc.data())
+    }
+
+    await batch.commit()
+    progress += batchDocs.length
+
+    console.log(`${subcollectionName}: ${progress}/${docs.length} - ${((progress / docs.length) * 100).toFixed(0)}%`)
   }
 
   console.log('Finished copy: ' + subcollectionName)
