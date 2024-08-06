cbae52f3595a59a18a3a1aabb229ceccc06d29fb
David Ernst <git@dsernst.com>
2024-08-06 15:48:13 -0700

    Clean up more form submission handling

--- pages/api/ukraine-updates-subscribe.ts
+++ pages/api/ukraine-updates-subscribe.ts
@@ -13,17 +13,19 @@ export default allowCors(async (req: NextApiRequest, res: NextApiResponse) => {
   if (!validateEmail(email)) return res.status(400).json({ error: 'Invalid email' })
 
   // Store submission in Firestore
-  await firebase
-    .firestore()
-    .collection('ukraine-updates')
-    .doc(new Date().toISOString() + ' ' + String(Math.random()).slice(2, 7))
-    .set({
-      created_at: new Date().toString(),
-      email,
-    })
+  await Promise.all([
+    firebase
+      .firestore()
+      .collection('ukraine-updates')
+      .doc(new Date().toISOString() + ' ' + String(Math.random()).slice(2, 7))
+      .set({
+        created_at: new Date().toString(),
+        email,
+      }),
 
-  // Notify admin via Pushover
-  pushover(`SIV ukraine signup`, email)
+    // Notify admin via Pushover
+    pushover(`SIV ukraine signup`, email),
+  ])
 
   // Send back success
   return res.status(201).json({ success: true })
