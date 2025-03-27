fac9c780f0e19d89ae6d90f8c0396a72bf51ccbf
David Ernst <git@dsernst.com>
2025-03-27 15:42:23 -0700

    /protocol memoize detailed encryption receipt for non-deterministic encoding calc

--- src/protocol/EncryptionReceipt.tsx
+++ src/protocol/EncryptionReceipt.tsx
@@ -1,4 +1,5 @@
 import { mapValues, omit } from 'lodash-es'
+import { useMemo } from 'react'
 import { stringToPoint } from 'src/crypto/curve'
 
 import { DetailedEncryptionReceipt } from '../vote/submitted/DetailedEncryptionReceipt'
@@ -8,21 +9,23 @@ import { useVoteContext } from './VoteContext'
 export function EncryptionReceipt(): JSX.Element {
   const { state } = useVoteContext()
 
-  // Convert this protocol demo's state into a compatible object
-  // for the main app's <DetailedEncryptionReceipt />
-  const demoState = {
-    election_title: 'Demo Election',
-    encoded: mapValues(state.plaintext, (value) => stringToPoint(`${state.verification}:${value}`).toHex()),
-    encrypted: mapValues(omit(state.encrypted, 'auth'), (value) => {
-      const [, encrypted, lock] = value?.match(/encrypted: (.*?), lock: (.*?) }/) || []
-      return { encrypted: encrypted || '', lock: lock || '' }
+  // Memoize the expensive calculations
+  const demoState = useMemo(
+    () => ({
+      election_title: 'Demo Election',
+      encoded: mapValues(state.plaintext, (value) => stringToPoint(`${state.verification}:${value}`).toHex()),
+      encrypted: mapValues(omit(state.encrypted, 'auth'), (value) => {
+        const [, encrypted, lock] = value?.match(/encrypted: (.*?),\s*lock: (.*?)\s*}/) || []
+        return { encrypted: encrypted || '', lock: lock || '' }
+      }),
+      plaintext: state.plaintext as Record<string, string>,
+      public_key,
+      randomizer: state.randomizer as Record<string, string>,
+      submitted_at: new Date(),
+      tracking: state.verification,
     }),
-    plaintext: state.plaintext as Record<string, string>,
-    public_key,
-    randomizer: state.randomizer as Record<string, string>,
-    submitted_at: new Date(),
-    tracking: state.verification,
-  }
+    [state.plaintext, state.verification, state.encrypted, state.randomizer],
+  )
 
   return <DetailedEncryptionReceipt auth={voters[0].auth} election_id="123" state={demoState} />
 }
