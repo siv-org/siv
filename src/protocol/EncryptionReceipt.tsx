c4052cd611a845a55929a6918425a363d939b8df
David Ernst <git@dsernst.com>
2025-03-27 15:24:46 -0700

    /protocol EncryptionReceipt should use production one, not re-implemented component

--- src/protocol/EncryptionReceipt.tsx
+++ src/protocol/EncryptionReceipt.tsx
@@ -1,55 +1,28 @@
-import { map } from 'lodash-es'
+import { mapValues, omit } from 'lodash-es'
 import { stringToPoint } from 'src/crypto/curve'
 
-import { public_key } from './election-parameters'
-import { Paper } from './Paper'
+import { DetailedEncryptionReceipt } from '../vote/submitted/DetailedEncryptionReceipt'
+import { public_key, voters } from './election-parameters'
 import { useVoteContext } from './VoteContext'
 
 export function EncryptionReceipt(): JSX.Element {
   const { state } = useVoteContext()
 
-  return (
-    <>
-      <Paper>
-        <code>
-          {`
-Encrypted @ ${new Date().toString()}
+  // Convert this protocol demo's state into a compatible object
+  // for the main app's <DetailedEncryptionReceipt />
+  const demoState = {
+    election_title: 'Demo Election',
+    encoded: mapValues(state.plaintext, (value) => stringToPoint(`${state.verification}:${value}`).toHex()),
+    encrypted: mapValues(omit(state.encrypted, 'auth'), (value) => {
+      const [, encrypted, lock] = value?.match(/encrypted: (.*?), lock: (.*?) }/) || []
+      return { encrypted: encrypted || '', lock: lock || '' }
+    }),
+    plaintext: state.plaintext as Record<string, string>,
+    public_key,
+    randomizer: state.randomizer as Record<string, string>,
+    submitted_at: new Date(),
+    tracking: state.verification,
+  }
 
-Encryption Formula
-  https://en.wikipedia.org/wiki/ElGamal_encryption
-  in Ristretto255 prime-order subgroup of Elliptic Curve25519
-
-  Encrypted = Encoded + (Recipient * randomizer)
-  Lock = (Generator * randomizer)
-
-Public Key
-${public_key}
-
----------
-
-Verification #: ${state.verification}
-
-${map(
-  state.plaintext,
-  (value: string, key: string) => `${key}
-  plaintext: ${state.verification}:${value}
-  encoded: ${stringToPoint(value)}
-  randomizer: ${state.randomizer[key]}
-  ${state.encrypted[key]?.slice(3, -2)}
-`,
-).join('\n')}
-`}
-        </code>
-      </Paper>
-
-      <style jsx>{`
-        code {
-          font-size: 11px;
-          max-width: 100%;
-          white-space: pre-wrap;
-          tab-size: 4;
-        }
-      `}</style>
-    </>
-  )
+  return <DetailedEncryptionReceipt auth={voters[0].auth} election_id="123" state={demoState} />
 }
