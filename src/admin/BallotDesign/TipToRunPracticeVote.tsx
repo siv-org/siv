a9f99efe00b3d8f6a0c3731d3a9e5b3cabb69d59
David Ernst <git@dsernst.com>
2025-04-26 22:19:24 -0700

    /admin/ballot-design practice tip: switch to violet

--- src/admin/BallotDesign/TipToRunPracticeVote.tsx
+++ src/admin/BallotDesign/TipToRunPracticeVote.tsx
@@ -2,7 +2,7 @@ import { BulbOutlined } from '@ant-design/icons'
 
 export const TipToRunPracticeVote = () => {
   return (
-    <div className="p-2 sm:mb-0 mb-1.5 border-2 border-blue-200 border-solid rounded-lg bg-blue-50">
+    <div className="p-2 sm:mb-0 mb-1.5 border-2 border-violet-200 border-solid rounded-lg bg-violet-50 shadow">
       <span className="mr-1 font-semibold">
         <BulbOutlined className="mr-1" />
         Tip:
