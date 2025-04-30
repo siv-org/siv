806beaf2be1a06bacf039ef0563915de24d038bc
David Ernst <git@dsernst.com>
2025-04-29 21:48:50 -0700

    Remove more prev eslintrc configs copied to new flatfile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -11,23 +6,5 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:typescript-sort-keys/recommended',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
   plugins: ['react', '@typescript-eslint', 'typescript-sort-keys', 'sort-destructure-keys', 'sort-keys-fix'],
-  rules: {
-    'react/react-in-jsx-scope': 0, // Handled by Next.js
-    'sort-destructure-keys/sort-destructure-keys': 1, // style
-    'sort-keys-fix/sort-keys-fix': 1, // style
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
 }
