52abf60333c6db3c0619718754f61aed651bb911
David Ernst <git@dsernst.com>
2025-04-29 22:07:30 -0700

    Remove more prev configs copied over

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:typescript-sort-keys/recommended',
   ],
-  plugins: ['react', '@typescript-eslint', 'typescript-sort-keys', 'sort-destructure-keys', 'sort-keys-fix'],
 }
