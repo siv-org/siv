69955bdbd5ec7cb3823ee3e3cbe94ccfea0f0173
David Ernst <git@dsernst.com>
2025-04-29 21:29:38 -0700

    Remove old eslintrc rules confirmed copied to new format

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,11 +21,6 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'typescript-sort-keys', 'sort-destructure-keys', 'sort-keys-fix'],
   rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 0, // Verbose
-    '@typescript-eslint/no-empty-function': 0, // unnecessary
-    '@typescript-eslint/no-unused-vars': 1, // hint not error
-    'react/jsx-sort-props': [2, { callbacksLast: true, shorthandFirst: true }], // style
-    'react/no-unknown-property': [2, { ignore: ['jsx', 'global'] }], // inserted by next's styled-jsx
     'react/react-in-jsx-scope': 0, // Handled by Next.js
     'sort-destructure-keys/sort-destructure-keys': 1, // style
     'sort-keys-fix/sort-keys-fix': 1, // style
