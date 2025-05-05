cd087a6255fe28293416ee6ae993dc0d99e4764e
Ariana I <git@arianaivan.com>
2025-05-05 14:27:56 -0700

    BannerAnnouncement: make separate component and then import it into AboveFold

--- src/homepage/AboveFold.tsx
+++ src/homepage/AboveFold.tsx
@@ -1,24 +1,13 @@
 import Image from 'next/image'
 
+import { BannerAnnouncement } from './BannerAnnouncement'
 import { darkBlue } from './colors'
 import { HeaderBar } from './HeaderBar'
-
 export const AboveFold = () => (
   <section>
     <HeaderBar />
 
-    <div className="w-full px-4 py-3 text-base italic font-medium text-center text-gray-900 rounded-lg shadow-xl bg-slate-200 my-11 md:text-lg">
-      DEF CON 2024 security report is now available —{' '}
-      <a
-        className="!text-blue-700"
-        href="https://hack.siv.org/reports/2024defcon"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        See the findings
-      </a>
-      .
-    </div>
+    <BannerAnnouncement />
 
     <h1>
       Secure Internet Voting
