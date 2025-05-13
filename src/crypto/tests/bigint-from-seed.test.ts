2e70fb3319ca871041fd799285f3aa92de2d0e2e
David Ernst <git@dsernst.com>
2025-05-13 07:30:58 -0700

    Unit test: RP.toString() is stable and canonical

--- src/crypto/tests/bigint-from-seed.test.ts
+++ src/crypto/tests/bigint-from-seed.test.ts
@@ -21,11 +21,18 @@ test('Deterministically generate a pseudorandom integer less than `max`, from a 
 test("RPs implicitly cast .toHex() when .join()'d", () => {
   // If this breaks, Fiat-Shamir PRNGs could lose soundness, letting in invalid proofs.
   // So we explicitly test for it, to protect against future library updates or similar.
-
   const point = G.multiply(BigInt(12345))
 
   const explicitHex = point.toHex()
   const joined = [point, point].join(',')
 
   expect(joined).toBe(`${explicitHex},${explicitHex}`)
+})
+
+test('RP.toString() is stable and canonical', () => {
+  // This test warns against changes in RP.toString(),
+  // which would invalidate existing Fiat–Shamir proofs.
+  const point = G.multiply(BigInt(12345))
+  const expected = 'b4c1b3cdef7ba1bd94fa95c7b736622046ef663285813c2293c52c5f4f9fb011'
+  expect(point.toString()).toBe(expected)
 })
