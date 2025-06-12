0e1dbd7688e84a3e6d281bd9922130d0e64deaca
Ariana I <git@arianaivan.com>
2025-06-12 03:32:23 -0700

    menu: no underline for SIV or sign-in btn

--- src/homepage/HeaderBar.tsx
+++ src/homepage/HeaderBar.tsx
@@ -5,14 +5,14 @@ export const HeaderBar = () => {
 
   const defaultLink = `${baseLink} text-indigo-900 hover:bg-indigo-50/80 hover:text-indigo-900 hover:shadow-sm`
 
-  const signInLink = `${baseLink} text-indigo-900 border shadow-md bg-white/60 bg-gradient-to-r from-indigo-100/60 to-white/60 backdrop-blur-md border-white/50 hover:bg-white/80 hover:from-indigo-200/80 hover:shadow-lg`
+  const signInLink = `${baseLink} no-underline hover:no-underline text-indigo-900 border shadow-md bg-white/60 bg-gradient-to-r from-indigo-100/60 to-white/60 backdrop-blur-md border-white/50 hover:bg-white/80 hover:from-indigo-200/80 hover:shadow-lg`
 
   return (
     <header className="w-full px-6 py-6 mx-auto border-b border-gray-200 max-w-7xl sm:px-8 lg:px-10 bg-gradient-to-r from-white via-gray-100 to-white">
       <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
         <h3 className="m-0">
           <Link href="/">
-            <a className="text-4xl font-bold tracking-tight text-transparent transition-all duration-200 bg-gradient-to-r from-[#002868] to-[#003a8c] bg-clip-text active:from-[#003a8c] active:to-[#0049b3]">
+            <a className="no-underline hover:no-underline text-4xl font-bold tracking-tight text-transparent transition-all duration-200 bg-gradient-to-r from-[#002868] to-[#003a8c] bg-clip-text active:from-[#003a8c] active:to-[#0049b3]">
               SIV
             </a>
           </Link>
