0ce3135170d213c8da71c0a57f738cd3aedbdab4
Ariana I <git@arianaivan.com>
2025-06-13 15:55:04 -0700

    footer: made component <FooterDivider/>

---
+++ src/homepage/FooterDivider.tsx
@@ -0,0 +1,13 @@
+// components/FooterLines.tsx
+export const FooterDivider = () => (
+  <div className="relative w-full h-[28vw] sm:h-[21vw] 2xl:h-[31vw]">
+    <img
+      alt="Footer lines decoration"
+      className="absolute top-0 left-1/2 -translate-x-1/2 w-[148vw]
+        sm:w-[120vw]"
+      src="/home3/footer-lines.png"
+    />
+  </div>
+)
+
+export default FooterDivider
