2d046076dcda562f319860c3e7b9459960458955
Ariana I <git@arianaivan.com>
2025-06-13 16:42:12 -0700

    footer: update positioning so that footer content is always in front of the decorative footer lines

--- src/homepage/Footer.tsx
+++ src/homepage/Footer.tsx
@@ -3,7 +3,7 @@ import { EmailSignup } from './EmailSignup'
 const email = 'team@siv.org'
 
 export const Footer = (): JSX.Element => (
-  <footer>
+  <footer className="relative z-10">
     <div className="flex flex-col w-full gap-8 px-4 py-8 mt-36 md:mt-96 sm:px-6 sm:py-12 md:flex-row md:items-start md:justify-between md:gap-0">
       {/* Left: Email Signup */}
       <div className="flex-1 min-w-[220px] md:pr-8 flex flex-col items-center md:items-start text-center md:text-left gap-4">
--- src/homepage/FooterDivider.tsx
+++ src/homepage/FooterDivider.tsx
@@ -1,6 +1,6 @@
 // components/FooterLines.tsx
 export const FooterDivider = () => (
-  <div className="relative w-full h-[28vw] sm:h-[21vw] 2xl:h-[31vw]">
+  <div className="relative w-full h-[28vw] sm:h-[21vw] 2xl:h-[31vw] z-0">
     <img
       alt="Footer lines decoration"
       className="absolute top-0 left-1/2 -translate-x-1/2 w-[148vw]
