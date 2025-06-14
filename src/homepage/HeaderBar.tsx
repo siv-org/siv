0d94df45d2c8be4bea272b7bfd2a0b4c118b7042
David Ernst <git@dsernst.com>
2025-06-13 23:04:52 -0700

    HeaderBar: Use new logo

--- src/homepage/HeaderBar.tsx
+++ src/homepage/HeaderBar.tsx
@@ -1,16 +1,20 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
-export const HeaderBar = () => {
-  const sharedStyles =
-    'px-4 py-2 text-base font-medium transition-all duration-75 rounded-lg hover:no-underline text-indigo-900 active:bg-indigo-200'
+import logo from './logo.png'
 
+const logoWidth = 50
+const sharedStyles =
+  'px-4 py-2 text-base font-medium transition duration-75 rounded-lg hover:no-underline text-indigo-900 active:bg-indigo-200'
+
+export const HeaderBar = () => {
   return (
     <header className="w-full px-6 py-6 mx-auto border-b border-gray-200 max-w-7xl sm:px-8 lg:px-10 bg-gradient-to-r from-white via-gray-100 to-white">
       <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
         <h3 className="m-0">
           <Link href="/">
-            <a className="no-underline hover:no-underline text-4xl font-bold tracking-tight text-transparent transition-all duration-200 bg-gradient-to-r from-[#002868] to-[#003a8c] bg-clip-text active:from-[#003a8c] active:to-[#0049b3]">
-              SIV
+            <a className="leading-none">
+              <Image alt="SIV" height={(logoWidth * 219) / 482} src={logo} width={logoWidth} />
             </a>
           </Link>
         </h3>
