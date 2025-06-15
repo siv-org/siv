a1ef5da7c6d08ef629fecf6a48838f81eb40d589
David Ernst <git@dsernst.com>
2025-06-15 16:48:04 -0700

    Tweak AreYouAVoter negative margin-bottom

--- src/homepage/AreYouAVoter.tsx
+++ src/homepage/AreYouAVoter.tsx
@@ -70,7 +70,7 @@ export function AreYouAVoter(): JSX.Element {
       <style jsx>{`
         .are-you-container {
           margin-top: 13vw;
-          margin-bottom: -20vw;
+          margin-bottom: -15vw;
           display: flex;
         }
 
