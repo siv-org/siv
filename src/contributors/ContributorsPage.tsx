ad19cc6f852d93e2003417726bf4de29cc80cb08
Ariana I <git@arianaivan.com>
2025-06-16 16:15:57 -0700

    contributors: add page padding

--- src/contributors/ContributorsPage.tsx
+++ src/contributors/ContributorsPage.tsx
@@ -4,11 +4,27 @@ import { Contributors } from './Contributors'
 
 export const ContributorsPage = () => {
   return (
-    <>
+    <div>
       <GlobalCSS />
       <HeaderBar />
-
       <Contributors />
-    </>
+
+      <style jsx>{`
+        div {
+          padding: 1rem 3rem;
+          padding-top: 0 !important;
+
+          width: 100%;
+          overflow-x: hidden;
+        }
+
+        /* For small screens */
+        @media (max-width: 700px) {
+          div {
+            padding: 1rem 2rem;
+          }
+        }
+      `}</style>
+    </div>
   )
 }
