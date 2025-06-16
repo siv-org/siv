065f496a143942a8b4f57004f6aa32e13f0c8835
Ariana I <git@arianaivan.com>
2025-06-16 16:33:25 -0700

    contributors: tweak Jason G's title & added 'Relevant' to Affiliation

--- src/contributors/Contributors.tsx
+++ src/contributors/Contributors.tsx
@@ -59,7 +59,7 @@ export function Contributors() {
       <ContributorRow
         affiliation="CREO Cybersecurity Lab, North Carolina A & T State University"
         focus="US Elections Cybersecurity"
-        name="Jason Green, PhD Candidate"
+        name="Jason Green"
       />
     </div>
   )
