0d6ebbf7c02e7d16c51745022167492a1a44184b
Ariana I <git@arianaivan.com>
2025-06-17 00:56:06 -0700

    /about: only one h1 per page

--- src/about/ResearchHeader.tsx
+++ src/about/ResearchHeader.tsx
@@ -1,6 +1,6 @@
 export const ResearchHeader = () => (
   <section className="mt-24 text-center">
-    <h1 className="mb-6 text-4xl font-medium tracking-tight text-gray-900">Academic Research</h1>
+    <h2 className="mb-6 text-4xl font-medium tracking-tight text-gray-900">Academic Research</h2>
 
     {/* Subtitle */}
     <p className="max-w-2xl mx-auto text-lg text-gray-600">
