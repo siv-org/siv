da3d82ddf3eb8240e12b56ba6056c15524a12956
Ariana I <git@arianaivan.com>
2025-06-17 00:45:17 -0700

    ContributorDesign.tsx: Inline prop typing

--- src/about/contributors/ContributorDesign.tsx
+++ src/about/contributors/ContributorDesign.tsx
@@ -1,9 +1,3 @@
-interface ContributorProps {
-  affiliation: string
-  focus: string
-  name: string
-}
-
 const Column = ({ topLabel, value }: { topLabel: string; value: string }) => (
   <div className={`mt-2 text-center text-gray-600 sm:text-left sm:mt-0 sm:w-48`}>
     <span className="text-xs tracking-wide text-gray-500 uppercase">{topLabel}</span>
@@ -11,7 +5,7 @@ const Column = ({ topLabel, value }: { topLabel: string; value: string }) => (
   </div>
 )
 
-export const ContributorRow = ({ affiliation, focus, name }: ContributorProps) => {
+export const ContributorRow = ({ affiliation, focus, name }: { affiliation: string; focus: string; name: string }) => {
   return (
     <div className="flex flex-col p-4 mb-2 transition-all duration-300 group rounded-2xl hover:bg-gray-50 sm:flex-row sm:items-start sm:justify-between">
       <h2 className="mb-2 text-xl font-medium tracking-tight text-center text-gray-900 sm:w-1/3 sm:text-left sm:mb-0">
