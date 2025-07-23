7c3998adc36efad5fa0b0331cb6fcd4b015b430d
Ariana I <git@arianaivan.com>
2025-07-23 15:02:21 -0700

    Integrate getServerSideProps to fetch election data

--- pages/election/[election_id]/vote.tsx
+++ pages/election/[election_id]/vote.tsx
@@ -1,1 +1,44 @@
-export { VotePage as default } from '../../../src/vote/VotePage'
+import { GetServerSideProps } from 'next'
+
+import { VotePage } from '../../../src/vote/VotePage'
+
+interface VotePageWrapperProps {
+  host: string
+}
+
+export default function VotePageWrapper(props: VotePageWrapperProps) {
+  return <VotePage {...props} />
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params, req } = context
+  const host = req.headers.host || 'localhost:3000'
+  const election_id = params?.election_id as string
+
+  let electionData = {}
+
+  if (election_id) {
+    try {
+      const baseUrl = req.headers.host?.includes('localhost') ? 'http://localhost:3000' : `https://${host}`
+      const response = await fetch(`${baseUrl}/api/election/${election_id}/info`)
+      if (response.ok) {
+        const data = await response.json()
+        if (!data.error) {
+          electionData = {
+            ballot_design: data.ballot_design,
+            election_title: data.election_title,
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Failed to fetch election data in getServerSideProps:', error)
+    }
+  }
+
+  return {
+    props: {
+      electionData,
+      host,
+    },
+  }
+}
