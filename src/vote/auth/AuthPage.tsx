95e93dd22535eff8af678b4add24eab268476686
David Ernst <git@dsernst.com>
2025-08-12 01:21:12 -0700

    /vote AuthInfoForm: Add 1p1v subheader

--- src/vote/auth/AuthPage.tsx
+++ src/vote/auth/AuthPage.tsx
@@ -12,8 +12,9 @@ export const AuthPage = () => {
       <main className="max-w-[750px] w-full mx-auto p-4 flex flex-col min-h-screen justify-between">
         <section>
           <h1 className="mt-8 text-3xl font-bold">Voter Authentication</h1>
+          <div className="text-sm opacity-50">for One-Person, One-Vote</div>
 
-          <div className="mt-3 mb-6 text-lg">
+          <div className="mt-6 mb-6 text-lg">
             <p className="mb-3 italic opacity-60">Your vote is now pending.</p>
             <p>
               <UserOutlined className="relative bottom-[5px] mr-0.5" /> Please provide your identifying information.
