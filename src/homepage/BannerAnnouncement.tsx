62ee37c25e6b1d95f0c3cdea8fdf32fb4f0732a6
David Ernst <git@dsernst.com>
2025-11-01 15:45:26 -0700

    BannerAnouncement: published in '25, not '24

--- src/homepage/BannerAnnouncement.tsx
+++ src/homepage/BannerAnnouncement.tsx
@@ -1,7 +1,7 @@
 export const BannerAnnouncement = () => {
   return (
-    <div className="w-full px-4 py-3 text-base italic font-medium text-center text-gray-900 rounded-lg shadow-xl bg-slate-200 my-11 md:text-lg">
-      DEF CON 2024 security report is now available —{' '}
+    <div className="px-4 py-3 my-11 w-full text-base italic font-medium text-center text-gray-900 rounded-lg shadow-xl bg-slate-200 md:text-lg">
+      DEF CON security report is now available —{' '}
       <a
         className="!text-blue-700"
         href="https://hack.siv.org/reports/2024defcon"
