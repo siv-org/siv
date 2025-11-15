0e73f4d344c8eb86caa535c2476f1654cb98def8
David Ernst <git@dsernst.com>
2025-11-14 22:16:08 -0800

    sms: Cache Lookup queries in DB

--- pages/api/sms/lookup-test.ts
+++ pages/api/sms/lookup-test.ts
@@ -1,3 +1,4 @@
+import { firebase } from 'api/_services'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Twilio } from 'twilio'
 
@@ -5,18 +6,47 @@ const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = process.env
 const client = new Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  void client
-  return res.status(401).json({ message: 'Disabled' })
+  // Comment out to test the endpoint locally
+  const DISABLED = true
+  if (DISABLED) return res.status(401).json({ message: 'Disabled' })
 
-  // const lookupNum = '+15551234567'
+  const lookupNum = '+15551234567'
 
-  // const phoneNumber = await client.lookups.v2
-  //   .phoneNumbers(lookupNum)
-  //   .fetch({ fields: 'line_type_intelligence,caller_name' })
+  // First check if DB already has the result for this lookup:
+  const dbInfo = await firebase.firestore().collection('sms-lookup').doc(lookupNum).get()
 
-  // const results = { lookupNum, phoneNumber }
+  let results = null
+  if (dbInfo.exists) {
+    results = { cachedFromDb: true, ...dbInfo.data() }
+  } else {
+    const twilioResults = await callerIdLookup(lookupNum)
+    // @ts-expect-error intentionally removing this circular reference
+    delete twilioResults._version
 
-  // console.log(JSON.stringify(results, null, 2))
+    results = { ...twilioResults, lookupTime: new Date().toISOString() }
+    console.log(results)
 
-  // res.status(201).json({ results, success: true })
+    // Cache the results in DB.
+    await firebase.firestore().collection('sms-lookup').doc(lookupNum).set(results)
+    results = { ...results, cachedToDb: true }
+  }
+
+  // Then check if DB already has the result for this lookup:
+  // const results = await callerIdLookup(lookupNum)
+
+  // Add current timestamp to output
+  results = { ...results, current_time: new Date().toISOString() }
+
+  console.log(JSON.stringify(results, null, 2))
+  res.status(201).json({ results })
 }
+
+function callerIdLookup(lookupNum: string) {
+  return client.lookups.v2.phoneNumbers(lookupNum).fetch({ fields: 'caller_name' })
+}
+// function carrierLookup(lookupNum: string) {
+//   return client.lookups.v2.phoneNumbers(lookupNum).fetch({ fields: 'line_type_intelligence' })
+// }
+// function callerIdAndCarrierLookup(lookupNum: string) {
+//   return client.lookups.v2.phoneNumbers(lookupNum).fetch({ fields: 'caller_name,line_type_intelligence' })
+// }
