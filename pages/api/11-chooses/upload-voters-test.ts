443299e2bd0f8d97cf267a02d88a6e54c5f97d91
David Ernst <git@dsernst.com>
2025-12-02 13:47:47 -0600

    11c upload: support multi-withheld indices

--- pages/api/11-chooses/upload-voters-test.ts
+++ pages/api/11-chooses/upload-voters-test.ts
@@ -15,9 +15,11 @@ import { election_ids_for_11chooses } from 'src/vote/auth/11choosesAuth/CustomAu
     }
 } */
 
-const sample_auth_tokens_by_voter_id: Record<string, string> = { '1234567890': 'aabbccddee', withheld1: 'bbccddeeff' }
+const sample_auth_tokens: Record<string, string> = { '1234567890': 'aabbccddee', w1: 'bbccddeeff', w2: 'ccddeeffaa' }
+const sample_distinct_age_ranges_index: Record<string, number> = { w1: 0, w2: 0 }
+const sample_multi_withheld_address_index: Record<string, number> = { w1: 3, w2: 3 }
 
-// 1 Public, 1 Withheld
+// 1 Public, 2 Withheld
 const sample_voters = [
   {
     'DOB/YOB/Age Range': '1970',
@@ -31,17 +33,26 @@ const sample_voters = [
     'First Name': '',
     'Last Name': '',
     'Privacy Status': 'Withheld',
-    'Voter ID': 'withheld1',
+    'Voter ID': 'w1',
+  },
+  {
+    'DOB/YOB/Age Range': '36 through 45',
+    'First Name': '',
+    'Last Name': '',
+    'Privacy Status': 'Withheld',
+    'Voter ID': 'w2',
   },
 ]
 
 const voterFileToUploadFormat = (v: (typeof sample_voters)[number], index: number) => ({
-  auth_token: sample_auth_tokens_by_voter_id[v['Voter ID']],
+  auth_token: sample_auth_tokens[v['Voter ID']],
   voter_file: {
+    distinct_age_ranges_index: sample_distinct_age_ranges_index[v['Voter ID']] || null,
     'DOB/YOB/Age Range': v['DOB/YOB/Age Range'],
     first_name: v['First Name'],
     is_withheld: v['Privacy Status'] === 'Withheld',
     last_name: v['Last Name'],
+    multi_withheld_address_index: sample_multi_withheld_address_index[v['Voter ID']] || null,
     state_voter_id: v['Voter ID'],
     voter_file_index: index,
   },
@@ -54,8 +65,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { election_id } = req.query
   if (typeof election_id !== 'string') return res.status(400).json({ error: 'election_id is required' })
   if (!election_ids_for_11chooses.includes(election_id)) return res.status(400).json({ error: 'Invalid election_id' })
-
-  if (req.headers.host !== 'localhost:3000') return res.status(405).json({ error: 'For localhost only' })
 
   const new_voter_uploads = sample_voters.map(voterFileToUploadFormat)
 
