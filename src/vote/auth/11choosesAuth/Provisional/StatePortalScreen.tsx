b089f918354da0cc028a59fb5ce76f64cc54e0ae
David Ernst <git@dsernst.com>
2025-12-03 22:44:52 -0600

    11c auth: All fields for State Portal Screen

--- src/vote/auth/11choosesAuth/Provisional/StatePortalScreen.tsx
+++ src/vote/auth/11choosesAuth/Provisional/StatePortalScreen.tsx
@@ -1,13 +1,22 @@
 import { TextField } from '@mui/material'
+import { useState } from 'react'
+import { OnClickButton } from 'src/_shared/Button'
+
+const fields = ['First Name', 'Last Name', 'Date of Birth (MM-DD-YYYY)', 'Street Address', 'City', 'Zip']
 
 export const StatePortalScreen = () => {
+  const [fieldValues, setFieldValues] = useState<Record<string, string>>({})
+
+  function setFieldValue(label: string, value: string) {
+    setFieldValues({ ...fieldValues, [label]: value })
+  }
+
   return (
     <div className="mx-auto max-w-96">
-      <h1 className="mt-8 text-3xl font-bold">Confirm Voter Eligibility</h1>
+      <p className="mt-8 text-lg font-medium opacity-50">Step 1 of 2</p>
+      <h1 className="mt-1 text-3xl font-bold">Voter Registration Lookup</h1>
 
-      <p className="text-lg font-medium opacity-50">Step 1 of 2</p>
-
-      <p className="mt-10 text-xl">
+      <p className="mt-8 text-xl">
         We need to confirm you are a voter on the official{' '}
         <a
           className="font-semibold text-blue-500 hover:underline"
@@ -20,18 +29,42 @@ export const StatePortalScreen = () => {
         .
       </p>
 
-      <p className="mt-6 opacity-70">This info must match exactly.</p>
-
-      <div className="text-left">
-        <p className="mt-6 text-xl">First name:</p>
-        <TextField autoFocus className="w-full min-h-20" />
-
-        <p className="text-xl">Last name:</p>
-        <TextField autoFocus className="w-full min-h-20" />
-
-        <p className="text-xl">Date of birth (MM-DD-YYYY):</p>
-        <TextField autoFocus className="w-full min-h-20" />
+      <div className="flex flex-col gap-8 mt-8 text-left">
+        {fields.map((label) => (
+          <Field key={label} {...{ label, setFieldValue, value: fieldValues[label] }} />
+        ))}
       </div>
+
+      <OnClickButton
+        className="!mt-10 w-full text-xl text-center max-w-80"
+        onClick={() => {
+          alert(JSON.stringify(fieldValues))
+        }}
+      >
+        Submit
+      </OnClickButton>
+    </div>
+  )
+}
+
+function Field({
+  label,
+  setFieldValue,
+  value,
+}: {
+  label: string
+  setFieldValue: (label: string, value: string) => void
+  value: string
+}) {
+  return (
+    <div>
+      <p className="text-xl">{label}</p>
+      <TextField
+        className="w-full"
+        InputProps={{ style: { fontSize: 22 } }}
+        onChange={(e) => setFieldValue(label, e.target.value)}
+        {...{ value }}
+      />
     </div>
   )
 }
