c47cca83e9f3fb1cf70f4ebe45e2332a0d614fe8
David Ernst <git@dsernst.com>
2025-12-05 18:57:05 -0500

    11c provisional coming soon: add missing space

--- pages/api/11-chooses/provisional/coming-soon-block.ts
+++ pages/api/11-chooses/provisional/coming-soon-block.ts
@@ -6,6 +6,6 @@ export default allowCors(async (req: NextApiRequest, res: NextApiResponse) => {
   const headers = ['x-vercel-ip-city', 'x-vercel-ip-country-region', 'x-vercel-ip-country']
   const location = headers.map((header) => req.headers[header]).join(', ')
 
-  await pushover('11c/provisional-coming-soon', 'request received from:' + location)
+  await pushover('11c/provisional-coming-soon', 'request received from: ' + location)
   return res.status(200).json({ success: true })
 })
