6f75c2d8531d50fa583a4e5a095217a4bfaf86cb
David Ernst <git@dsernst.com>
2025-12-05 00:35:07 -0600

    11c: Improve look of BackupAuthOptions

--- src/vote/auth/11choosesAuth/Provisional/BackupAuthOptions.tsx
+++ src/vote/auth/11choosesAuth/Provisional/BackupAuthOptions.tsx
@@ -1,46 +1,59 @@
-const detailsClassName = 'p-4 rounded-lg cursor-pointer bg-yellow-100 border-2 border-blue-700 hover:bg-yellow-200'
-
 export const BackupAuthOptions = () => {
   return (
     <div className="mx-auto max-w-96">
       <p className="mt-8 text-lg font-medium opacity-50">Step 2 of 2</p>
       <h1 className="mt-1 text-3xl font-bold">Provisional Auth Options</h1>
 
-      <p className="mt-8 text-xl">Lastly, we need to verify you are the person whose details you just submitted.</p>
+      <p className="mt-8 text-xl">Lastly, we need to verify you are the person whose name you just submitted.</p>
 
-      <div className="mt-8 text-lg text-left">Any of these are acceptable:</div>
+      <div className="mt-10 text-left">
+        <div className="mb-2 text-lg opacity-50">Any of these are acceptable:</div>
 
-      <div className="flex flex-col gap-8 mt-2">
-        <details className={detailsClassName}>
-          <summary>
-            <h3 className="text-xl font-medium">SMS + Caller ID Check</h3>
-            <div>works for ~1/3 of people</div>
-          </summary>
-        </details>
+        <Details title="SMS + Caller ID Check">
+          <div>works for ~1/3 of people</div>
+        </Details>
 
-        <details className={detailsClassName}>
-          <summary>
-            <h3 className="text-xl font-medium">Passport Scan</h3>
-          </summary>
-        </details>
-
-        <details className={detailsClassName}>
-          <summary>
-            <h3 className="text-xl font-medium">In-Person</h3>
-          </summary>
-          <div className="mt-3 text-xl font-semibold">
-            <a
-              className="hover:text-blue-700"
-              href="https://www.11chooses.com/#in-person"
-              rel="noreferrer noopener"
-              target="_blank"
-            >
-              Visit 11chooses.com for Locations & Times
-            </a>
-            .
+        <Details title="Passport Scan">
+          <div>
+            Use your smartphone{"'"}s NFC reader{' '}
+            <div className="mb-2 text-sm opacity-50">(what powers Apple/Google Pay) </div>
+            to validate the e-chip in your passport.
           </div>
-        </details>
+        </Details>
+
+        <Details title="In-Person">
+          <div className="mt-3">
+            <div className="text-xl font-medium">
+              Visit{' '}
+              <a
+                className="text-blue-700 hover:underline"
+                href="https://www.11chooses.com/#in-person"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                11chooses.com/#in-person
+              </a>{' '}
+              for Locations & Times
+            </div>
+
+            <div className="mt-3 space-y-2 text-sm opacity-50">
+              <p>You don{"'"}t have to vote again.</p>
+              <p>A poll worker will check your valid photo ID, and mark your email address as verified.</p>
+            </div>
+          </div>
+        </Details>
       </div>
     </div>
+  )
+}
+
+const Details = ({ children, title }: { children: React.ReactNode; title: string }) => {
+  return (
+    <details className="mb-4 ml-7">
+      <summary className="text-[22px] font-medium marker:text-black/70 marker:text-3xl list-outside text-[#111827] leading-[1.5] mb-2 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+        {title}{' '}
+      </summary>
+      <div className="pb-6 pl-1">{children}</div>
+    </details>
   )
 }
