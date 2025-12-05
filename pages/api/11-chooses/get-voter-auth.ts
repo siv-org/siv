a21818f6ee1a270e04acaba940e59607ea0d8ca0
David Ernst <git@dsernst.com>
2025-12-04 23:06:41 -0600

    11c get-voter-auth.ts: Minor cleanup

--- pages/api/11-chooses/get-voter-auth.ts
+++ pages/api/11-chooses/get-voter-auth.ts
@@ -3,12 +3,11 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { election_ids_for_11chooses, VoterInfo } from 'src/vote/auth/11choosesAuth/CustomAuthFlow'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // Validate auth_token
   const { auth_token, election_id } = req.body
-  if (typeof election_id !== 'string') return res.status(400).json({ error: 'election_id is required' })
   if (typeof auth_token !== 'string') return res.status(400).json({ error: 'auth_token is required' })
-
+  if (typeof election_id !== 'string') return res.status(400).json({ error: 'election_id is required' })
   if (!election_ids_for_11chooses.includes(election_id)) return res.status(400).json({ error: 'Invalid election_id' })
+  if (auth_token === 'link') return res.status(400).json({ error: 'Looking up auth=link not supported' })
 
   // Lookup voter by auth_token
   const electionDoc = firebase.firestore().collection('elections').doc(election_id)
@@ -16,7 +15,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   // If voter not found, error and ping admin
   if (!voter?.exists) {
-    await pushover('11chooses/get-voter-auth: auth not found', JSON.stringify({ auth_token }))
+    await pushover('11c/get-voter-auth: auth not found', JSON.stringify({ auth_token }))
     return res.status(400).json({ error: 'voter not found' })
   }
 
