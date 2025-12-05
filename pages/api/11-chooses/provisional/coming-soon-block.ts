a871da4d30203fdebd7c6775ec8d2779889ad8e5
David Ernst <git@dsernst.com>
2025-12-05 18:46:38 -0500

    11c provisional coming soon: Add approx location

--- pages/api/11-chooses/provisional/coming-soon-block.ts
+++ pages/api/11-chooses/provisional/coming-soon-block.ts
@@ -1,8 +1,11 @@
 import { allowCors } from 'api/_cors'
 import { pushover } from 'api/_services'
-import { NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default allowCors(async (req: unknown, res: NextApiResponse) => {
-  await pushover('11c/provisional-coming-soon', 'request received')
+export default allowCors(async (req: NextApiRequest, res: NextApiResponse) => {
+  const headers = ['x-vercel-ip-city', 'x-vercel-ip-country-region', 'x-vercel-ip-country']
+  const location = headers.map((header) => req.headers[header]).join(', ')
+
+  await pushover('11c/provisional-coming-soon', 'request received from:' + location)
   return res.status(200).json({ success: true })
 })
