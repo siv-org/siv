051a25a16addc41d98c603938621b87e609a8f72
David Ernst <git@dsernst.com>
2025-12-06 08:49:18 -0500

    11c provisional coming soon ping: %20 -> space

--- pages/api/11-chooses/provisional/coming-soon-block.ts
+++ pages/api/11-chooses/provisional/coming-soon-block.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default allowCors(async (req: NextApiRequest, res: NextApiResponse) => {
   const headers = ['x-vercel-ip-city', 'x-vercel-ip-country-region', 'x-vercel-ip-country']
-  const location = headers.map((header) => req.headers[header]).join(', ')
+  const location = headers.map((header) => req.headers[header]?.toString().replaceAll('%20', ' ')).join(', ')
 
   await pushover('11c/provisional-coming-soon', 'request received from: ' + location)
   return res.status(200).json({ success: true })
