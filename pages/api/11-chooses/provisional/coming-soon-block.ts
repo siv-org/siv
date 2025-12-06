41155c8a8a30855cf36956506a6a29a3963d9f6b
David Ernst <git@dsernst.com>
2025-12-06 09:25:56 -0500

    11c provisional coming-soon ping: Add IP

--- pages/api/11-chooses/provisional/coming-soon-block.ts
+++ pages/api/11-chooses/provisional/coming-soon-block.ts
@@ -6,6 +6,9 @@ export default allowCors(async (req: NextApiRequest, res: NextApiResponse) => {
   const headers = ['x-vercel-ip-city', 'x-vercel-ip-country-region', 'x-vercel-ip-country']
   const location = headers.map((header) => req.headers[header]?.toString().replaceAll('%20', ' ')).join(', ')
 
-  await pushover('11c/provisional-coming-soon', 'request received from: ' + location)
+  await pushover(
+    '11c/provisional-coming-soon',
+    'request received from: ' + location + '\n\n' + req.headers['x-real-ip'],
+  )
   return res.status(200).json({ success: true })
 })
