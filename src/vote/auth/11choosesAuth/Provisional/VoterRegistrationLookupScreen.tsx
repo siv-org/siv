d8f99aecb9e8e05f0b259712815686868102f9fd
David Ernst <git@dsernst.com>
2025-12-06 10:48:18 -0500

    11c VoterRegInfo: Hide 'Step 1 of 2' when provisionalAuthDisabled

--- src/vote/auth/11choosesAuth/Provisional/VoterRegistrationLookupScreen.tsx
+++ src/vote/auth/11choosesAuth/Provisional/VoterRegistrationLookupScreen.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import { OnClickButton } from 'src/_shared/Button'
 import { api } from 'src/api-helper'
 
+import { provisionalAuthDisabled } from './BackupAuthOptions'
+
 const fields = ['First Name', 'Last Name', 'Date of Birth (MM-DD-YYYY)', 'Street Address', 'City', 'Zip']
 
 export const VoterRegistrationLookupScreen = ({
@@ -22,9 +24,11 @@ export const VoterRegistrationLookupScreen = ({
   }
 
   return (
-    <div className="mx-auto max-w-96">
-      <p className="mt-8 text-lg font-medium opacity-50">Step 1 of 2</p>
-      <h1 className="mt-1 text-3xl font-bold">Voter Registration Lookup</h1>
+    <div className="pt-8 mx-auto max-w-96">
+      {/* Show step 1 of 2, when Auth is not disabled */}
+      {!provisionalAuthDisabled && <p className="mb-1 text-lg font-medium opacity-50">Step 1 of 2</p>}
+
+      <h1 className="text-3xl font-bold">Voter Registration Lookup</h1>
 
       <p className="mt-8 text-xl">
         We need to confirm you are on the official{' '}
