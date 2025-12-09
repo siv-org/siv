60a6f25e8d4034bc3fe9a3042fb7ee5d9558c307
David Ernst <git@dsernst.com>
2025-12-08 23:37:01 -0700

    11c sms: Listen for messages from embedded iframe

--- src/vote/auth/11choosesAuth/Provisional/SMSFlow.tsx
+++ src/vote/auth/11choosesAuth/Provisional/SMSFlow.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react'
+
 const devUrl = 'http://127.0.0.1:3002'
 const prodUrl = 'https://sms.siv.org'
 const domain = process.env.NODE_ENV === 'production' ? prodUrl : devUrl
 const url = domain + '/embed'
 
 export const SMSFlow = () => {
+  const [result, setResult] = useState<null | { confirmed_sms: string; firebase_uid: string; session_id: string }>(null)
+
+  // Listen for messages from the iframe
+  useEffect(() => {
+    const handler = (event: MessageEvent) => {
+      if (event.data?.type === 'IFRAME_MESSAGE') {
+        console.log('FROM IFRAME:', event.data.payload)
+        setResult(event.data.payload)
+      }
+    }
+
+    window.addEventListener('message', handler)
+    return () => window.removeEventListener('message', handler)
+  }, [])
+
   return (
     <div className="w-full h-[550px]">
-      <iframe className="w-full h-full" src={url}></iframe>
+      {!result ? <iframe className="w-full h-full" src={url}></iframe> : <div>Result: {JSON.stringify(result)}</div>}
     </div>
   )
 }
