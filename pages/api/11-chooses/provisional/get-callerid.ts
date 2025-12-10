4e007619cd6e2796c917ccc09baa64ff58d498b7
David Ernst <git@dsernst.com>
2025-12-09 19:47:20 -0700

    11c: Store callerID results along w/ provisional ballots

--- pages/api/11-chooses/provisional/get-callerid.ts
+++ pages/api/11-chooses/provisional/get-callerid.ts
@@ -1,9 +1,13 @@
 import { firebase, pushover } from 'api/_services'
 import { getCallerIdResults } from 'api/sms/lookup-test'
+import { firestore } from 'firebase-admin'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { election_ids_for_11chooses } from 'src/vote/auth/11choosesAuth/CustomAuthFlow'
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const { link_auth, lookupNum } = req.body
+  const { election_id, link_auth, lookupNum } = req.body
+  if (typeof election_id !== 'string') return res.status(400).json({ error: 'election_id is required' })
+  if (!election_ids_for_11chooses.includes(election_id)) return res.status(400).json({ error: 'Invalid election_id' })
 
   if (typeof lookupNum !== 'string') return res.status(400).json({ error: 'lookupNum is required' })
   if (typeof link_auth !== 'string') return res.status(400).json({ error: 'link_auth is required' })
@@ -23,6 +27,17 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
   const callerID = results.callerName?.caller_name
 
   await pushover('11c/get-callerid', `[${link_auth}] ${lookupNum}: ${callerID}`)
+
+  // Update the voterDoc with the callerID
+  await firebase
+    .firestore()
+    .collection('elections')
+    .doc(election_id)
+    .collection('votes-pending')
+    .doc(link_auth)
+    .update({
+      caller_id: firestore.FieldValue.arrayUnion({ callerID, phone: lookupNum, timestamp: new Date() }),
+    })
 
   return res.status(200).json({ results })
 }
--- src/vote/auth/11choosesAuth/Provisional/SMS/CallerIDCheck.tsx
+++ src/vote/auth/11choosesAuth/Provisional/SMS/CallerIDCheck.tsx
@@ -2,13 +2,21 @@ import { useEffect, useState } from 'react'
 import { Spinner } from 'src/admin/Spinner'
 import { api } from 'src/api-helper'
 
-export const CallerIDCheck = ({ link_auth, number }: { link_auth: string; number: string }) => {
+export const CallerIDCheck = ({
+  election_id,
+  link_auth,
+  number,
+}: {
+  election_id: string
+  link_auth: string
+  number: string
+}) => {
   const [results, setResults] = useState<null | { callerName: { caller_name: string } }>(null)
   const callerName = results?.callerName?.caller_name
 
   useEffect(() => {
     const checkCallerID = async () => {
-      const results = await api('11-chooses/provisional/get-callerid', { link_auth, lookupNum: number })
+      const results = await api('11-chooses/provisional/get-callerid', { election_id, link_auth, lookupNum: number })
       if (!results.ok) {
         console.error('lookup response', results, await results.json())
         return
