67924f7996c49b5dde14944847f3491f831d317d
David Ernst <git@dsernst.com>
2025-12-11 17:12:34 -0700

    11c: Don't publish election results yet

--- pages/api/election/[election_id]/decrypted-votes.ts
+++ pages/api/election/[election_id]/decrypted-votes.ts
@@ -5,6 +5,10 @@ import { firebase } from '../../_services'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { election_id } = req.query
 
+  if (typeof election_id !== 'string') return res.status(400).json({ error: 'election_id is required' })
+
+  if (keepDecryptedPrivate.includes(election_id)) return res.status(400).json([])
+
   const election = await firebase
     .firestore()
     .collection('elections')
@@ -12,9 +16,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     .get()
 
   // Is election_id in DB?
-  if (!election.exists) {
-    return res.status(400).end('Unknown Election ID.')
-  }
+  if (!election.exists) return res.status(400).end('Unknown Election ID.')
 
   res.status(200).json(election.data()?.decrypted || [])
 }
+
+// To begin decrypting in batches, without leaking results early
+import { live11chooses } from 'src/vote/YourAuthToken'
+
+const keepDecryptedPrivate = [live11chooses]
