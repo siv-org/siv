cd63b3fd0f79e599ea172d379d9d39597d86d3cd
David Ernst <git@dsernst.com>
2025-12-14 16:24:35 -0800

    /browser-storage: Fix AntiCoercionReminder padding

--- pages/browser-storage.tsx
+++ pages/browser-storage.tsx
@@ -40,12 +40,12 @@ function BrowserStoragePage() {
       </div>
 
       {/* Anti-Coercion Reminder */}
-      <details className="p-2 pt-0 mt-4 -ml-2 rounded-lg open:bg-gray-100 group">
-        <summary className="p-2 -ml-2 font-medium rounded-lg cursor-pointer hover:bg-gray-200 w-fit group-open:w-full">
+      <details className="mt-4 -ml-2 rounded-lg open:bg-gray-100 group">
+        <summary className="p-2 font-medium rounded-lg cursor-pointer hover:bg-gray-200 w-fit group-open:w-full">
           Reminder: Your vote is your private choice{' '}
           <SafetyOutlined className="mr-1.5 text-lg text-green-800 relative top-px" />
         </summary>
-        <div className="space-y-1 text-sm opacity-50">
+        <div className="p-2 space-y-1 text-sm opacity-50">
           <p>For elections to be truly Free and Fair, every voter must be able to make their own honest choices.</p>
           <p>
             If anyone is pressuring you to vote or prove you voted a certain way, SIV has{' '}
