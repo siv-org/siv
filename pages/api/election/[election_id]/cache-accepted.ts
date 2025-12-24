ba4c2bce21830094f3f22aab598c2760e22a1757
David Ernst <git@dsernst.com>
2025-12-24 15:22:28 -0800

    /cache-accepted: Add note about serverless state assumption

--- pages/api/election/[election_id]/cache-accepted.ts
+++ pages/api/election/[election_id]/cache-accepted.ts
@@ -34,6 +34,7 @@ const setCachingHeaders = (res: NextApiResponse, etag: string) => {
   res.setHeader('CDN-Cache-Control', 'public, s-maxage=60, stale-while-revalidate=86400')
 }
 
+// Assumes we're running in (Vercel) isolated serverless nodes, so global state doesn't risk contention
 let reads = 0
 let writes = 0
 let deletes = 0
