cf559f976b2c52833793737de1fca74a09bcea72
David Ernst <git@dsernst.com>
2026-01-20 18:14:37 -0800

    Upgrade sentry dep to v10

--- src/sentry.tsx
+++ src/sentry.tsx
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/browser'
-import { Integrations } from '@sentry/tracing'
 import { useEffect } from 'react'
 
 /** Hook to enable Sentry on the client */
@@ -8,7 +7,7 @@ export function useSentry() {
     Sentry.init({
       dsn: 'https://ec794d9df6c0425586d9f069775a8ad7@o510908.ingest.sentry.io/5607249',
       enabled: window.location.hostname !== 'localhost', // Disable during development
-      integrations: [new Integrations.BrowserTracing()],
+      integrations: [Sentry.browserTracingIntegration()],
       tracesSampleRate: 1.0,
     })
   }, [])
