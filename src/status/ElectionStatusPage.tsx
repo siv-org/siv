e566e94ee354a865fa867398c26125e0f3ecfebf
David Ernst <git@dsernst.com>
2026-01-30 17:13:39 -0800

    status page: Move link back to ballot under title, not inline

--- src/status/ElectionStatusPage.tsx
+++ src/status/ElectionStatusPage.tsx
@@ -37,19 +37,17 @@ export const ElectionStatusPage = (): JSX.Element => {
 
           {election_title && (
             <div>
-              <h2>
-                {election_title}
+              <h2 className="mb-1">{election_title}</h2>
 
-                {/* View Ballot */}
-                <a
-                  className="text-sm font-normal inline-block p-1.5 text-black ml-2 opacity-30 border rounded-lg transition duration-300 hover:opacity-100 hover:no-underline border-solid border-black/0 hover:border-black/30"
-                  href={`./${election_id}/vote?auth=preview`}
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Ballot
-                </a>
-              </h2>
+              {/* View Ballot */}
+              <a
+                className="text-sm font-normal inline-block p-1.5 -ml-1.5 text-black opacity-30 border rounded-lg transition duration-300 hover:opacity-100 hover:no-underline border-solid border-black/0 hover:border-black/30"
+                href={`./${election_id}/vote?auth=preview`}
+                rel="noreferrer"
+                target="_blank"
+              >
+                Ballot
+              </a>
 
               {/* Optional Election Homepage link */}
               {election_homepage && (
