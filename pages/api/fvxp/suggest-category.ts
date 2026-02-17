6722ece6285e386d0413c58d4cb58b02b024dba0
David Ernst <git@dsernst.com>
2026-02-17 02:17:54 -0800

    Add /api/fvxp/suggest-category

---
+++ pages/api/fvxp/suggest-category.ts
@@ -0,0 +1,14 @@
+import { allowCors } from 'api/_cors'
+import { pushover } from 'api/_services'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default allowCors(async (req: NextApiRequest, res: NextApiResponse) => {
+    const headers = ['x-vercel-ip-city', 'x-vercel-ip-country-region', 'x-vercel-ip-country']
+    const location = headers.map((header) => req.headers[header]?.toString().replaceAll('%20', ' ')).join(', ')
+
+    await pushover(
+        'fvxp/suggest-category',
+        'request received from: ' + location + ' (' + req.headers['x-real-ip'] + ')',
+    )
+    return res.status(200).json({ success: true })
+})
