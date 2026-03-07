b721028320811e7f05364f4c97daab50931fb3ff
Ariana I. <git@arianaivan.com>
2026-03-07 13:35:13 -0800

    Fix width of heighlight cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx', './pages/**/*.tsx'],
+  content: ['./src/**/*.{ts,tsx}', './pages/**/*.tsx'],
   corePlugins: { preflight: false },
   plugins: [],
   theme: {
