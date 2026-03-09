e23c2536e9d52c1a45398b8c5ef3680079470442
Ariana I. <git@arianaivan.com>
2026-03-09 14:45:02 -0700

    H26: delete ambient orb

--- src/homepage2026/Homepage2026.tsx
+++ src/homepage2026/Homepage2026.tsx
@@ -21,16 +21,6 @@ export function Homepage2026() {
 
   return (
     <div className={`overflow-x-hidden min-h-screen antialiased bg-h2026-bg text-h2026-text ${homepage2026FontClass}`}>
-      {/* Ambient orb */}
-      <div
-        className="pointer-events-none fixed z-0 sm:h-[700px] sm:w-[700px] h-[500px] w-[500px] rounded-full blur-[140px]"
-        style={{
-          background: 'radial-gradient(circle, rgba(26,107,74,0.3), transparent 70%)',
-          right: -200,
-          top: -100,
-        }}
-      />
-
       <div className="relative z-10">
         <Nav />
         <Hero />
