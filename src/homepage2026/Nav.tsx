28332e9ca5345218b25a49e1759b1cfc3f816545
Ariana I. <git@arianaivan.com>
2026-03-09 21:04:33 -0700

    H26-B: Nav, don't show SIV logo on small screens

--- src/homepage2026/Nav.tsx
+++ src/homepage2026/Nav.tsx
@@ -16,7 +16,7 @@ const logoWidth = (50 / 23) * logoHeight
 export function Nav() {
   return (
     <nav className="fixed left-1/2 top-4 z-[100] flex -translate-x-1/2 items-center sm:gap-9 gap-6 rounded-full border border-h2026-border bg-white/70 px-6 py-2.5 shadow-h2026-sm backdrop-blur-[24px]">
-      <Link className="relative leading-none no-underline" href="/">
+      <Link className="relative hidden leading-none no-underline sm:inline-block" href="/">
         <Image alt="SIV" height={logoHeight} src={logo} width={logoWidth} />
       </Link>
       <div className="flex gap-4 items-center sm:gap-7">
