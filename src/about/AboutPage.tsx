5e98f6fbb7684c9dd9012c096254ab83ce5b1d2c
Ariana I. <git@arianaivan.com>
2026-03-10 14:10:16 -0700

    About Page matching the H26 nav bar and footer

--- src/about/AboutPage.tsx
+++ src/about/AboutPage.tsx
@@ -1,9 +1,10 @@
-import { GlobalCSS } from 'src/GlobalCSS'
 import { useAnalytics } from 'src/useAnalytics'
 
 import { Head } from '../Head'
-import { Footer } from '../homepage/Footer'
-import { HeaderBar } from '../homepage/HeaderBar'
+import { h26fonts } from '../homepage2026/fonts'
+import { Footer } from '../homepage2026/Footer'
+import { Nav } from '../homepage2026/Nav'
+import { TailwindPreflight } from '../TailwindPreflight'
 import { AcademicResearchPapers } from './AcademicResearchPapers'
 import { Contributors } from './contributors/Contributors'
 import { ResearchHeader } from './ResearchHeader'
@@ -11,36 +12,19 @@ import { ResearchHeader } from './ResearchHeader'
 export const AboutPage = (): JSX.Element => {
   useAnalytics()
   return (
-    <>
+    <div className={`overflow-x-hidden min-h-screen antialiased bg-h26-bg text-h26-text ${h26fonts}`}>
       <Head title="About" />
-
-      <div>
-        <HeaderBar />
-
-        <Contributors />
-        <a id="research" />
-        <ResearchHeader />
-        <AcademicResearchPapers />
+      <div className="relative z-10">
+        <Nav />
+        <div className="px-4 py-14 sm:px-6 sm:py-20 md:px-8">
+          <Contributors />
+          <a id="research" />
+          <ResearchHeader />
+          <AcademicResearchPapers />
+        </div>
         <Footer />
       </div>
-
-      <GlobalCSS />
-
-      <style jsx>{`
-        div {
-          padding: 1rem 3rem;
-          padding-top: 0 !important;
-
-          width: 100%;
-          overflow-x: hidden;
-        }
-
-        @media (max-width: 700px) {
-          div {
-            padding: 1rem 2rem;
-          }
-        }
-      `}</style>
-    </>
+      <TailwindPreflight />
+    </div>
   )
 }
