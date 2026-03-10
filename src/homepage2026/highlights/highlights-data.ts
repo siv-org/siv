3140bf772d72b9b25da7bda70b39bb1caf757d69
Ariana I. <git@arianaivan.com>
2026-03-09 18:26:07 -0700

    H26: add links to highlights

--- src/homepage2026/highlights/highlights-data.ts
+++ src/homepage2026/highlights/highlights-data.ts
@@ -14,23 +14,29 @@ export const HIGHLIGHTS: Highlight[] = [
   {
     description: 'For each paper voter, 50x voted digitally.',
     href: 'https://blog.siv.org/2025/12/11chooses',
-    tag: 'Latest',
+    tag: 'Latest • December 2026',
     title: 'Multiple historic firsts, including the biggest digital election in US history',
   },
   {
     description: 'Preserving privacy & verifiability.',
     href: 'https://blog.siv.org/2025/08/overrides',
-    tag: 'Latest',
+    tag: 'Security',
     title: 'Anti-coercion & vote-selling solution, deployed',
   },
   {
     description: '$10,000 prizes. Top security researchers.',
     href: 'https://hack.siv.org/reports/2024defcon',
-    tag: 'Latest',
+    tag: 'Security',
     title: 'DEF CON Red-Team Hack\u00A0SIV Challenge: Results',
   },
   {
+    href: 'https://blog.siv.org/2023/11/utah-gop-special-election',
+    tag: 'Congressional Election',
+    title: 'Sitting Member of Congress elected using SIV',
+  },
+  {
     body: 'For decades, digital voting was correctly considered unsafe for high-stakes settings.\n\nSIV has solved:',
+    href: 'https://blog.siv.org/2025/11/siv-in-one-poster',
     items: [
       'Malware on voter devices',
       'Strong privacy',
@@ -42,29 +48,29 @@ export const HIGHLIGHTS: Highlight[] = [
     tag: 'Security',
     title: 'Strong solutions, built & deployed',
   },
+
   {
-    tag: 'Government',
-    title: 'Sitting Member of Congress elected using SIV',
-  },
-  // {
-  //   body: 'Groups with strong pre-existing distrust in US elections used SIV vs paper — leading to less fighting and more cooperation.',
-  //   quotes: ['This is election nirvana.', 'Today, we were all pollwatchers.'],
-  //   tag: 'Trust',
-  //   title: 'From distrust to cooperation',
-  // },
-  {
-    body: 'Switched to SIV for governance voting over funds worth millions.',
-    tag: 'Governance',
-    title: 'Zcash Foundation governance',
+    body: 'Groups with strong pre-existing distrust in US elections used SIV vs paper — leading to less fighting and more cooperation.',
+    href: 'https://blog.siv.org/2023/04/utah-gop-april-convention',
+    quotes: ['This is election nirvana.', 'Today, we were all pollwatchers.'],
+    tag: 'Trust',
+    title: 'From distrust to cooperation',
   },
   {
-    body: 'Rep. invited all residents to jointly allocate $2M community-improvement budget.',
+    body: 'Representative invited all residents to jointly allocate $2M community-improvement budget.',
+    href: 'https://blog.siv.org/2023/06/harlem-wallet',
     tag: 'Participatory Budgeting',
     title: 'Harlem, New York',
   },
   {
-    body: 'Used SIV to elect its leaders.',
-    tag: 'Global',
+    body: 'Switched to SIV for governance voting over funds worth millions.',
+    href: 'https://forum.zcashcommunity.com/t/request-for-zcap-members-thoughts-on-moving-from-helios-to-siv-for-future-voting/51785/7',
+    tag: 'Governance Voting',
+    title: 'Zcash Foundation governance',
+  },
+  {
+    body: 'Used SIV to elect its leaders, giving voters options to vote online or in person.',
+    tag: 'Government Election',
     title: 'Canadian First Nation government',
   },
   {
