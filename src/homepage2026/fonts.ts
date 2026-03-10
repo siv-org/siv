9273866cc8b630ce51e10885398aa4011b3f4b42
David Ernst <git@dsernst.com>
2026-03-10 05:13:56 -0700

    h26/fonts.ts: Remove unused weights

--- src/homepage2026/fonts.ts
+++ src/homepage2026/fonts.ts
@@ -2,25 +2,19 @@ import { Inter, JetBrains_Mono, Libre_Baskerville } from 'next/font/google'
 
 const inter = Inter({
   subsets: ['latin'],
-  variable: '--font-inter',
-  weight: ['300', '400', '500', '600', '700'],
+  weight: ['400', '500', '600', '700'],
 })
 
 const libreBaskerville = Libre_Baskerville({
   subsets: ['latin'],
   variable: '--font-libre-baskerville',
-  weight: ['400', '700'],
+  weight: ['400'],
 })
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
   variable: '--font-jetbrains-mono',
-  weight: ['400', '500'],
+  weight: ['400'],
 })
 
-export const homepage2026FontClass = [
-  inter.variable,
-  libreBaskerville.variable,
-  jetbrainsMono.variable,
-  inter.className,
-].join(' ')
+export const homepage2026FontClass = [libreBaskerville.variable, jetbrainsMono.variable, inter.className].join(' ')
