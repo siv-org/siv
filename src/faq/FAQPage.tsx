c5886fa672a4883d10b16a0fb1d085e9a3f79361
David Ernst <git@dsernst.com>
2026-03-15 17:16:48 -0400

    FAQPage.tsx: Remove extra <section> nesting

--- src/faq/FAQPage.tsx
+++ src/faq/FAQPage.tsx
@@ -39,75 +39,73 @@ export const FAQPage = (): JSX.Element => {
 
       <Nav />
 
-      <main className="overflow-hidden p-4 pt-24 w-full">
-        <section className="max-w-[750px] mx-auto mb-10">
-          <h1 className="text-2xl font-bold">Frequently Asked Questions</h1>
+      <main className="overflow-hidden p-4 pt-24 w-full max-w-[750px] mx-auto mb-10">
+        <h1 className="text-2xl font-bold">Frequently Asked Questions</h1>
 
-          {/* Expand/collapse all button */}
-          <div className="mb-4 text-right">
-            <OnClickButton
+        {/* Expand/collapse all button */}
+        <div className="mb-4 text-right">
+          <OnClickButton
+            onClick={() => {
+              const update = [...expanded].fill(any_collapsed)
+              setExpanded(update)
+            }}
+            style={{ fontSize: '14px', margin: 0, padding: '5px 15px', textAlign: 'right' }}
+          >
+            <>{any_collapsed ? 'Expand' : 'Collapse'} all</>
+          </OnClickButton>
+        </div>
+
+        {faq.map(({ deprecated_ids, id, q, resp }, index) => (
+          // Each item
+          <div className="mb-12 border border-black/15" key={index}>
+            {deprecated_ids && deprecated_ids.map((id) => <div id={id} key={id} />)}
+
+            {/* Question title */}
+            <h3
+              className="text-[16px] font-semibold bg-gray-700/10 hover:bg-black/10 p-4 cursor-pointer flex justify-between"
+              id={id}
               onClick={() => {
-                const update = [...expanded].fill(any_collapsed)
+                const update = [...expanded]
+                update[index] = !update[index]
                 setExpanded(update)
               }}
-              style={{ fontSize: '14px', margin: 0, padding: '5px 15px', textAlign: 'right' }}
             >
-              <>{any_collapsed ? 'Expand' : 'Collapse'} all</>
-            </OnClickButton>
+              <span>
+                {index + 1}. {q}
+              </span>
+              <label className="ml-5 w-[11px] cursor-pointer">{!expanded[index] ? '+' : '–'}</label>
+            </h3>
+
+            {/* Expanded content */}
+            {expanded[index] && (
+              <>
+                <p className="p-4 whitespace-pre-wrap" dangerouslySetInnerHTML={{ __html: resp }} />
+
+                {/* Permalink */}
+                {id && (
+                  <p className="pr-5 pb-2 text-right">
+                    <a className="font-normal text-black/50 hover:text-black/70" href={`#${id}`}>
+                      <LinkOutlined /> #{id}
+                    </a>
+                  </p>
+                )}
+              </>
+            )}
           </div>
+        ))}
 
-          {faq.map(({ deprecated_ids, id, q, resp }, index) => (
-            // Each item
-            <div className="mb-12 border border-black/15" key={index}>
-              {deprecated_ids && deprecated_ids.map((id) => <div id={id} key={id} />)}
-
-              {/* Question title */}
-              <h3
-                className="text-[16px] font-semibold bg-gray-700/10 hover:bg-black/10 p-4 cursor-pointer flex justify-between"
-                id={id}
-                onClick={() => {
-                  const update = [...expanded]
-                  update[index] = !update[index]
-                  setExpanded(update)
-                }}
-              >
-                <span>
-                  {index + 1}. {q}
-                </span>
-                <label className="ml-5 w-[11px] cursor-pointer">{!expanded[index] ? '+' : '–'}</label>
-              </h3>
-
-              {/* Expanded content */}
-              {expanded[index] && (
-                <>
-                  <p className="p-4 whitespace-pre-wrap" dangerouslySetInnerHTML={{ __html: resp }} />
-
-                  {/* Permalink */}
-                  {id && (
-                    <p className="pr-5 pb-2 text-right">
-                      <a className="font-normal text-black/50 hover:text-black/70" href={`#${id}`}>
-                        <LinkOutlined /> #{id}
-                      </a>
-                    </p>
-                  )}
-                </>
-              )}
-            </div>
-          ))}
-
-          <AddYourQuestion />
-        </section>
-
-        <Footer />
+        <AddYourQuestion />
       </main>
+
+      <Footer />
       <TailwindPreflight />
 
       <style global jsx>{`
-        section a {
+        main a {
           font-weight: bold;
           color: #1c72d7;
         }
-        section a:hover {
+        main a:hover {
           text-decoration: underline;
         }
       `}</style>
