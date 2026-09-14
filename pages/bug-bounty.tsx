4021110698f4932bcb23456eaf10f8b8c1229624
Ariana I. <git@arianaivan.com>
2026-09-13 22:06:31 -0700

    rename bug-bounty to vulnerability-program

--- pages/bug-bounty.tsx
+++
@@ -1,7 +0,0 @@
-import { BugBountyPage } from '../src/bug-bounty/BugBountyPage'
-
-export { getStaticProps } from '../src/bug-bounty/lastUpdated'
-
-export default function BugBounty(props: { activeFor: string; lastUpdated: string }) {
-  return <BugBountyPage {...props} />
-}
